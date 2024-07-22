2e09a3d14b87d06dc150471b8d49169acc60c41e
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-22 23:40:11 +0900

    init: 기본 코드 준비

---
+++ src/main/java/gift/domain/product/service/OptionService.java
@@ -0,0 +1,67 @@
+package gift.domain.product.service;
+
+import gift.domain.product.dto.OptionRequest;
+import gift.domain.product.dto.OptionResponse;
+import gift.domain.product.entity.Option;
+import gift.domain.product.entity.Product;
+import gift.domain.product.repository.OptionJpaRepository;
+import gift.domain.product.repository.ProductJpaRepository;
+import gift.exception.InvalidOptionInfoException;
+import gift.exception.InvalidProductInfoException;
+import java.util.List;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OptionService {
+
+    private final OptionJpaRepository optionJpaRepository;
+    private final ProductJpaRepository productJpaRepository;
+
+    public OptionService(OptionJpaRepository optionJpaRepository, ProductJpaRepository productJpaRepository) {
+        this.optionJpaRepository = optionJpaRepository;
+        this.productJpaRepository = productJpaRepository;
+
+    }
+
+    public void create(Product product, List<OptionRequest> optionRequestDtos) {
+        for (OptionRequest optionRequest : optionRequestDtos) {
+            Option option = optionRequest.toOption(product);
+            product.addOption(option);
+        }
+    }
+
+    public List<OptionResponse> readAll(long productId) {
+        Product product = productJpaRepository.findById(productId)
+            .orElseThrow(() -> new InvalidProductInfoException("error.invalid.product.id"));
+        List<Option> options = product.getOptions();
+        return options.stream().map(OptionResponse::from).toList();
+    }
+
+    @Transactional
+    public void update(Product product, List<OptionRequest> optionRequestDtos) {
+        optionJpaRepository.deleteAllByProductId(product.getId());
+        product.removeOptions();
+        create(product, optionRequestDtos);
+    }
+
+    @Transactional
+    public void deleteAllByProductId(long productId) {
+        optionJpaRepository.deleteAllByProductId(productId);
+    }
+
+    @Transactional
+    @Retryable(
+        retryFor = {ObjectOptimisticLockingFailureException.class},
+        maxAttempts = 100,
+        backoff = @Backoff(delay = 100)
+    )
+    public void subtractQuantity(long optionId, int quantity) {
+        Option option = optionJpaRepository.findByIdWithOptimisticLock(optionId)
+            .orElseThrow(() -> new InvalidOptionInfoException("error.invalid.option.id"));
+        option.subtract(quantity);
+    }
+}
---
+++ src/main/java/gift/domain/wishlist/dto/WishItemRequestDto.java
@@ -0,0 +1,16 @@
+package gift.domain.wishlist.dto;
+
+import gift.domain.product.entity.Product;
+import gift.domain.user.entity.User;
+import gift.domain.wishlist.entity.WishItem;
+import jakarta.validation.constraints.NotNull;
+
+public record WishItemRequestDto(
+
+    @NotNull(message = "상품 정보를 입력해주세요.")
+    Long productId)
+{
+    public WishItem toWishItem(User user, Product product) {
+        return new WishItem(null, user, product);
+    }
+}
