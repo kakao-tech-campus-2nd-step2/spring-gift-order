7bbfa3e58bdc2db7afb87fb7e395d08e30bc7000
Woojae Jeong <woo6388@naver.com>
2024-07-22 15:39:52 +0900

    refactor(RequestSCategory) : 유효성 검사 수정

--- src/main/java/gift/Model/RequestCategory.java
+++ src/main/java/gift/Model/RequestCategory.java
@@ -1,13 +1,20 @@
 package gift.Model;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record RequestCategory(
-        @NotNull
+        @NotBlank
         String name,
-        @NotNull
+
+        @NotBlank
+        @Size(min=7, max=7, message = "카테고리 색상 값의 길이는 7입니다")
         String color,
-        @NotNull
+
+        @NotBlank
         String imageUrl,
+
+        @NotNull
         String description
 ){ }
