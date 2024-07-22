8894fab6f15c1600f5624f0d10e53d04cdc7ad7b
HSHwan <tnghks2866@gmail.com>
2024-07-22 15:31:14 +0900

    refactor: jwtUtil @ConfigurationProperties 리팩토링

--- src/main/java/gift/global/security/JwtUtil.java
+++ src/main/java/gift/global/security/JwtUtil.java
@@ -7,19 +7,23 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
-@Component
+@ConfigurationProperties("jwt")
 public class JwtUtil {
-    @Value("${jwt.secretKey}")
-    private String secretKey;
 
-    @Value("${jwt.jwtExpirationInMs}")
-    private Long jwtExpirationInMs;
+    private final String secretKey;
+    private final Long jwtExpirationInMs;
+
+    @ConstructorBinding
+    public JwtUtil(String secretKey, Long jwtExpirationInMs) {
+        this.secretKey = secretKey;
+        this.jwtExpirationInMs = jwtExpirationInMs;
+    }
 
     private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
