855242dcc733fc861e2866d481ad54a80de424ad
amm0124 <amm012450@gmail.com>
2024-07-22 16:07:31 +0900

    REFACTORING : product controller endpoint 수정

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -23,28 +23,28 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("/products")
+    @GetMapping("")
     public ResponseEntity<List<ProductResponseDTO>> getProducts() {
         List<ProductResponseDTO> products = productService.getAllProducts();
         return ResponseEntity.status(HttpStatus.OK)
                 .body(products);
     }
 
-    @GetMapping("/products/{productId}")
+    @GetMapping("/{productId}")
     public ResponseEntity<Product> getProduct(@PathVariable("productId") Long productId) {
         Product product = productService.getProduct(productId);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(product);
     }
 
-    @PostMapping("/products")
+    @PostMapping("")
     public ResponseEntity<String> createProduct(@RequestBody @Valid ProductRequestDTO productRequestDTO) {
         productService.saveProduct(productRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Product added successfully");
     }
 
-    @DeleteMapping("/products/{productId}")
+    @DeleteMapping("/{productId}")
     public ResponseEntity<String> deleteProduct(@PathVariable("productId") Long productId) {
         System.out.println(productId);
         productService.deleteProduct(productId);
@@ -52,12 +52,15 @@ public class ProductController {
                 .body("Product deleted successfully");
     }
 
-    @PutMapping("/products/{productId}")
+    @PutMapping("/{productId}")
     public ResponseEntity<String> updateProduct(@PathVariable("productId") Long productId, @RequestBody @Valid ProductRequestDTO productRequestDTO) {
         productService.updateProduct(productId, productRequestDTO);
         return ResponseEntity.status(HttpStatus.OK)
                 .body("Product updated successfully");
     }
+
+
+
 
 }
 
