d4c3b37b5542fb1ea7ebd36a4e36c0d4009079c2
Woojae Jeong <woo6388@naver.com>
2024-07-22 14:41:52 +0900

    feat(RequestMember) : @NotBlank 어노테이션 추가 및 email 양식 유효성 검사 구현

--- src/main/java/gift/Model/RequestMember.java
+++ src/main/java/gift/Model/RequestMember.java
@@ -1,4 +1,18 @@
 package gift.Model;
 
-public record RequestMember(String email, String password) {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record RequestMember(
+        @NotBlank
+        @Pattern(
+                regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+                message = "이메일 주소 양식을 확인해주세요"
+        )
+        String email,
+
+        @NotBlank
+        String password
+) {}
+
+
