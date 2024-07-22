9ba86d8d6ad07344880827bbed83942df38a4ccf
Profile-exe <kang67572346@gmail.com>
2024-07-23 04:16:07 +0900

    feat(KakaoTokens): 인가 코드로 발급받은 토큰 정보를 옮기는 DTO

---
+++ src/main/java/gift/auth/dto/KakaoTokens.java
@@ -0,0 +1,16 @@
+package gift.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record KakaoTokens(
+
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("expires_in") Integer expiresIn,
+        @JsonProperty("refresh_token") String refreshToken,
+        @JsonProperty("refresh_token_expires_in") Integer refreshTokenExpiresIn,
+        @JsonProperty("scope") String scope
+) {
+}
