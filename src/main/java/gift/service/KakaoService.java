f0c22573902ac3e2aeed34008fbe663571ea892a
canyos <4581974@naver.com>
2024-07-22 16:37:25 +0900

    feat: 예외처리 추가

--- src/main/java/gift/service/KakaoService.java
+++ src/main/java/gift/service/KakaoService.java
@@ -1,5 +1,7 @@
 package gift.service;
 
+import gift.exception.exception.BadRequestException;
+import gift.exception.exception.UnAuthException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,13 @@ import java.net.URI;
 
 @Service
 public class KakaoService {
-    @Value( "${kakao.client_id}" )
+    @Value("${kakao.client_id}")
     String client_id;
-    @Value( "${kakao.redirect_uri}" )
+    @Value("${kakao.redirect_uri}")
     String redirect_uri;
     String code = "";
 
-    public String getToken(){
+    public String getToken() {
         var url = "https://kauth.kakao.com/oauth/token";
         var headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
@@ -24,10 +26,15 @@ public class KakaoService {
         body.add("grant_type", "authorization_code");
         body.add("client_id", client_id);
         body.add("redirect_uri", redirect_uri);
-        body.add("code",code);
+        body.add("code", code);
         var request = new RequestEntity<>(body, headers, HttpMethod.POST, URI.create(url));
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.postForEntity(url,request, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+        if(response.getStatusCode()==HttpStatus.UNAUTHORIZED)
+            throw new UnAuthException("인증되지 않은 요청");
+        if(response.getStatusCode()!=HttpStatus.OK)
+            throw new BadRequestException("잘못된 요청");
+
         return response.getBody();
     }
 }
