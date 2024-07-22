772e810cc1d25dcc7b85c4f8b8bcfdd0356172a5
anacnoda77 <sm1108shin@naver.com>
2024-07-22 16:47:52 +0900

    feat: 인가 코드 기반 토큰 요청 컨트롤러 메서드 작성

--- src/main/java/gift/controller/KakaoLoginApiController.java
+++ src/main/java/gift/controller/KakaoLoginApiController.java
@@ -1,15 +1,26 @@
 package gift.controller;
 
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @ConfigurationProperties(prefix = "kakao")
 public class KakaoLoginApiController {
+
+    private final RestClient client = RestClient.builder().build();
 
     @Value("${kakao.client-id}")
     private String clientId;
@@ -22,5 +33,34 @@ public class KakaoLoginApiController {
         model.addAttribute("redirect_uri", redirectUri);
         return "kakaoLogin";
     }
+
+    @RequestMapping("/logined")
+    public String logined(@RequestParam("body") String body, Model model) {
+        model.addAttribute("body", body);
+        return "successKakaoLogin";
+    }
+
+    @RequestMapping("/kakao/login/oauth2/code")
+    public String getToken(@RequestParam("code") String code, RedirectAttributes re) {
+
+        String url = "https://kauth.kakao.com/oauth/token";
+        LinkedMultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", clientId);
+        body.add("redirect_uri", redirectUri);
+        body.add("code", code);
+
+        ResponseEntity<String> response = client.post()
+            .uri(URI.create(url))
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(body)
+            .retrieve()
+            .toEntity(String.class);
+        re.addAttribute("body", response.getBody());
+        return "redirect:/logined";
+    }
+
+
+
 
 }
