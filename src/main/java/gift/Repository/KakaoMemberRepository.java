9e2095bf6f6e8be42221193b6944983b8a683880
test1 <test>
2024-07-23 01:05:56 +0900

    feat(KakaoMemberRepository) : KakaoMemberRepository 생성

---
+++ src/main/java/gift/Repository/KakaoMemberRepository.java
@@ -0,0 +1,7 @@
+package gift.Repository;
+
+import gift.DTO.KakaoMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KakaoMemberRepository extends JpaRepository<KakaoMember,String> {
+}
