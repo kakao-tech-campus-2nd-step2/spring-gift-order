7c177f6e9d78dda49a6059cfd23714f383d31edc
Seunghyun Ko <chris00825@naver.com>
2024-07-22 11:07:41 +0900

    feat: init gradle build

--- src/test/java/gift/product/service/ProductServiceTest.java
+++ src/test/java/gift/product/service/ProductServiceTest.java
@@ -1,5 +1,6 @@
 package gift.product.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
