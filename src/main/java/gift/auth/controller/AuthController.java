30fcf521f358a3227b6df979d4fe3623cb691d0c
Profile-exe <kang67572346@gmail.com>
2024-07-23 04:20:08 +0900

    feat(AuthController): 카카오 소셜 로그인 엔드포인트 구현

--- src/main/java/gift/auth/controller/AuthController.java
+++ src/main/java/gift/auth/controller/AuthController.java
@@ -3,11 +3,14 @@ package gift.auth.controller;
 import gift.auth.dto.LoginReqDto;
 import gift.auth.service.AuthService;
 import gift.auth.token.AuthToken;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +26,12 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<AuthToken> login(@RequestHeader("Authorization") String header, @RequestBody LoginReqDto loginReqDto) {
         AuthToken token = authService.login(header, loginReqDto);
+        return ResponseEntity.ok(token);
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<AuthToken> login(@RequestParam("code") String code, HttpServletResponse response) {
+        AuthToken token = authService.login(code);
         return ResponseEntity.ok(token);
     }
 }
