5c7e3451dd5be87f9d0d6a927b2fdb7f631c788d
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-22 16:16:58 +0900

    feat : KakaoLogin Controller 카카오 로그인 시 사용할 컨트롤러

---
+++ src/main/java/gift/kakaoLogin/KakaoLoginController.java
@@ -0,0 +1,43 @@
+package gift.kakaoLogin;
+
+import gift.user.LoginDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class KakaoLoginController {
+
+    @Value("${kakao.client-id}")
+    private String REST_API_KEY;
+
+    @Value("${kakao.redirect-url}")
+    private String REDIRECT_URI;
+
+    private final KakaoLoginService kakaoLoginService;
+
+    public KakaoLoginController(KakaoLoginService kakaoLoginService) {
+        this.kakaoLoginService = kakaoLoginService;
+    }
+
+    @GetMapping("/login")
+    public String getLoginView(Model model){
+        model.addAttribute("redirect_url", REDIRECT_URI);
+        model.addAttribute("client_id", REST_API_KEY);
+        model.addAttribute("loginDTO", new LoginDTO());
+        return "Login";
+    }
+
+    @GetMapping("/")
+    public String getCode(@RequestParam String code){
+        System.out.println(code);
+        String accessToken = kakaoLoginService.login(code);
+        return "MainView";
+    }
+
+
+
+}
