5efb569771fbf39e9126de25d5606a3524946943
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-22 14:21:47 +0900

    init: 4주차 클론

---
+++ src/main/java/gift/auth/JwtToken.java
@@ -0,0 +1,66 @@
+package gift.auth;
+
+import gift.entity.UserEntity;
+import gift.util.errorException.BaseHandler;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Base64;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtToken {
+
+    private String secretKey;
+    private long tokenExpTime;
+
+    public JwtToken(@Value("${jwt.secretKey}") String secretKey,
+        @Value("${jwt.tokenExpTime}") long tokenExpTime) {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        this.tokenExpTime = tokenExpTime;
+    }
+
+    public Token createToken(UserEntity user) {
+        Claims claims = Jwts.claims();
+        claims.put("id", user.getId());
+        claims.put("email", user.getEmail());
+
+        ZonedDateTime now = ZonedDateTime.now().withZoneSameInstant(ZoneId.of("UTC"));
+        ZonedDateTime expirationDateTime = now.plusSeconds(tokenExpTime);
+
+        return new Token(Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(Date.from(now.toInstant()))
+            .setExpiration(Date.from(expirationDateTime.toInstant()))
+            .signWith(SignatureAlgorithm.HS256, secretKey.getBytes())
+            .compact());
+    }
+
+    public Claims validateToken(String token) {
+        try {
+            return Jwts.parserBuilder()
+                .setSigningKey(secretKey.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        } catch (ExpiredJwtException e) {
+            throw new BaseHandler(HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다.");
+        }
+    }
+
+    public Long getId(String token) {
+        Claims claims = validateToken(token);
+        return claims.get("id", Long.class);
+    }
+
+    public String getEmail(String token) {
+        Claims claims = validateToken(token);
+        return claims.get("email", String.class);
+    }
+}
---
+++ src/main/java/gift/init/UserCreator.java
@@ -0,0 +1,21 @@
+package gift.init;
+
+import gift.domain.User.CreateUser;
+import gift.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserCreator {
+
+    @Autowired
+    private UserService userService;
+
+    public void UserCreator() {
+        userService.createUser(new CreateUser("kakao1@kakao.com", "1234"));
+        userService.createUser(new CreateUser("kakao2@kakao.com", "123456"));
+        userService.createUser(new CreateUser("kakao3@kakao.com", "1234"));
+        userService.createUser(new CreateUser("kakao4@kakao.com", "qwer1234"));
+        userService.createUser(new CreateUser("kakao5@kakao.com", "qer1234"));
+    }
+}
