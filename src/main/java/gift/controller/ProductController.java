ff167c8b04ca2d1804ae991d23bc3786dc71f7b0
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-22 09:41:31 +0900

    부산대 BE_김상해 4주차 과제 (3단계) (#418)
    * chore: lombok 추가
    
    * refactor: 멤버 컨트롤러 리턴 타입 수정
    
    - ResponseEntity<Map<String, Object>> 에서 ResponseEntity<ApiResponse>로 수정
    
    * chore: 파일 포매팅
    
    * chore: 파일 포매팅
    
    * feat: 카테고리 생성
    
    * chore: 파일 포매팅
    
    * chore: 파일포매팅
    
    * chore: 파일 포매팅
    
    * refactor: 리턴 타입 단일화
    
    * chore: 파일 포매팅
    
    * feat: PathVariable로 입력 방식 변경
    
    * feat: 생성자 및 게터 추가
    
    * refactor: 어노테이션 정리
    
    * docs: 초기화 데이터 사용하지 않음으로 변경
    
    * fix: 카테고리 컨트롤러 테스트 오류 수정
    
    * feat: 옵션 테스트 추가
    
    * feat: Transactional 어노테이션 추가
    
    * docs: README 변경

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -90,6 +90,7 @@ public class ProductController {
     public String showAddProductForm(Model model) {
         model.addAttribute("productDto", new ProductDto());
         model.addAttribute("categories", categoryService.getAllCategories());
+//        model.addAttribute("options", optionService.retreiveOptions());
         return "addproductform";
     }
 
