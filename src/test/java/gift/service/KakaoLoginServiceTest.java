7595826d97178096a13e4a003457f422cab01dcc
JunHyeongChoi <ajfeh22@gmail.com>
2024-07-22 19:17:37 +0900

    refactor: RestTemplate를 사용하는 메서드와 분리해서 테스트

--- src/test/java/gift/service/KakaoLoginServiceTest.java
+++ src/test/java/gift/service/KakaoLoginServiceTest.java
@@ -9,7 +9,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.client.RestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -20,9 +19,6 @@ class KakaoLoginServiceTest {
 
     @Mock
     private KakaoProperties properties;
-
-    @Mock
-    private RestClient client;
 
     @InjectMocks
     private KakaoLoginService kakaoLoginService;
@@ -49,53 +45,5 @@ class KakaoLoginServiceTest {
         then(properties).should().redirectUrl();
         then(properties).should().clientId();
     }
-
-//    @DisplayName("Access Token 을 가져온다.")
-//    @Test
-//    void getAccessToken() throws Exception {
-//        //given
-//        String code = "auth-code";
-//        String clientId = "test-client-id";
-//        String redirectUrl = "https://test.com";
-//        String accessToken = "test-access-token";
-//
-//        KakaoTokenInfoResponse response = new KakaoTokenInfoResponse(accessToken);
-//
-//        given(properties.clientId()).willReturn(clientId);
-//        given(properties.redirectUrl()).willReturn(redirectUrl);
-//
-//        RestClient.RequestBodyUriSpec requestBodyUriSpec = mock(RestClient.RequestBodyUriSpec.class);
-//        RestClient.RequestBodySpec requestBodySpec = mock(RestClient.RequestBodySpec.class);
-//        RestClient.ResponseSpec responseSpec = mock(RestClient.ResponseSpec.class);
-//
-//        given(client.post()).willReturn(requestBodyUriSpec);
-//        given(requestBodyUriSpec.uri(anyString())).willReturn(requestBodyUriSpec);
-//        given(requestBodyUriSpec.contentType(any(MediaType.class))).willReturn(requestBodyUriSpec);
-//        given(requestBodyUriSpec.body(any())).willReturn(requestBodySpec);
-//        given(requestBodySpec.retrieve()).willReturn(responseSpec);
-//        given(responseSpec.body(KakaoTokenInfoResponse.class)).willReturn(response);
-//
-//
-//        LinkedMultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-//        body.add("grant_type", "authorization_code");
-//        body.add("client_id", clientId);
-//        body.add("redirect_uri", redirectUrl);
-//        body.add("code", code);
-//
-//        given(client.post()
-//                .uri("https://kauth.kakao.com/oauth/token")
-//                .contentType(APPLICATION_FORM_URLENCODED)
-//                .body(body)
-//                .retrieve()
-//                .body(KakaoTokenInfoResponse.class)).willReturn(response);
-//
-//        //when
-//        String actualAccessToken = kakaoLoginService.getAccessToken(code);
-//
-//        //then
-//        assertThat(actualAccessToken).isEqualTo(accessToken);
-//        then(properties).should().clientId();
-//        then(properties).should().redirectUrl();
-//    }
 
 }
