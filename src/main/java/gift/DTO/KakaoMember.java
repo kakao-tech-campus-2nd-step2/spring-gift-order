761ae677e4c6c955e2a13a3a74e872e71ae7a85a
test1 <test>
2024-07-23 01:04:17 +0900

    feat(KakaoJwtToken) : KakaoMember 생성

---
+++ src/main/java/gift/DTO/KakaoMember.java
@@ -0,0 +1,33 @@
+package gift.DTO;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class KakaoMember {
+
+  @Id
+  private String clientId;
+  private String code;
+
+  protected KakaoMember() {
+  }
+
+  public KakaoMember(String clientId, String code) {
+    this.clientId = clientId;
+    this.code = code;
+  }
+
+  public String getClientId() {
+    return this.clientId;
+  }
+
+  public String getCode(){
+    return this.code;
+  }
+
+}
