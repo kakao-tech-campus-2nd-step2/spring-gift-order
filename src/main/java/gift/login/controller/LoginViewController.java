0e1f62b7b2fc4e6b56911f2fad9f7ac4679b6091
dong_yxxn <g0326kdy@gmail.com>
2024-07-22 22:08:26 +0900

    feat(LoginViewController): 로그인 페이지로 인증코드 url 넘기기

--- src/main/java/gift/login/controller/LoginViewController.java
+++ src/main/java/gift/login/controller/LoginViewController.java
@@ -1,2 +1,28 @@
-package gift.login.controller;public class LoginViewController {
+package gift.login.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginViewController {
+
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-url}")
+    private String redirectUrl;
+
+    @GetMapping
+    public String showLoginPage(Model model){
+        String location = "https://kauth.kakao.com/oauth/authorize?client_id=" + clientId
+            + "&redirect_url=" + redirectUrl
+            + "&response_type=code";
+        model.addAttribute("location", location);
+
+        return "kakao_login";
+    }
 }
