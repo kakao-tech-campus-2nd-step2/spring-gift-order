43eca0f749463e06e0ca4461d5da9eb2bfdb38e7
donghyuun <bomul0524@gmail.com>
2024-07-23 22:08:41 +0900

    refactor: 불필요 어노테이션 제걱

--- src/main/java/gift/domain/user/dto/kakao/KaKaoToken.java
+++ src/main/java/gift/domain/user/dto/kakao/KaKaoToken.java
@@ -1,11 +1,7 @@
 package gift.domain.user.dto.kakao;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record KaKaoToken(
-    @NotBlank
     String accessToken,
-    @NotBlank
     String refreshToken
 ) {
 
