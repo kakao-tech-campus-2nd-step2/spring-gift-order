8eb459c846329105e96e3aceb350a35c2d631377
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-23 15:55:01 +0900

    feat : KakaoLoginService 예외 처리 로직 변경

--- src/main/java/gift/kakaoLogin/KakaoLoginService.java
+++ src/main/java/gift/kakaoLogin/KakaoLoginService.java
@@ -1,51 +1,69 @@
 package gift.kakaoLogin;
 
+import gift.exception.ErrorCode;
+import gift.exception.KakaoException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestClient;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class KakaoLoginService {
 
     @Value("${kakao.client-id}")
     private String REST_API_KEY;
     @Value("${kakao.redirect-url}")
     private String REDIRECT_URI;
-    private final RestClient client = RestClient.create();
+    private final RestClient kakaoRestClient;
+
+    public KakaoLoginService(RestClient kakaoRestClient) {
+        this.kakaoRestClient = kakaoRestClient;
+    }
 
     public String login(String code){
         var url = "https://kauth.kakao.com/oauth/token";
 
         LinkedMultiValueMap<String, String> body = createBody(code);
 
-        ResponseEntity<Map> response = client.post()
+        ResponseEntity<KakaoResponse> response = kakaoRestClient.post()
                 .uri(URI.create(url))
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(body)
                 .retrieve()
                 .onStatus(status -> status.value() != 200, ((request, response1) -> {
-                    throw new RuntimeException("kakao " + response1.getStatusCode());
+                    log.info("[ERROR] " + response1.getStatusCode());
+
+                    ErrorCode errorCode = getKauthErrorCode(response1);
+                    throw new KakaoException(errorCode);
                 }))
-                .toEntity(Map.class);
+                .toEntity(KakaoResponse.class);
 
-        if(response.getStatusCode() == HttpStatus.UNAUTHORIZED){
-            throw new IllegalArgumentException();
-        }
+        return response.getBody().getAccess_token();
+    }
 
-        return (String) response.getBody().get("access_token");
+    private static ErrorCode getKauthErrorCode(ClientHttpResponse response) throws IOException {
+        String[] split = response.getHeaders().get("WWW-Authenticate").get(0).split(",");
+        String error = split[1].split("=")[1].replaceAll("\"", "");
+        String errorDescription = split[2].split("=")[1].replaceAll("\"", "");
+
+        ErrorCode errorCode = new ErrorCode(HttpStatus.valueOf(response.getStatusCode().value()), error, errorDescription);
+        return errorCode;
     }
 
     private LinkedMultiValueMap<String, String> createBody(String code) {
         LinkedMultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
-        body.add("client_id", REST_API_KEY);
+        body.add("client_id", ".");
         body.add("redirect_url", REDIRECT_URI);
         body.add("code", code);
         return body;
