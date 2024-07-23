272d38f70adcbc0f17d862391ed68e07973a39e0
hjinshin <gudwls818@gmail.com>
2024-07-23 22:19:21 +0900

    style: 불필요한 import 제거

--- src/main/java/gift/controller/restcontroller/AuthRestController.java
+++ src/main/java/gift/controller/restcontroller/AuthRestController.java
@@ -6,9 +6,11 @@ import gift.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Auth", description = "로그인 API")
 @RestController
--- src/test/java/study/RestClientTest.java
+++ src/test/java/study/RestClientTest.java
@@ -1,6 +1,5 @@
 package study;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.service.dto.KakaoTokenDto;
 import jakarta.validation.constraints.NotNull;
