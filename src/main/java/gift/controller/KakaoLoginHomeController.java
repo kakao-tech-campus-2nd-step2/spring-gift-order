91538dd14bccb9798b26b5eabba58cd07debd64d
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-23 19:11:18 +0900

    feat: KakaoLoginHomeController 구현

---
+++ src/main/java/gift/controller/KakaoLoginHomeController.java
@@ -0,0 +1,20 @@
+package gift.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class KakaoLoginHomeController {
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    @GetMapping("/kakao/login")
+    public String kakaoLoginPage(Model model) {
+        String kakaoAuthUrl = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri;
+        model.addAttribute("kakaoAuthUrl", kakaoAuthUrl);
+        return "kakao-login";
+    }
+}
