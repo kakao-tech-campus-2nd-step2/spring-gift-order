7c201235b9250e0250a5ae024c6721110bac03f9
wvssm <neoramiregi@gmail.com>
2024-07-23 19:16:13 +0900

    refactor(ProductTest): Product 객체 생성자에 Option 파라미터 추가

--- src/test/java/gift/domain/ProductTest.java
+++ src/test/java/gift/domain/ProductTest.java
@@ -18,7 +18,8 @@ class ProductTest {
     @BeforeEach
     void setUp(){
         category = new Category("교환권","#FF5733","https://example.com/images/exchange_voucher.jpg","다양한 상품으로 교환 가능한 교환권");
-        product = new Product("상품1", 1000, "http://product1", category);
+        Option option = new Option("옵션1", 10);
+        product = new Product("상품1", 1000, "http://product1", category, option);
         member = new Member("a@a.com","1234");
         wish = new Wish(member,product,1);
     }
