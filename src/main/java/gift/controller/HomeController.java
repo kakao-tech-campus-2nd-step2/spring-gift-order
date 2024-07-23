e125647bebb056cb7de9d369c7c9ba730c4a1b40
JJY <ddglackrp@gmail.com>
2024-07-23 16:02:30 +0900

    feat "view 추가"

--- src/main/java/gift/controller/HomeController.java
+++ src/main/java/gift/controller/HomeController.java
@@ -1,14 +1,14 @@
 package gift.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/home")
     public String home(){
-        return "누구나 접근할 수 있는 페이지 입니다.";
+        return "home";
     }
 
 }
