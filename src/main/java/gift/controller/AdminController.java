5166fd1ec347baf77591cc0dd8157de701cc6d6c
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-24 01:08:44 +0900

    feat : 4주차 코드 옮기기

---
+++ src/main/java/gift/controller/AdminController.java
@@ -0,0 +1,88 @@
+package gift.controller;
+
+import gift.dto.ProductDTO;
+import gift.model.Product;
+import gift.service.CategoryService;
+import gift.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/products")
+public class AdminController {
+
+    private final ProductService productService;
+    private final CategoryService categoryService;
+
+    public AdminController(ProductService productService, CategoryService categoryService) {
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public String getProducts(Model model, @PageableDefault(size = 5) Pageable pageable) {
+        Page<Product> products = productService.findAllProducts(pageable);
+        model.addAttribute("products", products);
+        return "product_list";
+    }
+
+    @GetMapping("/new")
+    public String showAddProductForm(Model model) {
+        model.addAttribute("productDTO", new ProductDTO("", "0", null, ""));
+        model.addAttribute("categories", categoryService.findAllCategories());
+        return "add_product_form";
+    }
+
+    @PostMapping
+    public String addProduct(@ModelAttribute @Valid ProductDTO productDTO, BindingResult result,
+        Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("productDTO", productDTO);
+            model.addAttribute("categories", categoryService.findAllCategories());
+            return "add_product_form";
+        }
+        productService.saveProduct(productDTO);
+        return "redirect:/admin/products";
+    }
+
+    @GetMapping("/{id}")
+    public String showEditProductForm(@PathVariable("id") long id, Model model) {
+        Product product = productService.findProductsById(id);
+        model.addAttribute("productDTO", ProductService.toDTO(product));
+        model.addAttribute("productID", id);
+        model.addAttribute("categories", categoryService.findAllCategories());
+        return "edit_product_form";
+    }
+
+    @PutMapping("/{id}")
+    public String editProduct(@PathVariable("id") long id,
+        @ModelAttribute @Valid ProductDTO updatedProductDTO,
+        BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("productID", id);
+            model.addAttribute("categories", categoryService.findAllCategories());
+            return "edit_product_form";
+        }
+        productService.updateProduct(updatedProductDTO, id);
+        return "redirect:/admin/products";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable("id") long id) {
+        productService.deleteProductAndWishlistAndOptions(id);
+        return "redirect:/admin/products";
+    }
+
+}
---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,60 @@
+package gift.controller;
+
+import gift.dto.ProductDTO;
+import gift.model.Product;
+
+import gift.service.CategoryService;
+import gift.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+    private final CategoryService categoryService;
+
+    public ProductController(ProductService productService, CategoryService categoryService) {
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody ProductDTO newProductDTO) {
+        productService.saveProduct(newProductDTO);
+        return ProductService.toEntity(newProductDTO, null,
+            categoryService.findCategoryById(newProductDTO.categoryId()));
+    }
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable(value = "id") long id) {
+        return productService.findProductsById(id);
+    }
+
+    @GetMapping
+    public Page<Product> getAllProduct(@PageableDefault(size = 5) Pageable pageable) {
+        return productService.findAllProducts(pageable);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable(value = "id") long id) {
+        productService.deleteProductAndWishlistAndOptions(id);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable("id") long id,
+        @RequestBody ProductDTO updatedProductDTO) {
+        productService.updateProduct(updatedProductDTO, id);
+        return productService.findProductsById(id);
+    }
+}
