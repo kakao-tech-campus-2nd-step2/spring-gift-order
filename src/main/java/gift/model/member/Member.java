8be368dac3bcec6e0e003eca6c923a7a8f6c649e
UiChan Lee <162671129+ana-raka@users.noreply.github.com>
2024-07-24 00:29:53 +0900

    강원대 BE_이의찬_5주차 과제(step0)  (#84)
    * Week5 step0 initial commit
    
    * Docs: step0 Readme update
    
    * Feat: columnDefinition comment 추가
    
    * Refactor: orElseThrow 적용
    
    * Feat: CategoryService 생성
    
    * Refactor: CategoryService 제거
    
    * Refactor: 상품 수량 감소 메서드 수정
    
    * Feat: 동시성 제어
    
    * Fix: ' 추가
    
    * Test: OptionTest 수행

---
+++ src/main/java/gift/model/member/Member.java
@@ -0,0 +1,52 @@
+package gift.model.member;
+
+import gift.model.wish.Wish;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, columnDefinition = "VARCHAR(255) COMMENT '고객 이메일'")
+    private String email;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) COMMENT '고객 비밀번호'")
+    private String password;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) COMMENT '권한'")
+    private String role;
+
+    protected Member() {
+    }
+  
+    public Member(String email, String password,String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public boolean isPasswordEqual(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+}
