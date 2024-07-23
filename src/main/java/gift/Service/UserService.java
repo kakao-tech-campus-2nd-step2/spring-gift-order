6a159e729318bedf223fe518d51ec822e3e6c97e
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-24 01:24:36 +0900

    경북대 BE_이도훈 5주차 과제(1단계) (#101)
    * 4주차 과제 불러오기
    
    * docs : 1단계 구현사항 추가
    
    * feat : API-KEY가 포함된 속성 추가
    
    * feat : API-KEY 파일 추가
    
    * feat : restTemplate 추가
    
    * feat : 카카오 로그인 기능 추가
    인증 화면으로 redirect하는 getKakaoLoginUrl 메소드,
    인증코드를 파라미터로 받아 토큰을 리턴하는 kakaoLogin 메소드 추가
    "
    
    * feat : 카카오 로그인 controller 추가

--- src/main/java/gift/Service/UserService.java
+++ src/main/java/gift/Service/UserService.java
@@ -7,18 +7,32 @@ import gift.Model.Entity.MemberEntity;
 import gift.Repository.MemberRepository;
 import gift.Token.JwtToken;
 import gift.Token.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Optional;
 
 @Service
 public class UserService {
         private final MemberRepository memberRepository;
         private final JwtTokenProvider jwtTokenProvider;
+        private final RestTemplate restTemplate;
 
-        public UserService(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider){
+        @Value("${redirectUri}")
+        private String redirectUri;
+        @Value("${clientId}")
+        private String clientId;
+        @Value("${restApiKey}")
+        private String restApiKey;
+
+        public UserService(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider,RestTemplate restTemplate){
             this.memberRepository = memberRepository;
             this.jwtTokenProvider = jwtTokenProvider;
+            this.restTemplate = restTemplate;
         }
 
         public JwtToken register(MemberDTO memberDTO){
@@ -37,5 +51,27 @@ public class UserService {
                 throw new LoginException();
 
             return new JwtToken(jwtTokenProvider.createToken(member.get()));
+        }
+
+        public String getKakaoLoginUrl(){
+            return "https://kauth.kakao.com/oauth/authorize" +
+                    "?scope=talk_message" +
+                    "&response_type=code" +
+                    "&redirect_uri="+ redirectUri +
+                    "&client_id=" + restApiKey;
+        }
+
+        public ResponseEntity<?> kakaoLogin(String authorizationCode){
+            var url = "https://kauth.kakao.com/oauth/token";
+            var headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+            var body = new LinkedMultiValueMap<String, String>();
+            body.add("grant_type", "authorization_code");
+            body.add("client_id", clientId);
+            body.add("redirect_uri", redirectUri);
+            body.add("code", authorizationCode);
+            var request = new RequestEntity<>(body, headers, HttpMethod.POST, URI.create(url));
+
+            return restTemplate.exchange(request, String.class);
         }
 }
