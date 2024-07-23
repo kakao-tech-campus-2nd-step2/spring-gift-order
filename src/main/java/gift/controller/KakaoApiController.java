204d3d942567b66b322b14afa77f6ad74cc9c87c
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-07-23 20:23:33 +0900

    feat : 주문 controller 만들기

--- src/main/java/gift/controller/KakaoApiController.java
+++ src/main/java/gift/controller/KakaoApiController.java
@@ -1,11 +1,14 @@
 package gift.controller;
 
+import gift.controller.dto.KakaoApiDTO;
+import gift.controller.dto.KakaoApiDTO.KakaoOrderResponse;
 import gift.service.KakaoApiService;
 import gift.utils.config.KakaoProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +38,13 @@ public class KakaoApiController {
         String kakaoToken = kakaoApiService.createKakaoToken(code, error, error_description, state);
 
         return ResponseEntity.ok(kakaoToken);
+    }
+
+    @PostMapping("/oauth/orders")
+    public ResponseEntity<KakaoApiDTO.KakaoOrderResponse> kakaoOrder(@RequestHeader("Authorization") String authHeader,
+        @RequestBody KakaoApiDTO.KakaoOrderRequest kakaoOrderRequest){
+        String accessToken = authHeader.substring(7);
+        KakaoOrderResponse kakaoOrderResponse = kakaoApiService.kakaoOrder(kakaoOrderRequest,accessToken);
+        return ResponseEntity.ok(kakaoOrderResponse);
     }
 }
