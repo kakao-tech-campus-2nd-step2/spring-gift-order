f2817a64ccd9052e95754e0de339ead8c8c7b01d
JinSeoHyun <15roa1121@gmail.com>
2024-07-23 16:11:08 +0900

    refactor(KakaoController) : 코드-토큰-정보조회-가입-jwt이 한 메서드에 있던 것을 일단 가입과 jwt 발급 부분 분리

--- src/main/java/gift/main/controller/KakaoController.java
+++ src/main/java/gift/main/controller/KakaoController.java
@@ -1,11 +1,13 @@
 package gift.main.controller;
 
 import gift.main.config.KakaoProperties;
+import gift.main.dto.KakaoProfile;
 import gift.main.dto.KakaoTokenResponse;
 import gift.main.dto.UserJoinRequest;
 import gift.main.service.KakaoService;
 import gift.main.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +21,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/kakao")
+@RequestMapping("/kakao/login")
 public class KakaoController {
 
     private final KakaoProperties kakaoProperties;
     private final UserService userService;
     private final KakaoService kakaoService;
+    private String code;
+    private HttpServletResponse response;
 
     public KakaoController(KakaoProperties kakaoProperties, UserService userService, KakaoService kakaoService) {
         this.kakaoProperties = kakaoProperties;
@@ -32,8 +36,8 @@ public class KakaoController {
         this.kakaoService = kakaoService;
     }
 
-    //카카오 로그인 화면 보여주기
-    @GetMapping("/login")
+    //0. 카카오 로그인 화면 보여주기
+    @GetMapping()
     public void requestKakaoLoginScreen(HttpServletResponse response) throws IOException {
         String url = kakaoProperties.codeRequestUri() +
                 "?client_id=" + kakaoProperties.clientId() +
@@ -43,8 +47,8 @@ public class KakaoController {
         response.sendRedirect(url);
     }
 
-    //카카오 로그인+유저정보등록+jwt발급하기
-    @GetMapping("/login/callback")
+    //1. 전달받은 코드로 엑세스 토큰 요청하기
+    @GetMapping(value = "/login/callback", params = {"code"})
     public ResponseEntity<?> getKakaoCode(@RequestParam("code") String code, HttpServletResponse response) {
         Map<String, Object> responseBody = new HashMap<>();
         KakaoTokenResponse tokenResponse = kakaoService.receiveKakaoToken(code);
@@ -55,5 +59,19 @@ public class KakaoController {
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .body(responseBody);
     }
+
+    //2. 전달받은 토큰으로 유저 정보 가져오기 -> 이건 왠지 가넝일듯
+    @GetMapping("/callback")
+    public ResponseEntity<?> getKakaoCode(HttpServletResponse response, KakaoProfile kakaoProfile) {
+        Map<String, Object> responseBody = new HashMap<>();
+        String token = userService.loginKakaoUser(kakaoProfile);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .body(responseBody);
+    }
+
+
+
+
 
 }
