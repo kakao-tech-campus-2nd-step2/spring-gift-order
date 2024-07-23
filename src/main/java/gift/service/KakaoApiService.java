f9fdea4d642f4756d95d97419aa11772a71889dd
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-07-23 17:36:53 +0900

    chore : 불필요한 import 삭제

--- src/main/java/gift/service/KakaoApiService.java
+++ src/main/java/gift/service/KakaoApiService.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestClient;
