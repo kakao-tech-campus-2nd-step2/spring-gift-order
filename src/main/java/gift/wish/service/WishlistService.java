31553c12162cde3770593f3804cd01bce2fe7c10
dong_yxxn <g0326kdy@gmail.com>
2024-07-23 23:32:19 +0900

    feat(WishlistService): WishslsitRepsone 사용

--- src/main/java/gift/wish/service/WishlistService.java
+++ src/main/java/gift/wish/service/WishlistService.java
@@ -1,13 +1,17 @@
 package gift.wish.service;
 
+import gift.product.service.ProductService;
 import gift.wish.domain.WishlistItem;
+import gift.wish.domain.WishlistResponse;
 import gift.wish.repository.WishlistRepository;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +19,29 @@ import org.springframework.stereotype.Service;
 public class WishlistService {
 
     private final WishlistRepository wishlistRepository;
-    @Autowired
-    public WishlistService(WishlistRepository wishlistRepository) {
+    private final ProductService productService;
+
+    public WishlistService(WishlistRepository wishlistRepository, ProductService productService) {
         this.wishlistRepository = wishlistRepository;
+        this.productService = productService;
     }
+
 
     public Page<WishlistItem> getWishlistByUserId(Long userId, Pageable pageable) {
         return wishlistRepository.findListByUserId(userId, pageable);
+    }
+    public Page<WishlistResponse> getWishlistResponseByUserId(Long userId, Pageable pageable){
+        Page<WishlistItem> wishlistItems = wishlistRepository.findListByUserId(userId, pageable);
+
+        List<WishlistResponse> wishlistResponses = wishlistItems.stream()
+            .map(item -> new WishlistResponse(
+                item.getId(),
+                item.getUser().getId(),
+                productService.convertToDTO(item.getProduct()),
+                item.getAmount()))
+            .toList();
+
+        return new PageImpl<>(wishlistResponses, pageable, wishlistItems.getTotalElements());
     }
     public List<WishlistItem> saveWishlistItemsWithUserId(Long userId, List<WishlistItem> wishlistItems) {
         List<WishlistItem> userWishlistItems = wishlistRepository.findListByUserId(userId);
