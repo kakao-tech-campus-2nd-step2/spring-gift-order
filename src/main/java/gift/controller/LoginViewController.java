b3c8a8c44cfb3048b31a70bc72953c4b5a667a54
noojmogstel <goaudgo@naver.com>
2024-07-23 16:15:52 +0900

    refactor: LoginViewController url 매핑변경

--- src/main/java/gift/controller/LoginViewController.java
+++ src/main/java/gift/controller/LoginViewController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/loginView")
 public class LoginViewController {
 
     @GetMapping
