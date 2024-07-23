f851eae28d489be4378d959f4295943f08494b60
박민규 <pmq0101@naver.com>
2024-07-24 03:57:43 +0900

    refactor(AuthController): change uri members -> auth

--- src/main/java/gift/controller/AuthController.java
+++ src/main/java/gift/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
     private final UserService userService;
