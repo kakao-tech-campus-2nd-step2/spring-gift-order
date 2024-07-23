bd494ea48e3773262cd4243b5d2e089e89bfbcba
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-23 15:14:29 +0900

    feat : 카카오 로그인 controller 추가

--- src/main/java/gift/Controller/LoginRestController.java
+++ src/main/java/gift/Controller/LoginRestController.java
@@ -2,10 +2,11 @@ package gift.Controller;
 
 import gift.Model.DTO.MemberDTO;
 import gift.Service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 public class LoginRestController {
@@ -23,5 +24,17 @@ public class LoginRestController {
     @PostMapping("/members/login")
     public ResponseEntity<?> login(@RequestBody MemberDTO memberDTO){
         return ResponseEntity.ok(userService.login(memberDTO));
+    }
+
+    @GetMapping("/kakao/link")
+    public void test(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+        response.setHeader("Location", userService.getKakaoLoginUrl());
+    }
+
+    @GetMapping("/kakao/login")
+    public ResponseEntity<?> kakaoCallback(@RequestParam String code) {
+        ResponseEntity<?> result = userService.kakaoLogin(code);
+        return result;
     }
 }
