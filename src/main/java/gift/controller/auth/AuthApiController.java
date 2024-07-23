b932b9205eb151cd493ff44d4d5ea3958cebba4c
JJY <ddglackrp@gmail.com>
2024-07-23 15:57:27 +0900

    feat "Kakao OAuth 로그인 처리 API 추가"
    - API 전용 CONTROLLER 로 분리

--- src/main/java/gift/controller/auth/AuthApiController.java
+++ src/main/java/gift/controller/auth/AuthApiController.java
@@ -1,34 +1,35 @@
-package gift.controller;
+package gift.controller.auth;
 
 import gift.dto.request.MemberRequestDto;
 import gift.dto.response.MemberResponseDto;
 import gift.service.AuthService;
+import gift.service.KakaoOauthService;
 import gift.service.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-@Controller
-public class AuthController {
+@RestController
+public class AuthApiController {
 
     private final AuthService authService;
     private final TokenService tokenService;
 
-    public AuthController(AuthService authService, TokenService tokenService) {
+    private final KakaoOauthService kakaoOauthService;
+
+    public AuthApiController(AuthService authService, TokenService tokenService, KakaoOauthService kakaoOauthService) {
         this.authService = authService;
         this.tokenService = tokenService;
+        this.kakaoOauthService = kakaoOauthService;
     }
 
-    @ResponseBody
     @PostMapping("/members/register")
     public ResponseEntity<Map<String, String>> memberSignUp(@RequestBody @Valid MemberRequestDto memberRequestDto){
         authService.memberJoin(memberRequestDto);
@@ -38,7 +39,6 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.CREATED).contentType(MediaType.APPLICATION_JSON).body(response);
     }
 
-    @ResponseBody
     @PostMapping("/members/login")
     public ResponseEntity<Map<String, String>> memberLogin(@RequestBody @Valid MemberRequestDto memberRequestDto){
         MemberResponseDto memberResponseDto = authService.findOneByEmailAndPassword(memberRequestDto);
@@ -47,6 +47,25 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.CREATED).contentType(MediaType.APPLICATION_JSON).body(response);
     }
+
+    @GetMapping("/members/login/oauth/kakao")
+    public ResponseEntity<Map<String, String>> memberKakaoLogin(HttpServletRequest servletRequest){
+        String code = servletRequest.getParameter("code");
+
+        String kakaoAccessToken = kakaoOauthService.getKakaoAccessToken(code);
+
+        String kakaoUserInformation = kakaoOauthService.getKakaoUserInformation(kakaoAccessToken);
+
+        MemberResponseDto memberResponseDto = authService.kakaoMemberLogin(kakaoUserInformation);
+
+        tokenService.tokenSave(kakaoAccessToken, memberResponseDto.email());
+
+        Map<String, String> response = new HashMap<>();
+        response.put("token", kakaoAccessToken);
+
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(response);
+    }
+
 
     public Map<String, String> getToken(String memberRequestDto) {
         UUID uuid = UUID.randomUUID();
