6565c84aadd907c263a1a2f02de793c61e716e0c
cussle <cussle@kakao.com>
2024-07-24 03:26:57 +0900

    feat: 액세스 토큰 추출 기능 구현

--- src/main/java/gift/controller/oauth/KakaoOAuthController.java
+++ src/main/java/gift/controller/oauth/KakaoOAuthController.java
@@ -1,20 +1,25 @@
 package gift.controller.oauth;
 
 import gift.config.KakaoProperties;
+import gift.dto.oauth.KakaoTokenResponse;
+import gift.service.oauth.KakaoOAuthService;
 import java.net.URI;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KakaoOAuthController {
 
     private final KakaoProperties kakaoProperties;
+    private final KakaoOAuthService kakaoOAuthService;
 
-    public KakaoOAuthController(KakaoProperties kakaoProperties) {
+    public KakaoOAuthController(KakaoProperties kakaoProperties, KakaoOAuthService kakaoOAuthService) {
         this.kakaoProperties = kakaoProperties;
+        this.kakaoOAuthService = kakaoOAuthService;
     }
 
     @GetMapping("/oauth/kakao")
@@ -25,5 +30,11 @@ public class KakaoOAuthController {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(URI.create(kakaoAuthUrl));
         return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+    }
+
+    @GetMapping("/oauth/kakao/callback")
+    public ResponseEntity<KakaoTokenResponse> kakaoCallback(@RequestParam("code") String code) {
+        KakaoTokenResponse response = kakaoOAuthService.getAccessToken(code);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/gift/service/oauth/KakaoOAuthService.java
+++ src/main/java/gift/service/oauth/KakaoOAuthService.java
@@ -1,5 +1,57 @@
 package gift.service.oauth;
 
+import gift.config.KakaoProperties;
+import gift.dto.oauth.KakaoTokenResponse;
+import java.net.URI;
+import java.util.Map;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientResponseException;
+
+@Service
 public class KakaoOAuthService {
 
+    private final KakaoProperties kakaoProperties;
+    private final RestClient client;
+
+    public KakaoOAuthService(KakaoProperties kakaoProperties, RestClient client) {
+        this.kakaoProperties = kakaoProperties;
+        this.client = client;
+    }
+
+    public KakaoTokenResponse getAccessToken(String code) {
+        String kakaoTokenUrl = "https://kauth.kakao.com/oauth/token";
+        LinkedMultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+
+        requestBody.add("grant_type", "authorization_code");
+        requestBody.add("client_id", kakaoProperties.clientId());
+        requestBody.add("redirect_uri", kakaoProperties.redirectUrl());
+        requestBody.add("code", code);
+
+        try {
+            ResponseEntity<Map> response = client.post()
+                .uri(URI.create(kakaoTokenUrl))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(requestBody)
+                .retrieve()
+                .toEntity(Map.class);
+            Map<String, Object> responseBody = response.getBody();
+
+            if (responseBody != null) {
+                String accessToken = (String) responseBody.get("access_token");
+                Integer expiresIn = (Integer) responseBody.get("expires_in");
+                String refreshToken = (String) responseBody.get("refresh_token");
+                Integer refreshTokenExpiresIn = (Integer) responseBody.get("refresh_token_expires_in");
+
+                return new KakaoTokenResponse(accessToken, expiresIn, refreshToken, refreshTokenExpiresIn);
+            } else {
+                throw new RuntimeException("토큰 응답이 올바르지 않습니다.");
+            }
+        } catch (RestClientResponseException e) {
+            throw new RuntimeException("토큰 발급에 실패했습니다.", e);
+        }
+    }
 }
