7627df523bfce4315a133d8d0fc2526efa9e0670
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-23 15:53:11 +0900

    feat : GlobalExceptionHandler kakaoException Handler

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package gift;
+package gift.exception;
 
 import gift.product.ResponseDTO;
 import io.jsonwebtoken.JwtException;
@@ -50,50 +50,16 @@ public class GlobalExceptionHandler {
 
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleRuntimeException(RuntimeException ex){
-        if(ex.getMessage().startsWith("kakao")){
-            int status = Integer.parseInt(ex.getMessage().split(" ")[1]);
-            System.out.println(ex.getMessage());
-
-            ResponseDTO responseDTO = new ResponseDTO(
-                    HttpStatus.valueOf(status),
-                    getMessage(status),
-                    null
-            );
-            return ResponseEntity.status(HttpStatus.valueOf(status)).body(responseDTO);
-
-        }
+    @ExceptionHandler(KakaoException.class)
+    public ResponseEntity<?> handleKakaoException(KakaoException ex){
         ResponseDTO responseDTO = new ResponseDTO(
-                HttpStatus.BAD_REQUEST,
-                "RuntimeException",
+                ex.getErrorCode().getHttpStatus(),
+                ex.getErrorCode().getCode(),
                 ex.getMessage()
+
         );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO);
+        return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(responseDTO);
 
-    }
-
-    private static String getMessage(int status) {
-        String message = "카카오 로그인 오류 발생 - ";
-        if(status == 400){
-            message += "API의 필수 파라미터 관련 오류가 발생했습니다.";
-        }
-        if(status==401){
-            message += "인증 관련 오류가 발생했습니다.";
-        }
-        if(status==403){
-            message += "권한 오류가 발생했습니다.";
-        }
-        if(status==500){
-            message += "서버 관련 시스템 오류가 발생했습니다.";
-        }
-        if(status==502){
-            message += "게이트웨이 또는 프로토콜 등의 통신 오류가 발생했습니다.";
-        }
-        if(status==503){
-            message += "서비스 점검중 입니다.";
-        }
-        return message;
     }
 
 }
