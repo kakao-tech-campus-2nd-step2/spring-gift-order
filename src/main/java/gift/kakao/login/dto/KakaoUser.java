4d8d57cec4381f27b7b648762e51f44414b786a5
dong_yxxn <g0326kdy@gmail.com>
2024-07-24 23:47:06 +0900

    refactor(KakaoUser): User와 관계 설정

--- src/main/java/gift/kakao/login/dto/KakaoUser.java
+++ src/main/java/gift/kakao/login/dto/KakaoUser.java
@@ -1,2 +1,32 @@
-package gift.kakao.login.dto;public class KakaoUser {
+package gift.kakao.login.dto;
+
+import gift.user.domain.User;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@DiscriminatorValue("kakao_user")
+public class KakaoUser extends User {
+    private String token;
+
+    public KakaoUser() {
+
+    }
+    public KakaoUser(String token) {
+        this.token = token;
+    }
+
+    public KakaoUser(String email, String password, String token) {
+        super(email, password);
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
