1cf6cf04e53ed780fdf39e821a900397349cb625
sanghee0820 <lsh277604@gmail.com>
2024-07-25 00:43:17 +0900

    feat: Kakao CallBack API추가
    callback으로 code를 받고, User정보와, AccessToken, RefreshToken을 Redis에 저장하도록함.

--- src/main/java/gift/auth/controller/KakaoOauthController.java
+++ src/main/java/gift/auth/controller/KakaoOauthController.java
@@ -1,26 +1,35 @@
 package gift.auth.controller;
 
-import gift.config.KakaoOauthConfig;
+import gift.auth.service.KakaoOAuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/oauth/kakao")
 public class KakaoOauthController {
-    private final KakaoOauthConfig kakaoOauthConfig;
+    private final KakaoOAuthService kakaoOAuthService;
 
-    public KakaoOauthController(KakaoOauthConfig kakaoOauthConfig) {
-        System.out.println(kakaoOauthConfig);
-        this.kakaoOauthConfig = kakaoOauthConfig;
+    public KakaoOauthController(KakaoOAuthService kakaoOAuthService) {
+        this.kakaoOAuthService = kakaoOAuthService;
     }
 
     @GetMapping
     public ResponseEntity<Void> getOauthURL() {
+        var kakaoLoginUrl = kakaoOAuthService.getKakaoLoginUrl();
         return ResponseEntity.status(HttpStatus.SEE_OTHER)
-                .header("location", kakaoOauthConfig.createURL())
+                .header("location", kakaoLoginUrl)
                 .build();
+    }
+
+    @GetMapping("/callback")
+    public ResponseEntity<String> callBack(@RequestParam("code") String code) {
+        kakaoOAuthService.callBack(code);
+//        kakaoOAuthService.(code);
+        return ResponseEntity.ok()
+                .body(code);
     }
 }
