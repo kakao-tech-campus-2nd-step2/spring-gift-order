477722904e3fbf9507b3b248b50578a5d34cfd12
Minju Song <101880766+mingjuu@users.noreply.github.com>
2024-07-25 03:19:05 +0900

    충남대 BE_송민주 5주차 과제 (1단계) (#111)
    * feat : 기존 코드 옮기기
    
    * fix : Map<String, Object> -> OptionRequest로 변경
    
    * refactor : DTO에 record 키워드 적용
    
    * docs : README.md 작성
    
    * feat : 토큰 발급및 회원 가입 로직

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -2,9 +2,11 @@ package gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
