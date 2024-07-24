02d080bb68a4e5391eab92229c2bdaa80e3a01e1
wvssm <neoramiregi@gmail.com>
2024-07-24 21:27:47 +0900

    feat(OptionResponse): toEntity 메서드 생성

--- src/main/java/gift/dto/response/OptionResponse.java
+++ src/main/java/gift/dto/response/OptionResponse.java
@@ -2,8 +2,12 @@ package gift.dto.response;
 
 import gift.domain.Option;
 
-public record OptionResponse(Long id, String name, int quantity) {
+public record OptionResponse(Long id, String name, int quantity, ProductResponse productResponse) {
     public static OptionResponse from(final Option option){
-        return new OptionResponse(option.getId(), option.getName(), option.getQuantity());
+        return new OptionResponse(option.getId(), option.getName(), option.getQuantity(), ProductResponse.from(option.getProduct()));
+    }
+
+    public Option toEntity(){
+        return new Option(this.id,this.name,this.quantity,productResponse.toEntity());
     }
 }
