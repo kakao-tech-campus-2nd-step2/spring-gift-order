cd48ecad2a77bb67fffe1ba236fad8c55ca72753
sanghee0820 <lsh277604@gmail.com>
2024-07-25 00:35:08 +0900

    feat: Kakao OauthConfig 세분화

--- src/main/java/gift/config/KakaoOauthConfig.java
+++ src/main/java/gift/config/KakaoOauthConfig.java
@@ -1,13 +1,37 @@
 package gift.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
 
 @ConfigurationProperties(prefix = "kakao")
 public record KakaoOauthConfig(
-        String key,
-        String url
+        String loginUrl,
+        String tokenUrl,
+        String userInfoUrl,
+        String clientId,
+        String redirection
 ) {
-    public String createURL() {
-        return url.concat(key);
+    public String createLoginUrl() {
+        return loginUrl.formatted(redirection, clientId);
+    }
+
+    public RestClient createTokenClient() {
+        return RestClient.create(tokenUrl);
+    }
+
+    public RestClient createUserInfoClient() {
+        return RestClient.create(userInfoUrl);
+    }
+
+    public MultiValueMap<String, String> createBody(String code) {
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("grant_type", "authorization_code");
+        requestBody.add("client_id", clientId);
+        requestBody.add("redirect_uri", redirection);
+        requestBody.add("code", code);
+
+        return requestBody;
     }
 }
