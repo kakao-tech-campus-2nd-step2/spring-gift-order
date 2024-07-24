3b2a5736765f5100e2998f78fb0f705f37d22405
kanguk <lm0828@naver.com>
2024-07-25 03:38:16 +0900

    fix : 카카오 유저가 인증을 제대로 못받는 에러 수정
    fix : 카카오 유저가 인증을 제대로 못받는 에러 수정
    
    카카오 유저가 인증이 되지 않던 문제를 수정한다.

--- src/main/java/gift/resolver/LoginMemberArgumentResolver.java
+++ src/main/java/gift/resolver/LoginMemberArgumentResolver.java
@@ -2,12 +2,13 @@ package gift.resolver;
 
 import gift.entity.KakaoUser;
 import gift.entity.User;
+import gift.exception.BusinessException;
+import gift.exception.ErrorCode;
 import gift.repository.KakaoUserRepository;
 import gift.repository.UserRepository;
 import gift.service.TokenService;
 import gift.util.AuthorizationHeaderProcessor;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -25,7 +26,6 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
     private final TokenService tokenService;
     private final AuthorizationHeaderProcessor authorizationHeaderProcessor;
 
-    @Autowired
     public LoginMemberArgumentResolver(UserRepository userRepository, KakaoUserRepository kakaoUserRepository, TokenService tokenService, AuthorizationHeaderProcessor authorizationHeaderProcessor) {
         this.userRepository = userRepository;
         this.kakaoUserRepository = kakaoUserRepository;
@@ -42,22 +42,35 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, org.springframework.web.bind.support.WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
         String token = authorizationHeaderProcessor.extractToken(request);
+
+        if (token == null || token.isEmpty()) {
+            throw new BusinessException(ErrorCode.INVALID_TOKEN);
+        }
+
         Map<String, String> userInfo = tokenService.extractUserInfo(token);
         String userType = userInfo.get("userType");
         String id = userInfo.get("id");
 
-        Optional<User> user;
-        if ("kakao".equals(userType)) {
-            Optional<KakaoUser> kakaoUser = kakaoUserRepository.findByKakaoId(Long.parseLong(id));
-            user = kakaoUser.map(KakaoUser::getUser);
-        } else {
-            user = userRepository.findByEmail(id);
-        }
+        Optional<User> user = findUserByIdAndType(id, userType);
 
         if (user.isEmpty()) {
-            return null;
+            throw new BusinessException(ErrorCode.USER_NOT_FOUND);
         }
 
         return user.get();
+    }
+
+    private Optional<User> findUserByIdAndType(String id, String userType) {
+        if ("kakao".equals(userType)) {
+            try {
+                Long kakaoId = Long.parseLong(id);
+                Optional<KakaoUser> kakaoUser = kakaoUserRepository.findById(kakaoId);
+                return kakaoUser.map(KakaoUser::getUser);
+            } catch (NumberFormatException e) {
+                throw new BusinessException(ErrorCode.INVALID_TOKEN, "Invalid Kakao ID format");
+            }
+        } else {
+            return userRepository.findByEmail(id);
+        }
     }
 }
