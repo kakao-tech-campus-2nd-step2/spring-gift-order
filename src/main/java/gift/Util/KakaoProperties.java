17de98f445226abbe03c91290670f44430b900b2
Woojae Jeong <woo6388@naver.com>
2024-07-25 05:16:20 +0900

    feat : 앱키, 시크릿 키 숨기기 구현

--- src/main/java/gift/Util/KakaoProperties.java
+++ src/main/java/gift/Util/KakaoProperties.java
@@ -2,7 +2,7 @@ package gift.Util;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "kakao")
 public record KakaoProperties(
         String clientId,
         String redirectUrl
