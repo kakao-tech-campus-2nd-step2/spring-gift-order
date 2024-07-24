9be12ea4f8301aa21fb4527fd840fb909f2ab644
BaeJunho <bjh4498@knu.ac.kr>
2024-07-25 07:03:23 +0900

    refactor : 명칭 변경 및 메서드 추가
    단순하게 Kakao user에 대한 처리만이 아닌, Kakao Api 사용에 대한 전반적 로직을 담당

--- src/main/java/gift/service/KakaoService.java
+++ src/main/java/gift/service/KakaoService.java
@@ -1,12 +1,14 @@
 package gift.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.DTO.Kakao.*;
 import gift.DTO.Order.OrderRequest;
 import gift.DTO.Product.ProductResponse;
 import gift.DTO.Token;
 import gift.DTO.User.UserRequest;
+import gift.DTO.User.UserResponse;
 import gift.domain.User;
 import gift.security.JwtTokenProvider;
 import gift.security.KakaoTokenProvider;
@@ -14,6 +16,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestClient;
@@ -21,24 +24,32 @@ import org.springframework.web.client.RestClient;
 import java.net.URI;
 
 @Service
-public class KakaoUserService {
+public class KakaoService {
     private final RestClient client = RestClient.builder().build();
     private final KakaoProperties kakaoProperties;
     private final KakaoTokenProvider kakaoTokenProvider;
     private final JwtTokenProvider jwtTokenProvider;
-    @Value("${kakao.loginUrl}")
-    private String loginUrl;
-    @Value("${kakao.messageUrl}")
-    private String messageUrl;
+    private final UserService userService;
+    private final String loginUrl;
+    private final String messageUrl;
+    private final String userInfoUrl;
 
-    public KakaoUserService(
+    public KakaoService(
             KakaoTokenProvider kakaoTokenProvider,
             JwtTokenProvider jwtTokenProvider,
-            KakaoProperties kakaoProperties
+            KakaoProperties kakaoProperties,
+            UserService userService,
+            @Value("${kakao.loginUrl}") String loginUrl,
+            @Value("${kakao.messageUrl}") String messageUrl,
+            @Value("${kakao.userInfoUrl}") String userInfoUrl
     ) {
         this.kakaoTokenProvider = kakaoTokenProvider;
         this.jwtTokenProvider = jwtTokenProvider;
         this.kakaoProperties = kakaoProperties;
+        this.userService = userService;
+        this.loginUrl = loginUrl;
+        this.messageUrl = messageUrl;
+        this.userInfoUrl = userInfoUrl;
     }
     /*
      * Code를 이용하여 트큰을 발급하는 로직
@@ -46,10 +57,11 @@ public class KakaoUserService {
     @Transactional
     public Token login(String code) throws JsonProcessingException {
         String token = kakaoTokenProvider.getToken(code);
-        User kakaoUser = kakaoTokenProvider.getKakaoUserInfo(token);
+        String kakaoId = getKakaoUserInfo(token);
+        UserResponse kakaoUserResponse = userService.saveKakao(kakaoId, token);
 
         return jwtTokenProvider.makeToken(new UserRequest(
-                kakaoUser.getUserId(), kakaoUser.getEmail(), kakaoUser.getPassword()
+                kakaoUserResponse.getUserId(), kakaoUserResponse.getEmail(), kakaoUserResponse.getPassword()
         ));
     }
     /*
@@ -95,13 +107,32 @@ public class KakaoUserService {
         return body;
     }
     /*
+     * 토큰을 이용하여 유저의 정보를 받아오기
+     */
+    public String getKakaoUserInfo(String access_token) throws JsonProcessingException {
+        ResponseEntity<String> entity = client.post()
+                .uri(URI.create(userInfoUrl))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + access_token)
+                .retrieve()
+                .toEntity(String.class);
+
+        String resBody = entity.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(resBody);
+        return jsonNode.get("id").asText();
+    }
+    /*
      * login을 위해 code를 받기 위한 url을 만드는 로직
      */
     public String makeLoginUrl(){
-        loginUrl += "?scope=talk_message";
-        loginUrl += "&response_type=code";
-        loginUrl += "&redirect_uri=" + kakaoProperties.getRedirectUrl();
-        loginUrl += "&client_id=" + kakaoProperties.getClientId();
-        return loginUrl;
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(loginUrl);
+        stringBuffer.append("?scope=talk_message");
+        stringBuffer.append("&response_type=code");
+        stringBuffer.append("&redirect_uri=").append(kakaoProperties.getRedirectUrl());
+        stringBuffer.append("&client_id=").append(kakaoProperties.getClientId());
+
+        return stringBuffer.toString();
     }
 }
