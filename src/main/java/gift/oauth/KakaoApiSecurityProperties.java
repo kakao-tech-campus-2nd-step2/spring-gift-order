47d6decaf71331e637a06dc5b682eb389e0f4417
dkswoals <a4445656@gmail.com>
2024-07-24 19:11:28 +0900

    feat: kakaoApiProperties 객체 구현
    보안 설정 바인딩용 객체

---
+++ src/main/java/gift/oauth/KakaoApiSecurityProperties.java
@@ -0,0 +1,63 @@
+package gift.oauth;
+
+import java.net.URI;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+@ConfigurationProperties(prefix = "kakao")
+public class KakaoApiSecurityProperties {
+
+    private final String clientId;
+
+    private final String redirectUri;
+
+    private final Uri url;
+
+    @ConstructorBinding
+    public KakaoApiSecurityProperties(String clientId, String redirectUri, Uri url) {
+        this.clientId = clientId;
+        this.redirectUri = redirectUri;
+        this.url = url;
+    }
+
+    public static class Uri {
+
+        private final String login;
+        private final String token;
+        private final String userInfo;
+
+        @ConstructorBinding
+        public Uri(String login, String token, String userInfo) {
+            this.login = login;
+            this.token = token;
+            this.userInfo = userInfo;
+        }
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public URI getLoginUri() {
+        var loginUri = ServletUriComponentsBuilder.fromUriString(url.login)
+            .queryParam("response_type", "code")
+            .queryParam("client_id", clientId)
+            .queryParam("redirect_uri", redirectUri)
+            .build().toUri();
+        return loginUri;
+    }
+
+    public URI getTokenUri() {
+        return URI.create(url.token);
+    }
+
+    public URI getUserInfoUri() {
+        return URI.create(url.userInfo);
+    }
+}
