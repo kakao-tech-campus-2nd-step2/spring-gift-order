058b42a4a58ab937ee3ce879415666394ed38f39
dong_yxxn <g0326kdy@gmail.com>
2024-07-24 15:07:49 +0900

    feat(OrderService): orderService 정의 - request -> entity 함수 정의 - entity -> response 함수 정의 - save 함수 정의 - creatorder 함수 정의

--- src/main/java/gift/order/service/OrderService.java
+++ src/main/java/gift/order/service/OrderService.java
@@ -1,2 +1,43 @@
-package gift.order.service;public class OrderService {
+package gift.order.service;
+
+import gift.option.domain.Option;
+import gift.option.repository.OptionRepository;
+import gift.order.domain.Order;
+import gift.order.domain.OrderRequest;
+import gift.order.domain.OrderResponse;
+import gift.order.repository.OrderRepository;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final OptionRepository optionRepository;
+
+    public OrderService(OrderRepository orderRepository, OptionRepository optionRepository) {
+        this.orderRepository = orderRepository;
+        this.optionRepository = optionRepository;
+    }
+
+    public OrderResponse createOrder(OrderRequest orderRequest){
+        Order order = save(convertRequestToEntity(orderRequest));
+        return convertEntityToResponse(order);
+    }
+    public Order save(Order order){
+        return orderRepository.save(order);
+    }
+    private Order convertRequestToEntity(OrderRequest orderRequest){
+        Option option = optionRepository.findById(orderRequest.optionId())
+            .orElseThrow(() -> new IllegalArgumentException("OrderService: OptionId가 없다."));
+        String orderDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+        return new Order(orderRequest.quantity(), orderDateTime, orderRequest.message(), option);
+    }
+    private OrderResponse convertEntityToResponse(Order order){
+        return new OrderResponse(order.getId(),
+                order.getOption().getId(),
+                        order.getQuantity(),
+                        order.getOrderDateTime(),
+                        order.getMessage());
+    }
 }
