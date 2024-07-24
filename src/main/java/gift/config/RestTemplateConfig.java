5eacb5bbfa264078028ee88283e862458e7ce588
GamJaDo <kim0615157@gmail.com>
2024-07-25 03:47:19 +0900

    feat (에러 헨들러, 응답 시간)

--- src/main/java/gift/config/RestTemplateConfig.java
+++ src/main/java/gift/config/RestTemplateConfig.java
@@ -2,13 +2,26 @@ package gift.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import gift.exception.RestTemplateResponseErrorHandler;
 
 @Configuration
 public class RestTemplateConfig {
 
 	@Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateResponseErrorHandler errorHandler) {
+		RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
+		restTemplate.setErrorHandler(errorHandler);
+		return restTemplate;
     }
+	
+	private ClientHttpRequestFactory clientHttpRequestFactory() {
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		factory.setConnectTimeout(5000);
+		factory.setReadTimeout(5000);
+		return factory;
+	}
 }
