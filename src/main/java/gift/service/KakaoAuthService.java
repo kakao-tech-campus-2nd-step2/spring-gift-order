3147faf56ec2c1b69fa8dee73fb150075f875e85
GamJaDo <kim0615157@gmail.com>
2024-07-25 03:48:50 +0900

    style (url을 yml에서 관리)
    메서드들을 각 기능별로 분리

--- src/main/java/gift/service/KakaoAuthService.java
+++ src/main/java/gift/service/KakaoAuthService.java
@@ -7,16 +7,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
-import gift.exception.KakaoNotEnabledException;
 
 @Service
 public class KakaoAuthService {
@@ -27,6 +23,9 @@ public class KakaoAuthService {
     @Value("${kakao.redirect-uri}")
     private String redirectUri;
     
+    @Value("${kakao.auth-url}")
+    private String authUrl;
+    
     private final RestTemplate restTemplate;
     
     public KakaoAuthService(RestTemplate restTemplate) {
@@ -34,31 +33,27 @@ public class KakaoAuthService {
     }
 	
     public Map<String, String> getAccessToken(String authorizationCode) {
-        String url = "https://kauth.kakao.com/oauth/token";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+    	RequestEntity<MultiValueMap<String, String>> request = buildAuthRequest(authorizationCode);
+    	ResponseEntity<Map<String, String>> response = ErrorHandling(request);
+        return response.getBody();
+    }
+    
+    private RequestEntity<MultiValueMap<String, String>> buildAuthRequest(String authorizationCode) {
+    	HttpHeaders headers = new HttpHeaders();
+    	MultiValueMap<String, String> body = createBody(authorizationCode);
+    	return new RequestEntity<>(body, headers, HttpMethod.POST, URI.create(authUrl));
+    }
+    
+    private MultiValueMap<String, String> createBody(String authorizationCode) {
+    	MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", clientId);
         body.add("redirect_uri", redirectUri);
         body.add("code", authorizationCode);
-
-        RequestEntity<MultiValueMap<String, String>> request = new RequestEntity<>(body, headers, HttpMethod.POST, URI.create(url));
-        ResponseEntity<Map<String, String>> response = restTemplate.exchange(
-            request,
-            new ParameterizedTypeReference<Map<String, String>>() {}
-        );
-
-        validateResponse(response);
-
-        return response.getBody();
+        return body;
     }
     
-    private void validateResponse(ResponseEntity<Map<String, String>> response) {
-        if (response.getStatusCode() == HttpStatus.FORBIDDEN) {
-            throw new KakaoNotEnabledException("Forbidden: [spring-gift] App disabled [talk_message] scopes for [TALK_MEMO_DEFAULT_SEND] API on developers.kakao.com. Enable it first.");
-        }
+    private ResponseEntity<Map<String, String>> ErrorHandling(RequestEntity<MultiValueMap<String, String>> request) {
+    	return restTemplate.exchange(request, new ParameterizedTypeReference<Map<String, String>>() {});
     }
 }
