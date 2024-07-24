6690072f5750eeb7684697e5b619eb2bb50a6fb8
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-24 15:11:03 +0900

    feat: 소셜 로그인 엔티티

--- src/main/java/gift/entity/UserEntity.java
+++ src/main/java/gift/entity/UserEntity.java
@@ -49,6 +49,11 @@ public class UserEntity {
         cascade = CascadeType.ALL, orphanRemoval = true)
     private List<WishEntity> wish;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "userEntity", fetch = FetchType.LAZY,
+        cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SocialEntity> socials;
+
     public UserEntity() {
     }
 
