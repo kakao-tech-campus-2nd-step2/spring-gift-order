10d95d6afd2d080a7d4eec07e29d72e1e8bf85a2
JJY <ddglackrp@gmail.com>
2024-07-25 01:24:42 +0900

    feat "내 서비스 Token Filter 추가"

---
+++ src/main/java/gift/filter/MyTokenFilter.java
@@ -0,0 +1,58 @@
+package gift.filter;
+
+
+import gift.domain.AuthToken;
+import gift.repository.token.TokenRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * post /login/token 요청 시 Authorization 토큰 값을 이미 가지고 있다면 /home(누구나 접근할 수 있는 페이지) 으로 리다이렉션 하기 위한 필터
+ */
+public class MyTokenFilter implements Filter {
+
+    private final TokenRepository tokenRepository;
+
+    public MyTokenFilter(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String path = httpRequest.getRequestURI();
+        if (path.equals("/home") || path.equals("/oauth/renew/kakao") || path.startsWith("/members") || path.startsWith("/login/oauth") || path.startsWith("/h2-console")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        AuthToken authToken = (AuthToken) httpRequest.getAttribute("AuthToken");
+
+        if(authToken.getAccessToken() == null){
+            if(authToken.getCreatedAt().plusSeconds(authToken.getTokenTime()).isBefore(LocalDateTime.now())){
+                tokenRepository.deleteById(authToken.getId());
+                httpResponse.sendRedirect("/home");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
