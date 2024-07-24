e536005c07c1a5cbf9c0babcb265db1097fa42ea
Geon Kim <kky8586696@naver.com>
2024-07-25 00:02:35 +0900

    refactor : 빈 주입 수정

--- src/test/java/gift/RestClientTest.java
+++ src/test/java/gift/RestClientTest.java
@@ -4,6 +4,7 @@ import gift.config.KakaoProperties;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,16 +17,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class RestClientTest {
-    private final RestClient client = RestClient.builder().build();
-
-
-    private final KakaoProperties kakaoProperties;
+    @Autowired
+    @Qualifier("customRestClient")
+    private RestClient restClient;
 
     @Autowired
-    public RestClientTest(KakaoProperties kakaoProperties) {
-        this.kakaoProperties = kakaoProperties;
-    }
-
+    private KakaoProperties kakaoProperties;
 
     @Test
     @DisplayName("액세스 토큰을 잘 받아오는지 테스트")
@@ -36,7 +33,7 @@ public class RestClientTest {
         body.add("client_id", kakaoProperties.getRestAPiKey());
         body.add("redirect_url","http://localhost:8080");
         body.add("code","ZjX1dBYc4mQufxfZKzyYdkDPGwvvUB7Tui91ZafEaCnEJEYyQxkZaQAAAAQKPXNNAAABkN8ZKY-t1856Xp2T3g");
-        var response = client.post()
+        var response = restClient.post()
                 .uri(URI.create(url))
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(body)
