5d0ca93e6b9b797580ba7e79bd6e686988f5e561
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-07-25 03:13:43 +0900

    충남대 BE_신성희 5주차 과제 (1단계) (#134)
    * feat : step1 README 작성
    
    * fix : test 폴더에 파일 잘못옮긴거 수정
    
    * refact : 파일 위치 정리
    
    * fix : test 파일 위치 정리
    
    * feat : 인가, 토큰받아오기 test 구현 -> 테스트 실행이 안되는 중..
    
    * fix : build.gradle 수정

---
+++ src/test/java/gift/MemberRepositoryTest.java
@@ -0,0 +1,21 @@
+//package gift;
+//
+//import gift.entity.Member;
+//import gift.repository.MemberRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//
+//@DataJpaTest
+//public class MemberRepositoryTest {
+//    private final MemberRepository users;
+//
+//    public MemberRepositoryTest(MemberRepository memberRepository) {
+//        users = memberRepository;
+//    }
+//
+//    @Test
+//    public void save(Member member) {
+//        users.save(member);
+//    }
+//
+//}
---
+++ src/test/java/gift/ProductRepositoryTest.java
@@ -0,0 +1,45 @@
+//package gift;
+//
+//import gift.entity.Category;
+//import gift.entity.Option;
+//import gift.entity.Product;
+//import gift.repository.CategoryRepository;
+//import gift.repository.ProductRepository;
+//import jakarta.transaction.Transactional;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@Transactional
+//@DataJpaTest
+//public class ProductRepositoryTest {
+//
+//    ProductRepository products;
+//    CategoryRepository categorys;
+//
+//    @Test
+//    void save() {
+//        Category category = categorys.findCategoryByName("식품");
+//        List<Option> options = new ArrayList<>();
+//        options.add(new Option("옵션1", 2L));
+//        final Product product = new Product("사과", 500, "naver.com", category, options);
+//        assertThat(product.getId()).isNull();
+//        var actualProduct = products.save(product);
+//        assertThat(actualProduct.getId()).isNotNull();
+//    }
+//
+//    @Test
+//    void findByName() {
+//        Category category = categorys.findCategoryByName("과일");
+//        List<Option> options = new ArrayList<>();
+//        options.add(new Option("옵션1", 2L));
+//        products.save(new Product("사과", 500, "naver.com", category, options));
+//        final Product actualProduct = products.findByName("사과");
+//        assertThat(actualProduct.getId()).isNotNull();
+//        assertThat(actualProduct.getName()).isEqualTo("사과");
+//    }
+//}
