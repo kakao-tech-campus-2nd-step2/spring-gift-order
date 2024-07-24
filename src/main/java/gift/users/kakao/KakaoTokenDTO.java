b244b2aa8c35da92c998442730e38eb7cba3c8f4
hyyyh0x <hyyyh0x@gmail.com>
2024-07-24 17:42:41 +0900

    chores: jsonNaming 추가

--- src/main/java/gift/users/kakao/KakaoTokenDTO.java
+++ src/main/java/gift/users/kakao/KakaoTokenDTO.java
@@ -1,5 +1,9 @@
 package gift.users.kakao;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoTokenDTO(String tokenType, String accessToken, int expiresIn,
                             String refreshToken, int refreshTokenExpiresIn) {
 
