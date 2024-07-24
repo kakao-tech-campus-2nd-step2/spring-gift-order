af5477b9d520f30a3f1e3b7badf9f4c1165e1fdc
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-24 12:43:37 +0900

    feat: 카카오 로그인 서비스층 requestAuthorizationCode(),getEmail() 추가

--- src/main/java/gift/service/KakaoLoginService.java
+++ src/main/java/gift/service/KakaoLoginService.java
@@ -1,6 +1,8 @@
 package gift.service;
 
 import gift.dto.response.KakaoTokenResponse;
+import gift.dto.response.KakaoUserInfoResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -12,10 +14,15 @@ public class KakaoLoginService {
     private static final String REDIRECT_URI = "http://localhost:8080";
     private static final String GRANT_TYPE = "authorization_code";
     private static final String TOKEN_REQUEST_URI = "https://kauth.kakao.com/oauth/token";
-    private static final String CLIENT_ID = "19b572e7babb2760f39ab8bfbc11a095";
-
-
+    private static final String AUTHORIZATION_CODE_REQUEST_URI = "https://kauth.kakao.com/oauth/authorize?scope=talk_message,account_email&response_type=code&redirect_uri=http://localhost:8080&client_id={%s}";
+    @Value("${clientId}")
+    private String CLIENT_ID;
     private RestClient client = RestClient.create();
+
+    MemberService memberService;
+    public KakaoLoginService(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     public KakaoTokenResponse getToken(String code) {
         MultiValueMap multiValueMap = new LinkedMultiValueMap();
@@ -33,7 +40,20 @@ public class KakaoLoginService {
     }
 
     public void requestAuthorizationCode() {
-        client.get()
-                .uri("https://kauth.kakao.com/oauth/authorize?scope=talk_message&response_type=code&redirect_uri=http://localhost:8080&client_id={%s}", CLIENT_ID);
+        System.out.println("here");
+        String retrieve = client.get()
+                .uri(AUTHORIZATION_CODE_REQUEST_URI, CLIENT_ID)
+                .retrieve()
+                .body(String.class);
+        System.out.println(retrieve);
+    }
+    public String getEmail(String token) {
+
+        KakaoUserInfoResponse body = client.get()
+                .uri("https://kapi.kakao.com/v2/user/me")
+                .header("Authorization", String.format("Bearer %s", token))
+                .retrieve()
+                .body(KakaoUserInfoResponse.class);
+        return body.kakao_account().email();
     }
 }
