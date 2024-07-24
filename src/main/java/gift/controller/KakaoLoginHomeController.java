59e44f6784b8988fa00d820b2e688becee6c9f12
Jintaek Jeong <87135698+jjt4515@users.noreply.github.com>
2024-07-24 22:43:32 +0900

    부산대 BE_정진택 5주차 과제(1단계) (#155)
    * docs: kakaoId 및 redirectUrl 설정
    
    * feat: KakaoLoginHomeController 구현
    
    * feat: kakao-login-home.html 작성
    
    * feat: KakaoLoginController 작성
    
    * feat: KakaoLoginService 작성
    
    * Update README.md
    
    * fix: filter 수정

---
+++ src/main/java/gift/controller/KakaoLoginHomeController.java
@@ -0,0 +1,22 @@
+package gift.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class KakaoLoginHomeController {
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    @GetMapping("/kakao/login/home")
+    public String kakaoLoginPage(Model model) {
+        String kakaoAuthUrl = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri;
+        model.addAttribute("kakaoAuthUrl", kakaoAuthUrl);
+        return "kakao-login-home";
+    }
+}
--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -2,14 +2,13 @@ package gift.controller;
 
 import gift.domain.Member;
 import gift.dto.request.MemberRequest;
+import gift.service.KakaoLoginService;
 import gift.service.MemberService;
 import gift.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
