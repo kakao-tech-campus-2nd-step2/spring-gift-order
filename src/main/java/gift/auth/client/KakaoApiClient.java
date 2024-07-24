d4aafc669caf75568c67aad2421005e768de06a4
Profile-exe <kang67572346@gmail.com>
2024-07-24 21:38:00 +0900

    refactor(KakaoApiClient): client 패키지로 이동

--- src/main/java/gift/auth/client/KakaoApiClient.java
+++ src/main/java/gift/auth/client/KakaoApiClient.java
@@ -1,11 +1,14 @@
-package gift.auth.controller;
+package gift.auth.client;
 
+import gift.auth.dto.DefaultTemplateResDto;
 import gift.auth.dto.KakaoProperties;
 import gift.auth.dto.KakaoTokens;
 import gift.auth.dto.KakaoUserInfo;
+import gift.auth.dto.TemplateObject;
 import java.util.Objects;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestClient;
@@ -15,7 +18,10 @@ public class KakaoApiClient {
 
     private final static String KAKAO_API_URL = "https://kauth.kakao.com/oauth/token";
     private final static String KAKAO_USER_INFO_URL = "https://kapi.kakao.com/v2/user/me";
+    private final static String KAKAO_MESSAGE_URL = "https://kapi.kakao.com/v2/api/talk/memo/default/send";
+    private final static String BASE_URL = "http://localhost:8080";
     private final static String BEARER_PREFIX = "Bearer ";
+    private final static String OBJECT_TYPE = "text";
 
     private final RestClient restClient = RestClient.builder().build();
 
@@ -28,7 +34,7 @@ public class KakaoApiClient {
     public String getAccessToken(String code) {
         LinkedMultiValueMap<String, String> body = kakaoProperties.makeBody(code);
 
-        var result = restClient.post()
+        ResponseEntity<KakaoTokens> entity = restClient.post()
                 .uri(KAKAO_API_URL)
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .accept(MediaType.APPLICATION_JSON)
@@ -36,7 +42,7 @@ public class KakaoApiClient {
                 .retrieve()
                 .toEntity(KakaoTokens.class);
 
-        return Objects.requireNonNull(result.getBody()).accessToken();
+        return Objects.requireNonNull(entity.getBody()).accessToken();
     }
 
     public KakaoUserInfo getUserInfo(String accessToken) {
