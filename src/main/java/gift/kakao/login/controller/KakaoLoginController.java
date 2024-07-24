67541294567f80e29ec212272cbe4b95556e55c3
dong_yxxn <g0326kdy@gmail.com>
2024-07-24 23:50:49 +0900

    refactor(KakaoLoginController): getUserInfo로 id받아오게 함

--- src/main/java/gift/kakao/login/controller/KakaoLoginController.java
+++ src/main/java/gift/kakao/login/controller/KakaoLoginController.java
@@ -1,6 +1,7 @@
 package gift.kakao.login.controller;
 
 import gift.kakao.login.service.KakaoLoginService;
+import gift.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KakaoLoginController {
     private final KakaoLoginService kakaoLoginService;
-    public KakaoLoginController(KakaoLoginService kakaoLoginService){
+
+    public KakaoLoginController(KakaoLoginService kakaoLoginService) {
         this.kakaoLoginService = kakaoLoginService;
     }
+
     @GetMapping("")
     public ResponseEntity<String> handleOAuthCallback(
         @RequestParam(value = "code") String code) {
-        System.out.println("token: " + code);
         String accessToken = kakaoLoginService.getAccessToken(code);
-        System.out.println("access_token: " + accessToken);
-        return new ResponseEntity<>(HttpStatus.OK);
+        String kakaoEmail = kakaoLoginService.getUserInfo(accessToken);
+        return new ResponseEntity<>(accessToken, HttpStatus.OK);
     }
 }
