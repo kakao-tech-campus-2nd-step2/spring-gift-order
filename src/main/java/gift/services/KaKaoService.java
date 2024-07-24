1858bf82c547f8828e3a17690a2a585e198062f0
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-25 05:17:06 +0900

    feat: KaKao Service 추가

---
+++ src/main/java/gift/services/KaKaoService.java
@@ -0,0 +1,97 @@
+package gift.services;
+
+import gift.classes.Exceptions.AuthException;
+import gift.dto.KaKaoUserDto;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class KaKaoService {
+
+    @Value("${kakao.client.id}")
+    private String KAKAO_CLIENT_ID;
+
+    @Value("${kakao.redirect.url}")
+    private String KAKAO_REDIRECT_URL;
+
+    private final static String KAKAO_AUTH_URI = "https://kauth.kakao.com";
+    private final static String KAKAO_API_URI = "https://kapi.kakao.com";
+
+    public String getKaKaoLogin() {
+        return KAKAO_AUTH_URI + "/oauth/authorize"
+            + "?client_id=" + KAKAO_CLIENT_ID
+            + "&redirect_uri=" + KAKAO_REDIRECT_URL
+            + "&response_type=code";
+    }
+
+    public String getKaKaoToken(String code) {
+        if (code == null) {
+            throw new AuthException("Failed to get authorization code.");
+        }
+
+        String accessToken = "";
+        String refreshToken = "";
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-type", "application/x-www-form-urlencoded");
+
+            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+            body.add("grant_type", "authorization_code");
+            body.add("client_id", KAKAO_CLIENT_ID);
+            body.add("redirect_uri", KAKAO_REDIRECT_URL);
+            body.add("code", code);
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(body, headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                KAKAO_AUTH_URI + "/oauth/token",
+                HttpMethod.POST,
+                httpEntity,
+                String.class
+            );
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObj = (JSONObject) jsonParser.parse(response.getBody());
+
+            accessToken = (String) jsonObj.get("access_token");
+            refreshToken = (String) jsonObj.get("refresh_token");
+        } catch (Exception e) {
+            throw new AuthException("API call failed");
+        }
+
+        return accessToken;
+    }
+
+    private KaKaoUserDto getUserInfoWithToken(String accessToken) throws Exception {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+            KAKAO_API_URI + "/v2/user/me",
+            HttpMethod.POST,
+            httpEntity,
+            String.class
+        );
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObj = (JSONObject) jsonParser.parse(response.getBody());
+        long id = (long) jsonObj.get("id");
+
+        return new KaKaoUserDto(id);
+    }
+}
