f42d8c88ba95b8bff1f8cee6017c7844f3e2d1d6
hyunseo <wndlthsk@naver.com>
2024-07-24 23:26:19 +0900

    feat : 카카오 로그인 컨트롤러 구현

---
+++ src/main/java/gift/controller/KakaoLoginController.java
@@ -0,0 +1,36 @@
+package gift.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import gift.domain.KakaoInfo;
+import gift.service.KakaoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kakao")
+public class KakaoLoginController {
+    private final KakaoService kakaoService;
+
+    public KakaoLoginController(KakaoService kakaoService) {
+        this.kakaoService = kakaoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<KakaoInfo> kakaoLogin(@RequestParam("code") String code) {
+        // 1. 인가 코드 받기 (code)
+
+        try {
+            // 2. 인가 코드로 토큰 발급
+            String accessToken = kakaoService.getKakaoToken(code);
+            // 3. 토큰에서 사용자 정보 가져오기
+            KakaoInfo kakaoInfo = kakaoService.getKakaoInfo(accessToken);
+
+            return ResponseEntity.ok(kakaoInfo);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
