74c923a4f1d5f9f6899313ddfecb96adbfa8c90e
Woojae Jeong <woo6388@naver.com>
2024-07-25 03:35:16 +0900

    feat : 인증 코드 및 토큰 받는 기능 구현

---
+++ src/main/java/gift/Controller/KakaoOauthController.java
@@ -0,0 +1,39 @@
+package gift.Controller;
+
+import gift.Service.KakaoLoginService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/kakao-oauth")
+public class KakaoOauthController {
+
+    private KakaoLoginService kakaoLoginService;
+
+    public KakaoOauthController(KakaoLoginService kakaoLoginService) {
+        this.kakaoLoginService = kakaoLoginService;
+    }
+
+    @GetMapping
+    public RedirectView requestLogin(){
+        URI redirectURI = kakaoLoginService.requestLogin();
+        return new RedirectView(redirectURI.toString());
+    }
+
+    @GetMapping("/oauth-code")
+    public ResponseEntity<Map<String,String>> getToken(@RequestParam ("code") String oauthCode) {
+        String accessToken = kakaoLoginService.getToken(oauthCode);
+        Map<String, String> response = new HashMap<>();
+        response.put("access_token", accessToken);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
