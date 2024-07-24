7f3ca066cd0a71aa0dd35912c7dcf911f911ecc9
dong_yxxn <g0326kdy@gmail.com>
2024-07-24 16:05:33 +0900

    refactor(OrderController): 옵션수량차감, wishlsit에서 제거함

--- src/main/java/gift/order/controller/OrderController.java
+++ src/main/java/gift/order/controller/OrderController.java
@@ -1,31 +1,42 @@
 package gift.order.controller;
 
+import gift.option.repository.OptionRepository;
+import gift.option.service.OptionService;
 import gift.order.domain.OrderRequest;
 import gift.order.domain.OrderResponse;
 import gift.order.service.OrderService;
+import gift.wish.repository.WishlistRepository;
+import gift.wish.service.WishlistService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
     private final OrderService orderService;
+    private final OptionService optionService;
 
-    public OrderController(OrderService orderService) {
+    private final WishlistService wishlistService;
+
+    public OrderController(OrderService orderService, OptionService optionService,
+        WishlistService wishlistService) {
         this.orderService = orderService;
+        this.optionService = optionService;
+        this.wishlistService = wishlistService;
     }
 
     @PostMapping
-    public ResponseEntity<OrderResponse> getOrder(@RequestBody OrderRequest orderRequest){
+    public ResponseEntity<OrderResponse> getOrder(@RequestBody OrderRequest orderRequest, @RequestParam("userId") Long userId){
         // 1. 주문 저장
         OrderResponse orderResponse = orderService.createOrder(orderRequest);
         // 2. 옵션 수량 차감, wishlist에서 제거
-
+        optionService.subtract(optionService.convertToDTO(optionService.findById(orderResponse.optionid())),
+                                orderResponse.quantity());
+        wishlistService.deleteByUserIdProductId(userId, optionService.findById(orderResponse.optionid()).getId());
         // 3. 카카오톡 메시지 api 전송
 
         // 4. response 반환
