46110c137483acc4e3a93ccac65e1cc2813d274a
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-24 15:14:11 +0900

    feat: 카카오로부터 코드, 토큰 호출

---
+++ src/main/java/gift/util/url/KakaoUrl.java
@@ -0,0 +1,8 @@
+package gift.util.url;
+
+public class KakaoUrl {
+
+    public static String getToken = "https://kauth.kakao.com/oauth/token";
+    public static String getInfo = "https://kapi.kakao.com/v1/user/access_token_info";
+    public static String redirectUrl = "/api/social/token/kakao";
+}
