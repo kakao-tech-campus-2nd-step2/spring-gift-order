cfff1d217385f4db8a94f00b895e4a7ce8f4e84c
dong_yxxn <g0326kdy@gmail.com>
2024-07-24 15:06:57 +0900

    feat(OrderController): OrderController 정의

--- src/main/java/gift/order/controller/OrderController.java
+++ src/main/java/gift/order/controller/OrderController.java
@@ -1,2 +1,35 @@
-package gift.order.controller;public class OrderController {
+package gift.order.controller;
+
+import gift.order.domain.OrderRequest;
+import gift.order.domain.OrderResponse;
+import gift.order.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> getOrder(@RequestBody OrderRequest orderRequest){
+        // 1. 주문 저장
+        OrderResponse orderResponse = orderService.createOrder(orderRequest);
+        // 2. 옵션 수량 차감, wishlist에서 제거
+
+        // 3. 카카오톡 메시지 api 전송
+
+        // 4. response 반환
+
+        return ResponseEntity.ok(orderResponse);
+    }
 }
