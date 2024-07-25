0fa4ae8171d3f2eb47567a00c8d41de45f3c8ebc
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-25 21:08:19 +0900

    feat: 로그인 완료후 마지막에 Jwt토큰 메모리 데이터베이스에 저장

--- src/main/java/gift/controller/api/KakaoLoginController.java
+++ src/main/java/gift/controller/api/KakaoLoginController.java
@@ -2,6 +2,7 @@ package gift.controller.api;
 
 import gift.dto.response.KakaoTokenResponse;
 import gift.dto.response.TokenResponse;
+import gift.repository.KakaoAccessTokenRepository;
 import gift.service.JwtTokenService;
 import gift.service.KakaoLoginService;
 import gift.service.MemberService;
@@ -16,11 +17,13 @@ public class KakaoLoginController {
     private final KakaoLoginService kakaoLoginService;
     private final MemberService memberService;
     private final JwtTokenService jwtTokenService;
+    private final KakaoAccessTokenRepository accessTokenRepository;
 
-    public KakaoLoginController(KakaoLoginService kakaoLoginService, MemberService memberService, JwtTokenService jwtTokenService) {
+    public KakaoLoginController(KakaoLoginService kakaoLoginService, MemberService memberService, JwtTokenService jwtTokenService, KakaoAccessTokenRepository accessTokenRepository) {
         this.kakaoLoginService = kakaoLoginService;
         this.memberService = memberService;
         this.jwtTokenService = jwtTokenService;
+        this.accessTokenRepository = accessTokenRepository;
     }
 
     @GetMapping("/")
@@ -31,6 +34,8 @@ public class KakaoLoginController {
 
         Long memberId = memberService.findMemberIdByEmail(email);
         TokenResponse tokenResponse = jwtTokenService.generateToken(memberId);
+        //이거 서비스층으로 변경
+        accessTokenRepository.saveAccessToken(memberId, kakaoToken.accessToken());
 
         return ResponseEntity.ok().body(tokenResponse);
     }
