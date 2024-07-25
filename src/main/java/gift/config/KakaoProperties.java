d74f55792815c7f2c8978239f81a54dff7e9337c
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-26 08:23:11 +0900

    feat : KakaoProperties 생성

---
+++ src/main/java/gift/config/KakaoProperties.java
@@ -0,0 +1,13 @@
+package gift.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@ConfigurationProperties("kakao")
+public record KakaoProperties(
+    String clientId,
+    String redirectUrl
+) {
+
+}
