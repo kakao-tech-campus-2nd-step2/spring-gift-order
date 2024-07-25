5c81cef26bea9974668a5360dca470372478850d
minju26 <mjalswn26@gmail.com>
2024-07-25 17:49:09 +0900

    test(OptionServiceTest): 주문 시 수량 차감 테스트

---
+++ src/test/java/gift/service/OptionServiceTest.java
@@ -0,0 +1,51 @@
+package gift.service;
+
+import gift.domain.Category;
+import gift.domain.Option;
+import gift.domain.Product;
+import gift.dto.request.OrderRequest;
+import gift.repository.OptionRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class OptionServiceTest {
+
+    private OptionRepository optionRepository;
+    private OptionService optionService;
+
+    @BeforeEach
+    void setUp() {
+        optionRepository = mock(OptionRepository.class);
+        optionService = new OptionService(optionRepository);
+    }
+
+    @Test
+    void order_subtractOptionQuantity() {
+        // given
+        Product product = new Product(1L, "name", 500, "image.image");
+        Category category1 = new Category(1L, "상품권");
+        Option option = new Option("optionName", 100, product);
+        int initQuantity = option.getQuantity();
+        product.setCategory(category1);
+        product.setOption(option);
+
+        OrderRequest orderRequest = new OrderRequest(1L, 9, "Please handle this order with care.");
+
+        given(optionRepository.findById(any())).willReturn(Optional.of(option));
+
+        // when
+        optionService.order(orderRequest);
+
+        // then
+        Assertions.assertThat(option.getQuantity())
+                .isEqualTo(initQuantity - orderRequest.quantity());
+    }
+
+}
