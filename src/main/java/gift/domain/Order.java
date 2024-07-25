c339ac4c30ef77e108b0ad9a1fc92f1c81731959
minju26 <mjalswn26@gmail.com>
2024-07-25 17:48:36 +0900

    feat(Order): 주문 엔티티 추가

---
+++ src/main/java/gift/domain/Order.java
@@ -0,0 +1,62 @@
+package gift.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDateTime;
+
+import static gift.constant.Message.POSITIVE_NUMBER_REQUIRED_MSG;
+
+@Entity
+@Table(name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id", nullable = false)
+    private Option option;
+
+    @Positive(message = POSITIVE_NUMBER_REQUIRED_MSG)
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private LocalDateTime orderDateTime;
+
+    @Column(nullable = false)
+    private String message;
+
+    public Order() {
+
+    }
+
+    public Order(Option option, int quantity, LocalDateTime orderDateTime, String message) {
+        this.option = option;
+        this.quantity = quantity;
+        this.orderDateTime = orderDateTime;
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Option getOption() {
+        return option;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public LocalDateTime getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
