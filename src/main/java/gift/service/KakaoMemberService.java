d43bc48b4ba6ea8b54eafc7f0fafdfbbaf7f0f7a
wvssm <neoramiregi@gmail.com>
2024-07-25 10:22:43 +0900

    feat(KakaoMemberController): getKakaoUserEmail, isKakaoTokenValid 메서드 추가

--- src/main/java/gift/service/KakaoMemberService.java
+++ src/main/java/gift/service/KakaoMemberService.java
@@ -1,32 +1,28 @@
 package gift.service;
 
-import gift.domain.KakaoMember;
-import gift.dto.response.KakaoMemberResponse;
-import gift.exception.KakaoMemberNotFoundException;
-import gift.repository.KakaoMemberRepository;
+import gift.exception.MissingAuthorizationCodeException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.Map;
 
-import static gift.constants.Messages.NOT_FOUND_KAKAO_MEMBER;
+import static gift.constants.Messages.MISSING_AUTHORIZATION_CODE;
 
 @Service
 public class KakaoMemberService {
 
     @Value("${kakao.client-id}")
     private String clientId;
-    private String redirectUri = "http://localhost:8080";
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+    private final RestTemplate restTemplate;
 
-    private final KakaoMemberRepository kakaoMemberRepository;
-
-    public KakaoMemberService(KakaoMemberRepository kakaoMemberRepository) {
-        this.kakaoMemberRepository = kakaoMemberRepository;
+    public KakaoMemberService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public String getKakaoCodeUrl() {
@@ -34,32 +30,57 @@ public class KakaoMemberService {
         return baseUrl + "?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri;
     }
 
-    public String processToken(String header) {
-        header = header.substring(7);
-        return header;
-    }
-
-    @Transactional(readOnly = true)
-    public boolean checkTokenExists(String token) {
-        return kakaoMemberRepository.existsByAccessToken(token);
-    }
-
     public String getKakaoToken(String code) {
+        if (code == null || code.isEmpty()) {
+            throw new MissingAuthorizationCodeException(MISSING_AUTHORIZATION_CODE);
+        }
+
         var url = "https://kauth.kakao.com/oauth/token";
         var headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+
         var body = new LinkedMultiValueMap<String, String>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", clientId);
         body.add("redirect_uri", redirectUri);
         body.add("code", code);
         var request = new RequestEntity<>(body, headers, HttpMethod.POST, URI.create(url));
+        var response = restTemplate.exchange(request, Map.class);
 
-        var response = new RestTemplate().exchange(request, Map.class);
         Map<String, Object> responseBody = response.getBody();
         String accessToken = (String) responseBody.get("access_token");
         System.out.println("Access Token: " + accessToken);
         return accessToken;
+    }
+
+    public String getKakaoUserEmail(String token) {
+        var url = "https://kapi.kakao.com/v2/user/me";
+
+        var headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        var request = new RequestEntity<>(headers, HttpMethod.GET, URI.create(url));
+
+        var response = restTemplate.exchange(request, Map.class);
+        Map<String, Object> responseBody = response.getBody();
+        String id = responseBody.get("id").toString();
+        String userEmail = id+"@kakao.com";
+        return userEmail;
+    }
+
+    public boolean isKakaoTokenValid(String token){
+        String url = "https://kapi.kakao.com/v1/user/access_token_info";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+
+        var request = new RequestEntity<>(headers, HttpMethod.GET, URI.create(url));
+
+        try {
+            var response = restTemplate.exchange(request, String.class);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public void sendKakaoMessage(String accessToken, String message) {
@@ -74,21 +95,9 @@ public class KakaoMemberService {
         sb.append("{\"object_type\":\"text\",\"text\":\"")
                 .append(message)
                 .append("\",\"link\":{\"web_url\":\"\"}}");
-        body.add("template_object",sb.toString());
+        body.add("template_object", sb.toString());
 
         var request = new RequestEntity<>(body, headers, HttpMethod.POST, URI.create(url));
-        var response = new RestTemplate().exchange(request, Map.class);
-    }
-
-    @Transactional
-    public void save(String token, String email) {
-        kakaoMemberRepository.save(new KakaoMember(token, email));
-    }
-
-    @Transactional(readOnly = true)
-    public KakaoMemberResponse findByAccessToken(String token){
-        return kakaoMemberRepository.findByAccessToken(token)
-                .map(KakaoMemberResponse::from)
-                .orElseThrow(()->new KakaoMemberNotFoundException(NOT_FOUND_KAKAO_MEMBER));
+        var response = restTemplate.exchange(request, Map.class);
     }
 }
