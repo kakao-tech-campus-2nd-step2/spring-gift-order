8b5494085a423d90eb69d319d7396daa49917aca
JunHyeongChoi <ajfeh22@gmail.com>
2024-07-25 14:50:29 +0900

    fix: 모든 API에서 인증이 필요하다는 표시 해결
    SwaggerConfig에서 글로벌로 적용된 인증 요구 제거

--- src/main/java/gift/config/SwaggerConfig.java
+++ src/main/java/gift/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package gift.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +14,6 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("API").version("1.0"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
