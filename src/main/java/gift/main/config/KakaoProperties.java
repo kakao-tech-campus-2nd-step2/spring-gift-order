6ccfcc6c87b1254c997058690002c8d99522806b
JinSeoHyun <15roa1121@gmail.com>
2024-07-25 17:07:28 +0900

    refactor : 직접 문자열 선언해서 사용하지 않고, 상수문자열이나 프로퍼티로 이용할 수 있게 변경

--- src/main/java/gift/main/config/KakaoProperties.java
+++ src/main/java/gift/main/config/KakaoProperties.java
@@ -7,7 +7,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "spring.kakao") //내부적으로 세터를 이용해서 등록하게 한다.
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoProperties(
-     String grantType,
+     String authorizationCode,
+     String refreshToken,
      String codeRequestUri,
      String messageRequestUri,
      String tokenRequestUri,
--- src/main/java/gift/main/service/KakaoService.java
+++ src/main/java/gift/main/service/KakaoService.java
@@ -24,6 +24,9 @@ public class KakaoService {
     private static final MediaType CONTENT_TYPE = new MediaType(APPLICATION_FORM_URLENCODED, UTF_8);
     private static final String AUTHORIZATION = "Authorization";
     private static final String BEARER = "Bearer ";
+    private static final String TEMPLATE_OBJECT = "template_object";
+    private static final String KAKAO_USER_REQUEST_LIST = "kakao_account.profile";
+
 
     private final KakaoProperties kakaoProperties;
     private final RestClient restClient;
@@ -38,26 +41,25 @@ public class KakaoService {
 
     //카카오 인가코드를 이용한 엑세스 토큰 요청하기
     public KakaoToken requestKakaoToken(String code) {
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>(); //바디 객체를 만드는 부분 Map으로 만들면 되고 key 값은 카카오문서에서 요청하는 이ㄹ름으로
-        map.add("grant_type", kakaoProperties.grantType());
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", kakaoProperties.authorizationCode());
         map.add("client_id", kakaoProperties.clientId());
         map.add("redirect_uri", kakaoProperties.redirectUri());
         map.add("code", code);
 
-        return restClient.post() //어떤 메서드로 요청할건지 , 클라이언트 생성은 간단하게 restClient = RestClient.create();로 하면돼 나는 초기화할때하는중
-                .uri(kakaoProperties.tokenRequestUri()) //요청 보낼 uri 등록하는데, 만약 요청에 쿼리 파라미터가 필요할 경우 여기다가 써야해 ?jdkfj= 이런식으로
-                .contentType(CONTENT_TYPE) //보내는 바디의 타입을 지정하는 부분인데, 이건 문서보고 어떤타입을 쓸지 고민해서 넣어바바 모르겠으면 물어보고
+        return restClient.post()
+                .uri(kakaoProperties.tokenRequestUri())
+                .contentType(CONTENT_TYPE)
                 .body(map)
-                .body("dfjk")//바디 객체를 넣는 방법
-                .retrieve() //요청을 보내고 응답을 가져오는 메서드, 실질적은 요청은 여기에서 일어남
-                .toEntity(KakaoToken.class) // 요청을 어떤 엔티티로 바꿀지 등록하는 부분이야 이 엔티티의 필드 명은 카카오가 전달해주는 이름이랑 동일해야하는데 자바는 보통 소문자대문자 하는데 JSon은 _으로 하잖아? 이거 매핑하는 방법이 여러개 있는데 일단 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class) 이거 전체 클래스명위에 붙여줘
+                .retrieve()
+                .toEntity(KakaoToken.class)
                 .getBody();
     }
 
     //카카오 엑세스 토큰을 이용한 유저정보 가져오기
     public User getKakaoProfile(KakaoToken tokenResponse) {
         KakaoProfileRequest kakaoProfileRequest = restClient.post()
-                .uri(kakaoProperties.userRequestUri() + "[\"kakao_account.profile\"]")
+                .uri(kakaoProperties.userRequestUri() + KAKAO_USER_REQUEST_LIST)
                 .contentType(CONTENT_TYPE)
                 .header(AUTHORIZATION, BEARER + tokenResponse.accessToken())
                 .retrieve()
@@ -68,7 +70,7 @@ public class KakaoService {
         return kakaoProfileRequest.convertToUser();
     }
 
-    public void SaveToken(User user, KakaoToken kakaoToken) {
+    public void saveToken(User user, KakaoToken kakaoToken) {
         Token token = new Token(user, kakaoToken);
         tokenRepository.save(token);
     }
@@ -80,7 +82,7 @@ public class KakaoService {
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_TOKEN));
 
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("grant_type", "refresh_token");
+        map.add("grant_type",kakaoProperties.refreshToken());
         map.add("client_id", kakaoProperties.clientId());
         map.add("refresh_token",token.getRefreshToken());
 
@@ -92,17 +94,18 @@ public class KakaoService {
                 .toEntity(KakaoToken.class)
                 .getBody();
 
+        assert renewToken != null;
         token.updete(renewToken);
     }
 
-    public void SendOrderMessage(OrderResponce orderResponce, UserVo userVo)  {
+    public void sendOrderMessage(OrderResponce orderResponce, UserVo userVo)  {
         //토큰을 갱신하는 부분
         renewToken(userVo);
 
         //요청바디 객체를 만드는 부분
         MultiValueMap<Object, Object> map = new LinkedMultiValueMap<>();
         String templateObjectJson = TextTemplateFactory.convertOrderResponseToTextTemplateJson(orderResponce);
-        map.set("template_object", templateObjectJson);
+        map.set(TEMPLATE_OBJECT, templateObjectJson);
 
         //요청을 위한 토큰을 만드는 부분
         Token token = tokenRepository.findByUserId(userVo.getId())
