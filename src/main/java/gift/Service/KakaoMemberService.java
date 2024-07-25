10ad2c218cc08fe31bee0d2a2dec116477dfd257
test1 <test>
2024-07-25 17:45:17 +0900

    feat(KakaoMemberSerive) : 예외처리 (관련 에러로 변경)

--- src/main/java/gift/Service/KakaoMemberService.java
+++ src/main/java/gift/Service/KakaoMemberService.java
@@ -3,12 +3,15 @@ package gift.Service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.DTO.KakaoJwtToken;
+import gift.Exception.UnauthorizedException;
 import gift.Repository.KakaoJwtTokenRepository;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestClient;
+
+;
 
 @Service
 public class KakaoMemberService {
@@ -19,8 +22,8 @@ public class KakaoMemberService {
   private static final String API_KEY = dotenv.get("API_KEY");
   private static final String URL = "https://kauth.kakao.com/oauth/token";
 
-  public KakaoMemberService(KakaoJwtTokenRepository kakaoJwtTokenRepository){
-    this.kakaoJwtTokenRepository=kakaoJwtTokenRepository;
+  public KakaoMemberService(KakaoJwtTokenRepository kakaoJwtTokenRepository) {
+    this.kakaoJwtTokenRepository = kakaoJwtTokenRepository;
   }
 
   public KakaoJwtToken getToken(String autuhorizationKey) {
@@ -49,14 +52,16 @@ public class KakaoMemberService {
       String scope = jsonNode.get("scope").asText();
       int refreshTokenExpiresIn = jsonNode.get("refresh_token_expires_in").asInt();
 
-      KakaoJwtToken kakaoJwtToken = new KakaoJwtToken(accessToken, tokenType, refreshToken, expiresIn, scope,
+      KakaoJwtToken kakaoJwtToken = new KakaoJwtToken(accessToken, tokenType, refreshToken,
+        expiresIn, scope,
         refreshTokenExpiresIn);
       kakaoJwtTokenRepository.save(kakaoJwtToken);
 
       return kakaoJwtToken;
 
     } catch (Exception e) {
-      throw new RuntimeException(e);
+      throw new UnauthorizedException(
+        "KOE320 : 동일한 인가 코드를 두 번 이상 사용하거나, 이미 만료된 인가 코드를 사용한 경우, 혹은 인가 코드를 찾을 수 없는 경우입니다.");
     }
   }
 }
