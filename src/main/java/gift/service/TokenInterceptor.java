10a7ac8dc383f107e81918c48e9f75a01c382fae
HyeJiJUN <jhj910421@gmail.com>
2024-07-25 14:26:20 +0900

    feat: change whishlist to use KakaoAuth

--- src/main/java/gift/service/TokenInterceptor.java
+++ src/main/java/gift/service/TokenInterceptor.java
@@ -15,11 +15,11 @@ import java.util.NoSuchElementException;
 
 @Component
 public class TokenInterceptor implements HandlerInterceptor {
-    private final MemberRepository memberRepository;
-
-    public TokenInterceptor(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+//    private final MemberRepository memberRepository;
+//
+//    public TokenInterceptor(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     @Value("${secret_key}")
     private String secretKey;
@@ -27,20 +27,20 @@ public class TokenInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         try{String authHeader = request.getHeader("Authorization");
-        BearerToken token = new BearerToken(authHeader);
-        request.setAttribute("bearerToken", token);
-
-        Claims claims = getClaims(token);
-        request.setAttribute("claims", claims);
-
-        Integer idInt = (Integer) claims.get("id");
-        Long memberId = idInt.longValue();
-        request.setAttribute("memberId", memberId);
-
-        Member member = getMemberByAuth(memberId);
-        request.setAttribute("member", member);
-
-        return true;
+            BearerToken token = new BearerToken(authHeader);
+            request.setAttribute("bearerToken", token);
+//
+//        Claims claims = getClaims(token);
+//        request.setAttribute("claims", claims);
+//
+//        Integer idInt = (Integer) claims.get("id");
+//        Long memberId = idInt.longValue();
+//        request.setAttribute("memberId", memberId);
+//
+//        Member member = getMemberByAuth(memberId);
+//        request.setAttribute("member", member);
+//
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
@@ -48,16 +48,16 @@ public class TokenInterceptor implements HandlerInterceptor {
         }
     }
 
-    public Claims getClaims(BearerToken token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(secretKey)
-                .parseClaimsJws(token.getToken())
-                .getBody();
-        return claims;
-    }
-
-    public Member getMemberByAuth(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoSuchElementException("해당 멤버가 없습니다."));
-    }
+//    public Claims getClaims(BearerToken token) {
+//        Claims claims = Jwts.parser()
+//                .setSigningKey(secretKey)
+//                .parseClaimsJws(token.getToken())
+//                .getBody();
+//        return claims;
+//    }
+//
+//    public Member getMemberByAuth(Long memberId) {
+//        return memberRepository.findById(memberId)
+//                .orElseThrow(() -> new NoSuchElementException("해당 멤버가 없습니다."));
+//    }
 }
