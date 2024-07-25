a93adf1d8f361b664835cfa12e1ee9ac0d86a862
sim-mer <gyu2781@gmail.com>
2024-07-25 11:45:30 +0900

    feat(oauth) - RestClient Timeout, Connection Pool 설정 추가 - 로깅을 위한 InputStream -> String 유틸 메서드 추가

--- src/main/java/gift/oauth/business/client/KakaoApiClient.java
+++ src/main/java/gift/oauth/business/client/KakaoApiClient.java
@@ -3,10 +3,16 @@ package gift.oauth.business.client;
 import gift.oauth.business.dto.OAuthParam;
 import gift.oauth.business.dto.OAuthProvider;
 import gift.oauth.business.dto.OauthToken;
+import java.io.InputStream;
+import java.util.Scanner;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -17,7 +23,27 @@ public class KakaoApiClient implements OAuthApiClient {
     private Logger log = LoggerFactory.getLogger(KakaoApiClient.class);
 
     public KakaoApiClient() {
-        this.restClient = RestClient.create();
+        this.restClient = RestClient.builder()
+            .requestFactory(getClientHttpRequestFactory())
+            .build();
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        var clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000);
+        clientHttpRequestFactory.setConnectionRequestTimeout(3000);
+
+        var connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(10);
+
+        var httpClient = HttpClientBuilder.create()
+            .setConnectionManager(connectionManager)
+            .build();
+
+        clientHttpRequestFactory.setHttpClient(httpClient);
+
+        return clientHttpRequestFactory;
     }
 
     @Override
@@ -37,6 +63,7 @@ public class KakaoApiClient implements OAuthApiClient {
             .onStatus(
                 HttpStatusCode::isError,
                 (request, response) -> {
+                    log.error("Error Response Body: {}", convert(response.getBody()));
                     throw new RuntimeException("Failed to get access token from Kakao API.");
                 }
             )
@@ -45,5 +72,10 @@ public class KakaoApiClient implements OAuthApiClient {
         log.info("Kakao access token: {}", result.access_token());
 
         return result.access_token();
+    }
+
+    private String convert(InputStream inputStream) {
+        Scanner scanner = new Scanner(inputStream, "UTF-8");
+        return scanner.useDelimiter("\\A").next();
     }
 }
