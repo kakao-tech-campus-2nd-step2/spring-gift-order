513b7380ad334e70b4055d7c89a1279a762fa8d5
Giheon Do <dgh0001@naver.com>
2024-07-25 14:52:12 +0900

    test: findByProductIdAndOptionId 테스트 추가

--- src/test/java/gift/repository/ProductOptionRepositoryTest.java
+++ src/test/java/gift/repository/ProductOptionRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -31,5 +33,33 @@ public class ProductOptionRepositoryTest {
 
         // then
         assertThat(productOptionRepository.findByProductId(product.getId()).size()).isEqualTo(0);
+    }
+
+    @Test
+    void findByProductIdAndOptionId_존재x() {
+        // given
+        Product product = productRepository.save(new Product(new ProductDTO("test", 123, "test.com", 1L)));
+        Option option = optionRepository.save(new Option(new OptionDTO("test", 123)));
+        productOptionRepository.save(new ProductOption(product, option, option.getName()));
+
+        // when
+        Optional<ProductOption> expect = productOptionRepository.findByProductIdAndOptionId(product.getId(), option.getId()+1);
+
+        // then
+        assertThat(expect).isEmpty();
+    }
+
+    @Test
+    void findByProductIdAndOptionId_존재O() {
+        // given
+        Product product = productRepository.save(new Product(new ProductDTO("test", 123, "test.com", 1L)));
+        Option option = optionRepository.save(new Option(new OptionDTO("test", 123)));
+        productOptionRepository.save(new ProductOption(product, option, option.getName()));
+
+        // when
+        Optional<ProductOption> expect = productOptionRepository.findByProductIdAndOptionId(product.getId(), option.getId());
+
+        // then
+        assertThat(expect).isPresent();
     }
 }
