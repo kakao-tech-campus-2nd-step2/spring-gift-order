a0613f682739dad676643931f62d18c1cc2b97da
JJY <ddglackrp@gmail.com>
2024-07-25 15:10:39 +0900

    feat "문자열 상수 클래스 추가"

---
+++ src/main/java/gift/utils/StringConstant.java
@@ -0,0 +1,8 @@
+package gift.utils;
+
+public class StringConstant {
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String EXPIRES_IN = "expires_in";
+    public static final String REFRESH_TOKEN = "refresh_token";
+    public static final String REFRESH_TOKEN_EXPIRES_IN = "refresh_token_expires_in";
+}
