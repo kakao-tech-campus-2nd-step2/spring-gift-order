a81f2e439e613fd7ae8ace3f66424c4fd5d648aa
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-07-26 08:38:18 +0900

    feat: step0

---
+++ src/main/java/gift/doamin/product/dto/ProductParam.java
@@ -0,0 +1,61 @@
+package gift.doamin.product.dto;
+
+import gift.doamin.product.entity.Product;
+import java.util.List;
+
+public class ProductParam {
+
+    private Long id;
+
+    private Long userId;
+
+    private Long categoryId;
+
+    private String name;
+
+    private Integer price;
+
+    private String imageUrl;
+
+    private List<OptionParam> options;
+
+    public ProductParam(Product product) {
+        this.id = product.getId();
+        this.userId = product.getUser().getId();
+        this.categoryId = product.getCategory().getId();
+        this.name = product.getName();
+        this.price = product.getPrice();
+        this.imageUrl = product.getImageUrl();
+        this.options = product.getOptions().stream()
+            .map(option -> new OptionParam(option.getId(), option.getName(), option.getQuantity()))
+            .toList();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public List<OptionParam> getOptions() {
+        return options;
+    }
+}
