48c941a34b764213001c2dc4a8699b437401a51d
young-in <hardworkingini@gmail.com>
2024-07-25 16:41:06 +0900

    refactor(KakaoAuthService) : 카카오 로그인을 통해 토큰을 받아오면서 조금 더 직관적으로 이해할 수 있도록 코드 수정

--- src/main/java/gift/service/KakaoTokenService.java
+++ src/main/java/gift/service/KakaoTokenService.java
@@ -3,6 +3,7 @@ package gift.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -17,13 +18,22 @@ public class KakaoTokenService {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        String body = String.format("grant_type=authorization_code&client_id=%s&redirect_uri=http://localhost:8080/callback&code=%s", appKey, authorizationCode);
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", appKey);
+        params.add("redirect_uri", "http://localhost:8080/callback");
+        params.add("code", authorizationCode);
 
-        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        HttpEntity<LinkedMultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<String> response = restTemplate.exchange(tokenUrl, HttpMethod.POST, request, String.class);
-        return response.getBody(); // Access Token 반환
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(tokenUrl, HttpMethod.POST, request, String.class);
+            return response.getBody(); // Access Token 반환
+        } catch (Exception e) {
+            System.err.println("엑세스토큰 처리시, 문제가 발생했습니다. " + e.getMessage());
+            return null;
+        }
     }
 }
 
