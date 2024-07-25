b27ec03e4e4d4903ed5d82dbe34f98870228781c
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-25 18:25:07 +0900

    feat: api 요청 타임아웃 설정

--- src/main/java/gift/config/RestClientConfig.java
+++ src/main/java/gift/config/RestClientConfig.java
@@ -1,15 +1,19 @@
 package gift.config;
 
+import gift.exception.OauthLoginException;
 import gift.external.api.kakao.KakaoProperties;
 import gift.external.api.kakao.client.KakaoApiClient;
 import gift.external.api.kakao.client.KakaoAuthClient;
-import gift.exception.OauthLoginException;
+import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -26,6 +30,11 @@ public class RestClientConfig {
 
     @Bean
     public KakaoApiClient kakaoApiClient(RestClient.Builder restClientBuilder) {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+            .withConnectTimeout(Duration.ofSeconds(5))
+            .withReadTimeout(Duration.ofSeconds(5));
+        ClientHttpRequestFactory requestFactory = ClientHttpRequestFactories.get(settings);
+
         RestClient restClient = restClientBuilder
             .baseUrl(kakaoProperties.apiBaseUrl())
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
@@ -33,6 +42,7 @@ public class RestClientConfig {
             .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
                 throw new OauthLoginException("error.oauth.response");
             })
+            .requestFactory(requestFactory)
             .build();
         return HttpServiceProxyFactory
             .builderFor(RestClientAdapter.create(restClient))
