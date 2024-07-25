b5f5ac984cec383ea01a92471af3d7239371df11
sanghee0820 <lsh277604@gmail.com>
2024-07-25 20:51:13 +0900

    feat: KakaoAccount DTO 추가

--- src/main/java/gift/auth/service/dto/KakaoUserInfoResponse.java
+++ src/main/java/gift/auth/service/dto/KakaoUserInfoResponse.java
@@ -1,9 +1,15 @@
 package gift.auth.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 
 public record KakaoUserInfoResponse(
         Long id,
-        LocalDateTime connected_at
+
+        @JsonProperty("connected_at")
+        LocalDateTime connectedAt,
+
+        @JsonProperty("kakao_account")
+        KakaoAcountResponse kakaoAccount
 ) {
 }
