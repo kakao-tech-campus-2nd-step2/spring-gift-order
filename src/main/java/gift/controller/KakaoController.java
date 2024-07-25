ccad182c41f20c2e179ac0108584a555e2644088
HyeJiJUN <jhj910421@gmail.com>
2024-07-25 14:28:40 +0900

    feat: order product and send kakaoMessage

--- src/main/java/gift/controller/KakaoController.java
+++ src/main/java/gift/controller/KakaoController.java
@@ -1,12 +1,11 @@
 package gift.controller;
 
+import gift.model.BearerToken;
 import gift.service.KakaoAuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class KakaoController {
@@ -35,7 +34,15 @@ public class KakaoController {
     @GetMapping("/kakaoAuth")
     public String getKakaoAuthToken(@RequestParam String code){
         String token = kakaoAuthService.getKakaoToken(code);
-        System.out.println(token);
+        System.out.println("kakoAuth return : " + token);
         return "redirect:/products";
+    }
+
+    @GetMapping("/KakaoOrder/{productId}/{optionName}/{num}")
+    @ResponseBody
+    public void orderProduct(HttpServletRequest request, @PathVariable Long productId,
+                             @PathVariable String optionName, @PathVariable int num){
+        BearerToken token = (BearerToken) request.getAttribute("bearerToken");
+        kakaoAuthService.orderProduct(token.getToken(), productId, optionName, num);
     }
 }
