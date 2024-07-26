08223394da479f3a16c437d4d1a1cc59990c66b3
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-26 21:39:52 +0900

    fix: 사용하지 않는 프로퍼티 삭제, 중복되는 메서드 병합

--- src/main/java/gift/controller/KakaoApiController.java
+++ src/main/java/gift/controller/KakaoApiController.java
@@ -2,11 +2,8 @@ package gift.controller;
 
 import gift.service.KakaoApiService;
 import gift.service.UserService;
-import gift.dto.OrderRequestDTO;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -14,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 public class KakaoApiController {
     private final KakaoApiService kakaoApiService;
     private final UserService userService;
-
-    private String accessToken;
 
     public KakaoApiController(KakaoApiService kakaoApiService, UserService userService) {
         this.kakaoApiService = kakaoApiService;
@@ -29,23 +24,11 @@ public class KakaoApiController {
 
     @GetMapping
     public String kakaoLoginVerify(@RequestParam("code") String authorizationCode) {
-        accessToken = kakaoApiService.getAccessToken(authorizationCode);
+        String accessToken = kakaoApiService.getAccessToken(authorizationCode);
         String email = kakaoApiService.getEmailFromToken(accessToken);
-        userService.kakaoSign(email);
-
-        return "redirect:/kakao/login/success";
-    }
-
-    @GetMapping("login/success")
-    public String kakaoLoginSuccess(Model model) {
-        model.addAttribute("accessToken", accessToken);
+         userService.kakaoSign(email);
 
         return "kakaoLoginSuccess";
     }
 
-    @PostMapping("/send/message")
-    public ResponseEntity<Void> sendMessage(String accessToken, @RequestBody OrderRequestDTO orderRequestDTO) {
-        kakaoApiService.sendMessage(accessToken, orderRequestDTO);
-        return ResponseEntity.ok().build();
-    }
 }
