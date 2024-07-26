58e7e1df7803455fb6e03ecceb55f5d60cab50d8
Lucerna00 <pjs3859811@naver.com>
2024-07-26 13:30:20 +0900

    feat: add process to receive kakao tokens

--- src/main/java/gift/controller/LoginController.java
+++ src/main/java/gift/controller/LoginController.java
@@ -1,21 +1,27 @@
 package gift.controller;
 
+import gift.exception.NoSuchMemberException;
+import gift.service.KakaoLoginService;
 import gift.util.KakaoProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
     private final KakaoProperties kakaoProperties;
+    private final KakaoLoginService kakaoLoginService;
 
     @Autowired
-    public LoginController(KakaoProperties kakaoProperties) {
+    public LoginController(KakaoProperties kakaoProperties, KakaoLoginService kakaoLoginService) {
         this.kakaoProperties = kakaoProperties;
+        this.kakaoLoginService = kakaoLoginService;
     }
 
     @GetMapping
@@ -23,5 +29,14 @@ public class LoginController {
         model.addAttribute("kakaoClientId", kakaoProperties.clientId());
         model.addAttribute("kakaoRedirectUri", kakaoProperties.redirectUri());
         return "login";
+    }
+
+    @RequestMapping("/oauth2/kakao")
+    public Object KakaoLogin(@RequestParam("code") String code) {
+        try {
+            return ResponseEntity.ok().body(kakaoLoginService.login(code));
+        } catch (NoSuchMemberException e) {
+            return "register";
+        }
     }
 }
