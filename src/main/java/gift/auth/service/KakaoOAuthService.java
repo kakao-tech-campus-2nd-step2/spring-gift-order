4786def3530c7283fce348b11dafd564c3ec7859
sanghee0820 <lsh277604@gmail.com>
2024-07-26 17:39:27 +0900

    feat: AccessToken 반환 기능 추가

--- src/main/java/gift/auth/service/KakaoOAuthService.java
+++ src/main/java/gift/auth/service/KakaoOAuthService.java
@@ -12,14 +12,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class KakaoOAuthService {
     private final KakaoOauthConfig kakaoOauthConfig;
-    private final OAuthAccessTokenRepository OAuthAccessTokenRepository;
-    private final OAuthRefreshTokenRepository OAuthRefreshTokenRepository;
+    private final OAuthAccessTokenRepository oAuthAccessTokenRepository;
+    private final OAuthRefreshTokenRepository oAuthRefreshTokenRepository;
 
-    public KakaoOAuthService(KakaoOauthConfig kakaoOauthConfig, OAuthAccessTokenRepository OAuthAccessTokenRepository,
+    public KakaoOAuthService(KakaoOauthConfig kakaoOauthConfig, OAuthAccessTokenRepository oAuthAccessTokenRepository,
                              OAuthRefreshTokenRepository OAuthRefreshTokenRepository) {
         this.kakaoOauthConfig = kakaoOauthConfig;
-        this.OAuthAccessTokenRepository = OAuthAccessTokenRepository;
-        this.OAuthRefreshTokenRepository = OAuthRefreshTokenRepository;
+        this.oAuthAccessTokenRepository = oAuthAccessTokenRepository;
+        this.oAuthRefreshTokenRepository = OAuthRefreshTokenRepository;
     }
 
     public String getKakaoLoginUrl() {
@@ -33,10 +33,16 @@ public class KakaoOAuthService {
         var accessToken = kakaoTokenResponse.toAccessTokenFrom(userInfo.kakaoAccount().email());
         var refreshToken = kakaoTokenResponse.toRefreshTokenFrom(userInfo.kakaoAccount().email());
 
-        OAuthAccessTokenRepository.save(accessToken);
-        OAuthRefreshTokenRepository.save(refreshToken);
+        oAuthAccessTokenRepository.save(accessToken);
+        oAuthRefreshTokenRepository.save(refreshToken);
 
         return KakaoCallbackInfo.of(userInfo.kakaoAccount().email());
+    }
+
+    public String getAccessToken(final String username) {
+        var token = oAuthAccessTokenRepository.findById(username).orElseThrow();
+
+        return token.getToken();
     }
 
     private KakaoTokenResponse getKakaoTokenResponse(final String code) {
