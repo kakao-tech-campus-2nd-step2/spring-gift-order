a0d91729899bc5f3b020bca81762a8323cb84ace
daolove0323 <daolove0323@gmail.com>
2024-07-26 15:46:20 +0900

    feat: kakao token 요청

--- src/main/java/gift/service/AuthService.java
+++ src/main/java/gift/service/AuthService.java
@@ -1,15 +1,27 @@
 package gift.service;
 
+import static gift.controller.auth.AuthMapper.toTokenRequestBody;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gift.config.KakaoConfig;
+import gift.controller.auth.KakaoToken;
+import gift.controller.auth.KakaoTokenResponse;
 import gift.controller.auth.LoginRequest;
 import gift.controller.auth.Token;
 import gift.domain.Member;
 import gift.exception.FailedHashException;
 import gift.exception.MemberNotExistsException;
 import gift.exception.PasswordNotMatchedException;
+import gift.exception.UnauthenticatedException;
 import gift.repository.MemberRepository;
 import gift.util.HashUtil;
 import gift.util.JwtUtil;
+import java.net.URI;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class AuthService {
@@ -43,4 +55,20 @@ public class AuthService {
             kakaoConfig.getKakaoAppKey(), kakaoConfig.getRedirectUri());
     }
 
+    public KakaoToken getKakaoToken(String authorizationCode) {
+        final RestClient client = RestClient.builder().build();
+        ResponseEntity<String> response;
+        JsonNode jsonNode;
+        try {
+            response = client.post().uri(URI.create(KakaoConfig.getTokenUrl()))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(toTokenRequestBody(KakaoConfig.getGrantType(), kakaoConfig.getKakaoAppKey(),
+                    kakaoConfig.getRedirectUri(), authorizationCode)).retrieve()
+                .toEntity(String.class);
+            jsonNode = new ObjectMapper().readTree(response.getBody());
+        } catch (Exception ex) {
+            throw new UnauthenticatedException("cannot get Kakao token");
+        }
+        return new KakaoToken(jsonNode.get("access_token").asText());
+    }
 }
