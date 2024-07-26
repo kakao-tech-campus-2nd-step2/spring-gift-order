d1f0c374ea95367fcaf00657138f6e21cee333d5
mingjuu <tisu79@naver.com>
2024-07-26 10:39:52 +0900

    feat : 토큰 저장을 위한 KakaoToken 엔티티 추가

--- src/main/java/gift/domain/KakaoToken.java
+++ src/main/java/gift/domain/KakaoToken.java
@@ -1,2 +1,45 @@
-package gift.domain;public class KakaoToken {
+package gift.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "kakao_tokens")
+public class KakaoToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userEmail;
+
+    @Column(nullable = false)
+    private String token;
+
+    public KakaoToken() {}
+
+    public KakaoToken(String userEmail, String token) {
+        this.userEmail = userEmail;
+        this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userId) {
+        this.userEmail = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
