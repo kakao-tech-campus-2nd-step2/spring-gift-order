36e9c3e74631e9f3c3a65e6d0d7ac1672630ab75
dkswoals <a4445656@gmail.com>
2024-07-26 10:40:57 +0900

    feat: RestClient config 구현

---
+++ src/main/java/gift/config/RestClientConfig.java
@@ -0,0 +1,31 @@
+package gift.config;
+
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${api.timeout}")
+    private int timeout;
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+            .requestFactory(getClientHttpRequestFactory())
+            .build();
+    }
+
+    public ClientHttpRequestFactory getClientHttpRequestFactory() {
+        var factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(timeout));
+        factory.setReadTimeout(Duration.ofSeconds(timeout));
+        return factory;
+    }
+
+}
