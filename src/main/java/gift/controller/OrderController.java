bc8f52be0a838839fdfcdd4887302950ea3a0de9
seoyoung-dev <seoyoung.dev@gmail.com>
2024-07-27 01:18:16 +0900

    feat: OrderController 구현

---
+++ src/main/java/gift/controller/OrderController.java
@@ -0,0 +1,31 @@
+package gift.controller;
+
+import gift.annotation.LoginMember;
+import gift.dto.OrderRequestDto;
+import gift.dto.OrderResponseDto;
+import gift.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/orders")
+@RestController
+public class OrderController {
+
+private final OrderService orderService;
+
+@Autowired
+public OrderController(OrderService orderService) {
+    this.orderService = orderService;
+}
+
+@PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(@LoginMember Long memberId, @Valid OrderRequestDto orderRequestDto) {
+    OrderResponseDto orderResponseDto = orderService.createOrder(memberId, orderRequestDto);
+    return new ResponseEntity<>(HttpStatus.CREATED);
+}
+}
