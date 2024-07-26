759792514f3f4f0b60768514157cf3665b3bbfc1
Kangboom <jkl6147@naver.com>
2024-07-26 17:08:36 +0900

    feat: add KakaoMessageException

---
+++ src/main/java/gift/kakaoApi/exceptiion/KakaoMessageException.java
@@ -0,0 +1,7 @@
+package gift.kakaoApi.exceptiion;
+
+public class KakaoMessageException extends RuntimeException{
+    public KakaoMessageException(String message) {
+        super(message);
+    }
+}
