3c88d684adb48de9e837495aa7744202494e5fe0
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-07-26 14:25:55 +0900

    docs : README 작성

--- src/main/java/gift/service/KakaoApiService.java
+++ src/main/java/gift/service/KakaoApiService.java
@@ -3,7 +3,6 @@ package gift.service;
 import java.net.URI;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
