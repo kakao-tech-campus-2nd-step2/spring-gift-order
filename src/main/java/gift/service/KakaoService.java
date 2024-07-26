501145776b5619acd225a2e757199c769957273f
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-26 15:52:49 +0900

    feat : Email 사용하여 User 저장 메서드 및 JwtToken 발급 메서드 작성

--- src/main/java/gift/service/KakaoService.java
+++ src/main/java/gift/service/KakaoService.java
@@ -3,22 +3,32 @@ package gift.service;
 import gift.config.KakaoProperties;
 import gift.dto.KakaoAccessTokenDTO;
 import gift.dto.KakaoUserInfoDTO;
+import gift.dto.MemberDTO;
+import gift.model.Member;
+import gift.repository.MemberRepository;
+import gift.util.JwtUtil;
 import java.net.URI;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestClient;
 
 @Service
+@Transactional(readOnly = true)
 public class KakaoService {
 
     private final KakaoProperties kakaoProperties;
     private final RestClient restClient = RestClient.builder().build();
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
 
-    public KakaoService(KakaoProperties kakaoProperties) {
+    public KakaoService(KakaoProperties kakaoProperties, MemberRepository memberRepository, JwtUtil jwtUtil) {
         this.kakaoProperties = kakaoProperties;
+        this.memberRepository = memberRepository;
+        this.jwtUtil = jwtUtil;
     }
 
     public String generateKakaoLoginUrl() {
@@ -59,5 +69,23 @@ public class KakaoService {
             .toEntity(KakaoUserInfoDTO.class);
         KakaoUserInfoDTO kakaoUserInfoDTO = response.getBody();
         return kakaoUserInfoDTO.kakaoAccountDTO().email();
+    }
+
+    @Transactional
+    public Member saveKakaoUser(String email) {
+        Member member = memberRepository.findByEmail(email);
+        if (member == null) {
+            String name = email.split("@")[0];
+            String password = name + name;
+            MemberDTO memberDTO = new MemberDTO(name, email, password);
+            member = new Member(null, memberDTO.name(), memberDTO.email(), memberDTO.password(), "user");
+            memberRepository.save(member);
+        }
+        return member;
+    }
+
+    public String generateToken(String email, String role) {
+        String jwtToken = jwtUtil.generateToken(email, role);
+        return jwtToken;
     }
 }
