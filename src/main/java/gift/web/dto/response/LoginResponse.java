12cfa989e567ecb7a972103e399559175f7298e9
나제법 <skwpqjq@gmail.com>
2024-07-26 16:19:09 +0900

    feat: LoginResponse.java 필드명 변경

--- src/main/java/gift/web/dto/response/LoginResponse.java
+++ src/main/java/gift/web/dto/response/LoginResponse.java
@@ -1,19 +1,18 @@
 package gift.web.dto.response;
 
-import gift.authentication.token.Token;
-
 public class LoginResponse {
 
-    private Token token;
+    private String accessToken;
 
     private LoginResponse() {
     }
 
-    public LoginResponse(Token token) {
-        this.token = token;
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
     }
 
-    public Token getToken() {
-        return token;
+    public String getAccessToken() {
+        return accessToken;
     }
+
 }
