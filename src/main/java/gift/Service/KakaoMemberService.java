43ee4f84033866b2c344db17c5bfc656266879e8
test1 <test>
2024-07-26 14:59:12 +0900

    feat : API구분

--- src/main/java/gift/Service/KakaoMemberService.java
+++ src/main/java/gift/Service/KakaoMemberService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.DTO.KakaoJwtToken;
 import gift.Exception.UnauthorizedException;
+import gift.KakaoLoginApi;
 import gift.Repository.KakaoJwtTokenRepository;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.http.MediaType;
@@ -15,13 +16,14 @@ import org.springframework.web.client.RestClient;
 public class KakaoMemberService {
 
   private KakaoJwtTokenRepository kakaoJwtTokenRepository;
-  RestClient restClient = RestClient.builder().build();
+  private KakaoLoginApi kakaoLoginApi;
   static Dotenv dotenv = Dotenv.configure().load();
   private static final String API_KEY = dotenv.get("API_KEY");
   private static final String URL = "https://kauth.kakao.com/oauth/token";
 
-  public KakaoMemberService(KakaoJwtTokenRepository kakaoJwtTokenRepository) {
+  public KakaoMemberService(KakaoJwtTokenRepository kakaoJwtTokenRepository, KakaoLoginApi kakaoLoginApi) {
     this.kakaoJwtTokenRepository = kakaoJwtTokenRepository;
+    this.kakaoLoginApi=kakaoLoginApi;
   }
 
   public KakaoJwtToken getToken(String autuhorizationKey) {
@@ -31,33 +33,9 @@ public class KakaoMemberService {
     body.add("client_id", API_KEY);
     body.add("redirect_url", "http://localhost:8080");
     body.add("code", autuhorizationKey);
-    var response = restClient.post()
-      .uri(URL)
-      .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-      .body(body)
-      .retrieve()
-      .body(String.class);
 
-    ObjectMapper objectMapper = new ObjectMapper();
-    try {
-      JsonNode jsonNode = objectMapper.readTree(response);
-      String accessToken = jsonNode.get("access_token").asText();
-      String tokenType = jsonNode.get("token_type").asText();
-      String refreshToken = jsonNode.get("refresh_token").asText();
-      int expiresIn = jsonNode.get("expires_in").asInt();
-      String scope = jsonNode.get("scope").asText();
-      int refreshTokenExpiresIn = jsonNode.get("refresh_token_expires_in").asInt();
+    KakaoJwtToken kakaoJwtToken = kakaoLoginApi.kakaoLoginApiPost(URL, body);
+    return kakaoJwtToken;
 
-      KakaoJwtToken kakaoJwtToken = new KakaoJwtToken(accessToken, tokenType, refreshToken,
-        expiresIn, scope,
-        refreshTokenExpiresIn);
-      kakaoJwtTokenRepository.save(kakaoJwtToken);
-
-      return kakaoJwtToken;
-
-    } catch (Exception e) {
-      throw new UnauthorizedException(
-        "KOE320 : 동일한 인가 코드를 두 번 이상 사용하거나, 이미 만료된 인가 코드를 사용한 경우, 혹은 인가 코드를 찾을 수 없는 경우입니다.");
-    }
   }
 }
