83b6e8a5b8d6cff736a6fbab983a2b430b68a970
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-26 21:41:51 +0900

    refactor: KakaoLoginController refactor

--- src/main/java/gift/controller/api/KakaoLoginController.java
+++ src/main/java/gift/controller/api/KakaoLoginController.java
@@ -1,9 +1,8 @@
 package gift.controller.api;
 
-import gift.client.KakaoApiClient;
-import gift.client.requestBody.KakaoTokenRequestBody;
-import gift.dto.response.KakaoTokenResponse;
 import gift.dto.response.JwtTokenResponse;
+import gift.dto.response.KakaoTokenResponse;
+import gift.service.KakaoApiService;
 import gift.service.MemberService;
 import gift.service.TokenService;
 import org.springframework.http.ResponseEntity;
@@ -14,21 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KakaoLoginController {
 
-    private final KakaoApiClient kakaoApiClient;
     private final MemberService memberService;
     private final TokenService tokenService;
+    private final KakaoApiService kakaoApiService;
 
-    public KakaoLoginController(KakaoApiClient kakaoApiClient, MemberService memberService, TokenService tokenService) {
-        this.kakaoApiClient = kakaoApiClient;
+    public KakaoLoginController(MemberService memberService, TokenService tokenService, KakaoApiService kakaoApiService) {
         this.memberService = memberService;
         this.tokenService = tokenService;
+        this.kakaoApiService = kakaoApiService;
     }
 
     @GetMapping("/")
     public ResponseEntity<JwtTokenResponse> getJwtToken(@RequestParam("code") String code) {
-        KakaoTokenResponse kakaoToken = kakaoApiClient.getKakaoToken(new KakaoTokenRequestBody(code));
+        KakaoTokenResponse kakaoToken = kakaoApiService.getKakaoToken(code);
 
-        String email = kakaoApiClient.getMemberEmail(kakaoToken.accessToken());
+        String email = kakaoApiService.getMemberEmail(kakaoToken.accessToken());
         Long memberId = memberService.findMemberIdByEmail(email);
 
         JwtTokenResponse JwtTokenResponse = tokenService.generateJwtToken(memberId);
