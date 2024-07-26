653886bbd6fc443000ea0945a13cd9c6738af022
Kiwoong Kim <gw150311@naver.com>
2024-07-26 17:04:00 +0900

    feat: 주문 시 옵션 수량 차감

--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -8,27 +8,39 @@ import gift.repository.OptionRepository;
 import gift.repository.OrderRepository;
 import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OptionRepository optionRepository;
+    private final OptionService optionService;
 
-    public OrderService(OrderRepository orderRepository, OptionRepository optionRepository) {
+    public OrderService(
+        OrderRepository orderRepository,
+        OptionRepository optionRepository,
+        OptionService optionService
+        ) {
         this.orderRepository = orderRepository;
         this.optionRepository = optionRepository;
+        this.optionService = optionService;
     }
 
+    @Transactional
     public OrderResponse order(OrderRequest orderRequest) {
         Long optionId = orderRequest.optionId();
+        Long quantity = orderRequest.quantity();
+
         Option option= optionRepository.findById(optionId)
                             .orElseThrow(() -> new RuntimeException("No such option" + optionId));
-        Order newOrder = new Order(orderRequest.quantity(),
+        Order newOrder = new Order(quantity,
                                     orderRequest.message(),
                                     LocalDateTime.now(),
                                     option);
         Order savedOrder = orderRepository.save(newOrder);
+        optionService.decrementOptionQuantity(optionId, quantity);
+
         return OrderResponse.fromEntity(savedOrder);
     }
 }
