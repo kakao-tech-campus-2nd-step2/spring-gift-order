38e50a577be16da6b4e85b08df1ccc1e1a781074
sunandrabbit <sunandrabbit@gamil.com>
2024-07-26 15:10:18 +0900

    feat : 나에게 메세지를 보내는 기능 추가

--- src/main/java/gift/service/KakaoService.java
+++ src/main/java/gift/service/KakaoService.java
@@ -1,14 +1,31 @@
 package gift.service;
 
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.auth.KakaoProperties;
 import gift.domain.KakaoProfile;
 import gift.domain.KakaoToken;
+import gift.domain.Order;
 import gift.domain.Token;
 import gift.entity.MemberEntity;
+import gift.entity.OptionEntity;
+import gift.entity.ProductEntity;
+import gift.error.NotFoundException;
+import gift.repository.OptionRepository;
+import gift.repository.ProductRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import jdk.jfr.ContentType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -18,14 +35,17 @@ public class KakaoService {
     private final static String KAKAO_GET_AUTH_CODE_URL = "https://kauth.kakao.com/oauth/authorize";
     private final static String KAKAO_GET_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
     private final static String KAKAO_GET_USER_PROFILE = "https://kapi.kakao.com/v2/user/me";
+    private final static String KAKAO_SEND_MESSAGE_TO_ME = "https://kapi.kakao.com/v2/api/talk/memo/default/send";
     private final KakaoProperties kakaoProperties;
     private final MemberService memberService;
     private final RestClient restClient;
+    private final OptionRepository optionRepository;
 
-    public KakaoService(KakaoProperties kakaoProperties, MemberService memberService) {
+    public KakaoService(KakaoProperties kakaoProperties, MemberService memberService, OptionRepository optionRepository) {
         this.kakaoProperties = kakaoProperties;
         this.memberService = memberService;
         restClient = RestClient.create();
+        this.optionRepository = optionRepository;
     }
 
     public String getCode() {
@@ -33,6 +53,7 @@ public class KakaoService {
             .queryParam("client_id", kakaoProperties.restApiKey())
             .queryParam("redirect_uri", kakaoProperties.redirectUrl())
             .queryParam("response_type", "code")
+            .queryParam("scope","talk_message")
             .toUriString();
         return codeUri;
     }
@@ -66,7 +87,61 @@ public class KakaoService {
     public Token login(String code) {
         KakaoToken kakaoToken = getToken(code);
         KakaoProfile profile = getUserProfile(kakaoToken.access_token());
-        return memberService.kakaoLogin(profile.id());
+        return memberService.kakaoLogin(profile.id(), kakaoToken.access_token());
+    }
+
+    public void sendOrderMessage(Long memberId, Order order) throws JsonProcessingException {
+        MemberEntity memberEntity = memberService.getMemberEntityById(memberId)
+            .orElseThrow(() -> new NotFoundException("Not found member"));
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("template_object", createTemplate(order));
+
+        restClient.post()
+            .uri(KAKAO_SEND_MESSAGE_TO_ME)
+            .header("Authorization", "Bearer " + memberEntity.getKakaoToken())
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(body)
+            .retrieve();
+
+        System.out.println("send Message Work!");
+
+    }
+
+    private String createTemplate(Order order) throws JsonProcessingException {
+
+        OptionEntity optionEntity = optionRepository.findById(order.getOptionId())
+            .orElseThrow(() -> new NotFoundException("Not found option"));
+        ProductEntity productEntity = optionEntity.getProductEntity();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, Object> links = new HashMap<>();
+        links.put("web_url", "http://www.daum.net");
+        links.put("mobile_web_url", "http://m.daum.net");
+
+        Map<String, Object> content = new HashMap<>();
+        content.put("title", "주문해 주셔서 감사합니다.");
+        content.put("description", productEntity.getCategoryEntity().getDescription());
+        content.put("image_url", productEntity.getImageUrl());
+        content.put("image_width", 640);
+        content.put("image_height", 640);
+        content.put("link", links);
+
+        Map<String, Object> item = new HashMap<>();
+        item.put("item", productEntity.getName());
+        item.put("item_op", productEntity.getPrice());
+
+        List<Map<String, Object>> items = List.of(item);
+
+        Map<String, Object> itemContent = new HashMap<>();
+        itemContent.put("items", items);
+
+        Map<String, Object> message = new HashMap<>();
+        message.put("object_type", "feed");
+        message.put("content", content);
+        message.put("item_content", itemContent);
+
+        return objectMapper.writeValueAsString(message);
     }
 
 }
