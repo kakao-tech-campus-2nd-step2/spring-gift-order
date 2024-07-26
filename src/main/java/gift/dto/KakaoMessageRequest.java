77d70bd050901fc4feea2215a896b1957982768c
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-26 15:07:35 +0900

    Refactor: 스네이크 케이스 고려함

--- src/main/java/gift/dto/KakaoMessageRequest.java
+++ src/main/java/gift/dto/KakaoMessageRequest.java
@@ -1,5 +1,9 @@
 package gift.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoMessageRequest(
         String objType,
         String text,
