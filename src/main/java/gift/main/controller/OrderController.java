8e3752f6f9e2398b8c5e678bef23e4bde2dc8dc0
JinSeoHyun <15roa1121@gmail.com>
2024-07-26 17:34:08 +0900

    refactor : 토큰 갱신 로직을 카카오 서비스에서 토큰 서비스로 이동

--- src/main/java/gift/main/controller/OrderController.java
+++ src/main/java/gift/main/controller/OrderController.java
@@ -5,6 +5,8 @@ import gift.main.annotation.SessionUser;
 import gift.main.dto.OrderRequest;
 import gift.main.dto.OrderResponce;
 import gift.main.dto.UserVo;
+import gift.main.entity.ApiToken;
+import gift.main.service.ApiTokenService;
 import gift.main.service.KakaoService;
 import gift.main.service.OptionService;
 import gift.main.service.OrderService;
@@ -19,19 +21,21 @@ public class OrderController {
     private final OrderService orderService;
     private final KakaoService kakaoService;
     private final OptionService optionService;
+    private final ApiTokenService apiTokenService;
 
     public OrderController(OrderService orderService,
-                           KakaoService kakaoService, OptionService optionService) {
+                           KakaoService kakaoService, OptionService optionService, ApiTokenService apiTokenService) {
         this.orderService = orderService;
         this.kakaoService = kakaoService;
         this.optionService = optionService;
+        this.apiTokenService = apiTokenService;
     }
 
     @PostMapping("/{id}")//@PathVariable
     public ResponseEntity<?> orderProduct(@PathVariable(name = "id") Long productId, @RequestBody OrderRequest orderRequest, @SessionUser UserVo sessionUserVo) throws JsonProcessingException {
         optionService.removeOptionQuantity(orderRequest.optionId(), orderRequest.quantity()); //이 부분을 바꿀까 생각 중
         OrderResponce orderResponce = orderService.orderProduct(orderRequest, sessionUserVo, productId); //상품을 주문하고, 해당 상품의 옵션을 제거해야한다.
-        kakaoService.renewToken(sessionUserVo);
+        apiTokenService.renewToken(sessionUserVo);
         kakaoService.sendOrderMessage(orderResponce, sessionUserVo);
 
         return ResponseEntity.ok(orderResponce);
