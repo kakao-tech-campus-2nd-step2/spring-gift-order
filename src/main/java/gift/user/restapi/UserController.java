b8d6830bc6816009f6007b0146bbbc12263e09d0
GyuminBae <168344532+GyuminBae@users.noreply.github.com>
2024-07-27 08:10:31 +0900

    경북대 BE_배규민 5주차 과제 (3단계) (#249)
    * docs: 인증인가 API 문서 추가
    
    * docs: 상품 관련 api 추가
    
    * docs: 사용자 관련 api 추가
    
    * docs: 위시리스트 관련 api 추가
    
    * docs: 상품 관련 api 파라미터 설명 추가
    
    * docs: 위시리스트 관련 api 파라미터 설명 추가
    
    * docs: api 에러 응답 추가
    
    ---------
    
    Co-authored-by: Gyumin Bae <bgmsound0528@gmail.com>

--- src/main/java/gift/user/restapi/UserController.java
+++ src/main/java/gift/user/restapi/UserController.java
@@ -1,11 +1,20 @@
 package gift.user.restapi;
 
+import gift.advice.ErrorResponse;
 import gift.core.domain.user.User;
 import gift.core.domain.user.UserService;
+import gift.user.restapi.dto.response.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "사용자")
 public class UserController {
     private final UserService userService;
 
@@ -15,7 +24,24 @@ public class UserController {
     }
 
     @GetMapping("/api/user/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userService.getUserById(id);
+    @Operation(summary = "사용자 조회", description = "사용자를 조회합니다.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "사용자를 조회합니다.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "사용자를 찾을 수 없습니다.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    )
+            }
+    )
+    public UserResponse getUser(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+
+        return UserResponse.of(user);
     }
 }
--- src/main/java/gift/wishes/restapi/WishesController.java
+++ src/main/java/gift/wishes/restapi/WishesController.java
@@ -7,6 +7,13 @@ import gift.core.domain.product.ProductService;
 import gift.core.domain.wishes.WishesService;
 import gift.wishes.restapi.dto.request.AddWishRequest;
 import gift.wishes.restapi.dto.response.PagedWishResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 
 @RestController
+@Tag(name = "위시리스트")
 public class WishesController {
     private final WishesService wishesService;
     private final ProductService productService;
@@ -28,6 +36,19 @@ public class WishesController {
     }
 
     @GetMapping("/api/wishes")
+    @Operation(
+            summary = "위시리스트 조회",
+            description = "위시리스트를 조회합니다.",
+            parameters = {
+                    @Parameter(name = "page", description = "페이지 번호 (기본 값 : 0)"),
+                    @Parameter(name = "size", description = "페이지 크기 (기본 값 : 10)")
+            }
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "위시리스트를 조회합니다.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PagedWishResponse.class))
+    )
     public PagedWishResponse getWishes(
             @LoggedInUser Long userId,
             @PageableDefault(size = 10) Pageable pageable
@@ -37,12 +58,48 @@ public class WishesController {
     }
 
     @PostMapping("/api/wishes")
+    @Operation(summary = "위시리스트 추가", description = "위시리스트에 상품을 추가합니다.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "위시리스트에 상품을 추가합니다."
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "상품을 찾을 수 없습니다."
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "위시리스트에 상품 추가 실패"
+                    )
+            }
+    )
     public void addWish(@LoggedInUser Long userId, @RequestBody AddWishRequest request) {
         Product product = productService.get(request.productId());
         wishesService.addProductToWishes(userId, product);
     }
 
     @DeleteMapping("/api/wishes/{productId}")
+    @Operation(
+            summary = "위시리스트 삭제",
+            description = "위시리스트에서 상품을 삭제합니다.",
+            parameters = {
+                    @Parameter(name = "productId", description = "상품 ID")
+            }
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "위시리스트에서 상품을 삭제합니다."
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "위시리스트에서 상품을 찾을 수 없습니다."
+                    )
+            }
+    )
     public void removeWish(@LoggedInUser Long userId, @PathVariable Long productId) {
         Product product = productService.get(productId);
         wishesService.removeProductFromWishes(userId, product);
