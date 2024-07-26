a35079f3b4121129c18ee8c02eecda1ae85f7e69
Awhn <ahnshwan@outlook.com>
2024-07-26 17:50:12 +0900

    fix : change repo method name

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -12,7 +12,7 @@ public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     @Query("SELECT w FROM Wishlist w where w.id BETWEEN :startId AND :endId and w.member.id = :memberId")
     List<Wishlist> findByIdAndIdAndMember_id(@Param("startId") int id1, @Param("endId") int id2, @Param("memberId") int member_id);
     int searchCount_productByMember_idAndProduct_id(int member_id, int product_id);
-    void deleteByMember_idAndMember_id(int member_id, int product_id);
+    void deleteByMember_idAndProduct_id(int member_id, int product_id);
     Optional<Wishlist> findById(int id);
     Wishlist save(Wishlist wishlist);
 }
