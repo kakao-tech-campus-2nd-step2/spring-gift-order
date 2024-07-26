02066ad00b26fc3230b0c445b69a6c98fb9fcf50
hyunseo <wndlthsk@naver.com>
2024-07-26 17:57:17 +0900

    feat : 주문하기 구현

---
+++ src/main/java/gift/service/OrderService.java
@@ -0,0 +1,45 @@
+package gift.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import gift.domain.Member;
+import gift.domain.Order;
+import gift.dto.KakaoMessageDto;
+import gift.dto.OrderRequestDto;
+import gift.dto.OrderResponseDto;
+import gift.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final KakaoService kakaoService;
+
+    public OrderService(OrderRepository orderRepository, KakaoService kakaoService) {
+        this.orderRepository = orderRepository;
+        this.kakaoService = kakaoService;
+    }
+
+    public OrderResponseDto createOrder(OrderRequestDto orderRequestDto) {
+        Order order = new Order(orderRequestDto.optionId(), orderRequestDto.quantity(), orderRequestDto.message());
+        Order savedOrder = orderRepository.save(order);
+        return OrderResponseDto.convertToDto(savedOrder);
+    }
+
+    public void sendOrderMessage(OrderRequestDto orderRequestDto, Member member) {
+        try {
+            kakaoService.sendKakaoMessageToMe(
+                member.getAccessToken(),
+                new KakaoMessageDto(
+                    "text",
+                    orderRequestDto.message(),
+                    "https://developers.kakao.com/docs/latest/ko/message/rest-api#default-template-msg-me",
+                    "https://developers.kakao.com/docs/latest/ko/message/rest-api#default-template-msg-me"
+                )
+            );
+        }
+        catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
