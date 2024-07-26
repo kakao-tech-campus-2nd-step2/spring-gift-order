766816b34743717c04778d08e595e39420a2bdec
ez23re <ez23re@gmail.com>
2024-07-26 12:48:01 +0900

    timeout, error handle 설정 추가

--- src/main/java/gift/config/AppConfig.java
+++ src/main/java/gift/config/AppConfig.java
@@ -1,15 +1,52 @@
 package gift.config;
 
+import gift.exception.ApiRequestException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 
-/** 스프링에 빈 주입 **/
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.*;
+
 @Configuration
 public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
+        restTemplate.setErrorHandler(errorHandler());
+        return restTemplate;
+    }
+
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);  // 연결 타임아웃 5초
+        factory.setReadTimeout(5000);     // 읽기 타임아웃 5초
+        return factory;
+    }
+
+    private ResponseErrorHandler errorHandler() {
+        return new DefaultResponseErrorHandler() {
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                // Custom error handling
+                switch (response.getStatusCode()) {
+                    case BAD_REQUEST:
+                        throw new ApiRequestException("Bad Request");
+                    case NOT_FOUND:
+                        throw new ApiRequestException("Not Found");
+                    case INTERNAL_SERVER_ERROR:
+                        throw new ApiRequestException("Internal Server Error");
+                    default:
+                        super.handleError(response);
+                }
+            }
+        };
     }
 }
--- src/main/java/gift/service/KakaoProperties.java
+++ src/main/java/gift/service/KakaoProperties.java
@@ -1,9 +1,11 @@
 package gift.service;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
+@ConfigurationProperties(prefix = "kakao")
 public class KakaoProperties {
 
     @Value("${kakao.client-id}")
