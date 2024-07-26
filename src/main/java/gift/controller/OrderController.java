8c032102bba664224531cc26e297580e792cba0d
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-26 14:11:58 +0900

    Refactor: 트랜잭션 보장을 위해 orderProcessingService 생성

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -4,11 +4,7 @@ import gift.dto.OrderRequest;
 import gift.dto.OrderResponse;
 import gift.model.LoginMember;
 import gift.model.Member;
-import gift.model.Order;
-import gift.model.Product;
-import gift.service.OptionService;
-import gift.service.OrderService;
-import gift.service.WishService;
+import gift.service.OrderProcessingService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,33 +13,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/orders")
 @RestController
 public class OrderController {
-    private final OrderService orderService;
-    private final WishService wishService;
-    private final OptionService optionService;
+    private final OrderProcessingService orderProcessingService;
 
-    public OrderController(OrderService orderService, WishService wishService, OptionService optionService) {
-        this.orderService = orderService;
-        this.wishService = wishService;
-        this.optionService = optionService;
+    public OrderController(OrderProcessingService orderProcessingService) {
+        this.orderProcessingService = orderProcessingService;
     }
 
     @PostMapping
     public ResponseEntity<OrderResponse> make(@RequestBody @Valid OrderRequest request, @LoginMember Member member) {
-        // 옵션 개수 차감
-        optionService.subtractQuantity(request);
+        OrderResponse response = orderProcessingService.orderProcess(request, member);
 
-        // 해당 상품이 wish list 에 있다면 제거
-        wishService.deleteWish(
-                optionService.getProductById(request).getId(),
-                member
-        );
-
-        // 주문 메시지 보냄
-        orderService.sendOrderMessage(request, member);
-
-        // 주문
-        Order order = orderService.make(request);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(order.toDto());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
