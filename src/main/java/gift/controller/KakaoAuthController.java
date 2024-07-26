5776f1719ea2dbd059d7e4b82eaa1934bf58b5ed
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-26 15:47:03 +0900

    refactor : kakao login 관련 코드 수정

--- src/main/java/gift/controller/KakaoAuthController.java
+++ src/main/java/gift/controller/KakaoAuthController.java
@@ -1,10 +1,8 @@
 package gift.controller;
 
-import gift.service.KakaoLoginService;
-import org.springframework.beans.factory.annotation.Autowired;
+import gift.service.KakaoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,22 +10,22 @@ import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/api/kakao")
-public class KakaoLoginController {
-    private KakaoLoginService kakaoLoginService;
-    public KakaoLoginController(KakaoLoginService kakaoLoginService) {
-        this.kakaoLoginService = kakaoLoginService;
+public class KakaoAuthController {
+    private KakaoService kakaoService;
+    public KakaoAuthController(KakaoService kakaoService) {
+        this.kakaoService = kakaoService;
     }
 
     @GetMapping("/login")
     public RedirectView getKakaoLoginUrl() {
         RedirectView redirectView = new RedirectView();
-        String url = kakaoLoginService.getKakaoUrl();
+        String url = kakaoService.getKakaoUrl();
         redirectView.setUrl(url);
         return redirectView;
     }
     @GetMapping("/callback")
     public ResponseEntity<?> callback(@RequestParam("code") String code) {
-        String accessToken = kakaoLoginService.getAccessToken(code);
+        String accessToken = kakaoService.getAccessToken(code);
         return ResponseEntity.ok(accessToken);
     }
 }
