98563f3e55fc9e94fd71a2d2d25fd0a47e41e277
hyyyh0x <hyyyh0x@gmail.com>
2024-07-26 16:01:56 +0900

    feat: kakaoToken DB에 저장하기

---
+++ src/main/java/gift/users/kakao/TokenRepository.java
@@ -0,0 +1,8 @@
+package gift.users.kakao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    Token findByUserIdAndSns(long userId, String sns);
+}
---
+++ src/main/java/gift/users/kakao/TokenService.java
@@ -0,0 +1,23 @@
+package gift.users.kakao;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+    private final TokenRepository tokenRepository;
+
+    public TokenService(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    public void saveToken(Long userId, KakaoTokenDTO kakaoTokenDTO){
+        Token token = new Token(userId, "kakao", kakaoTokenDTO.accessToken(),
+            kakaoTokenDTO.expiresIn(), kakaoTokenDTO.refreshToken(), kakaoTokenDTO.refreshTokenExpiresIn());
+        tokenRepository.save(token);
+    }
+
+    public String findToken(long userId, String sns){
+        return tokenRepository.findByUserIdAndSns(userId, sns).getAccessToken();
+    }
+}
