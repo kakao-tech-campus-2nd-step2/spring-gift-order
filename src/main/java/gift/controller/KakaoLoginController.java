d67db5a28bc065a92c178df0f1de735532ddd2ed
Junhyeong <61654061+cliant@users.noreply.github.com>
2024-07-26 14:20:57 +0900

    전남대 BE_박준형 5주차 과제(step1) (#110)
    * docs : README 작성
    
    * feat : 로그인 페이지 추가
    
    * feat : 인가 코드 받기 기능 추가
    
    * feat : 토큰 받기 기능 추가
    
    * chore : 코드정리
    
    * chore : 인카 코드 받기 요청에 scope 추가
    
    * chore : 예외처리 수정
    
    * chore : 불필요한 import 삭제
    
    * feat : 사용자 정보 가져오기 기능 추가
    
    * chore : 예외처리 추가
    
    * chore : .gitignore 추가
    
    * feat : 통신 실패에 대한 예외 처리 추가
    
    * chore : 예외 처리 세부화
    
    ---------
    
    Co-authored-by: 박준형 <jun@bagjunhyeong-ui-MacBookAir.local>

---
+++ src/main/java/gift/controller/KakaoLoginController.java
@@ -0,0 +1,44 @@
+package gift.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import gift.dto.KakaoProperties;
+import gift.dto.response.KakaoTokenResponse;
+import gift.dto.response.KakaoUserInfoResponse;
+import gift.service.KakaoApiService;
+
+@Controller
+public class KakaoLoginController {
+
+    private KakaoProperties kakaoProperties;
+    private KakaoApiService kakaoApiService;
+
+    public KakaoLoginController(KakaoProperties kakaoProperties, KakaoApiService kakaoApiService){
+        this.kakaoProperties = kakaoProperties;
+        this.kakaoApiService = kakaoApiService;
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("kakaoApiKey", kakaoProperties.getApiKey());
+        model.addAttribute("redirectUri", kakaoProperties.getRedirectUri());
+        return "login";
+    }
+
+    @RequestMapping("/login/code")
+    public ResponseEntity<String> kakaoLogin(@RequestParam String code) {
+
+        KakaoTokenResponse response = kakaoApiService.getToken(code);
+
+        KakaoUserInfoResponse kakaoUserInfoResponse = kakaoApiService.getUserInfo(response.getAccessToken());
+        
+        return new ResponseEntity<>(response.getAccessToken(), HttpStatus.OK);
+    }
+    
+}
---
+++ src/main/java/gift/service/KakaoApiService.java
@@ -0,0 +1,127 @@
+package gift.service;
+
+import java.net.URI;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestClient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gift.dto.KakaoProperties;
+import gift.dto.response.KakaoTokenResponse;
+import gift.dto.response.KakaoUserInfoResponse;
+import gift.exception.CustomException;
+
+@Service
+public class KakaoApiService {
+
+    private final RestClient client;
+    private final KakaoProperties kakaoProperties;
+    private final ObjectMapper objectMapper;
+
+
+    public KakaoApiService(KakaoProperties kakaoProperties, ObjectMapper objectMapper){
+        this.kakaoProperties = kakaoProperties;
+        this.objectMapper = objectMapper;
+
+        // HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        // factory.setConnectTimeout(5000);
+
+        this.client = RestClient
+                .builder()
+                .build();
+       
+    }
+
+    public KakaoTokenResponse getToken(String code){
+        
+        var url = "https://kauth.kakao.com/oauth/token";
+        var body = createBody(code);
+
+        try {
+
+            ResponseEntity<String> response = client.post()
+                .uri(URI.create(url))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(body)
+                .retrieve()
+                .toEntity(String.class);
+                
+            if(response.getStatusCode().is2xxSuccessful()){
+
+                String jsonBody = response.getBody();
+                return objectMapper.readValue(jsonBody, KakaoTokenResponse.class);
+
+            } else if(response.getStatusCode().is4xxClientError()){
+                throw new CustomException("Bad Request " + response.getBody(), HttpStatus.valueOf(response.getStatusCode().value()));
+            } else if(response.getStatusCode().is5xxServerError()){
+                throw new CustomException("System error " + response.getBody(), HttpStatus.valueOf(response.getStatusCode().value()));
+            } else {
+                throw new CustomException("Unexpected response error " + response.getBody(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (CustomException e) {
+            throw new CustomException(e.getMessage(), e.getHttpStatus());
+        } catch (JsonProcessingException e){
+            throw new CustomException("Error parsing token response", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            throw new CustomException("Unexpected error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public KakaoUserInfoResponse getUserInfo(String accessToken){
+
+        var url = "https://kapi.kakao.com/v2/user/me";
+        
+        try {
+            ResponseEntity<String> response = client.post()
+                    .uri(URI.create(url))
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .header("Authorization", "Bearer " + accessToken)
+                    .retrieve()
+                    .toEntity(String.class);
+
+            if(response.getStatusCode().is2xxSuccessful()){
+
+                String jsonBody = response.getBody();
+                return objectMapper.readValue(jsonBody, KakaoUserInfoResponse.class);
+
+            } else if(response.getStatusCode().is4xxClientError()){
+                throw new CustomException("Bad Request " + response.getBody(), HttpStatus.valueOf(response.getStatusCode().value()));
+            } else if(response.getStatusCode().is5xxServerError()){
+                throw new CustomException("System error " + response.getBody(), HttpStatus.valueOf(response.getStatusCode().value()));
+            } else {
+                throw new CustomException("Unexpected response error " + response.getBody(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (CustomException e) {
+            throw new CustomException(e.getMessage(), e.getHttpStatus());
+        } catch (JsonProcessingException e){
+            throw new CustomException("Error parsing token response", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            throw new CustomException("Unexpected error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    private LinkedMultiValueMap<String, String> createBody(String code) {
+        var body = new LinkedMultiValueMap<String, String>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", kakaoProperties.getApiKey());
+        body.add("redirect_url", kakaoProperties.getRedirectUri());
+        body.add("code", code);
+        return body;
+
+    }
+
+    // private ClientHttpRequestFactory getClientHttpRequestFactory() {
+    //     HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+    //     clientHttpRequestFactory.setConnectTimeout(100);
+    //     clientHttpRequestFactory.setConnectionRequestTimeout(70);
+    //     return clientHttpRequestFactory;
+    // }
+}
