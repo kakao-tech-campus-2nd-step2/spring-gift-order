0ae10f7c0335fb9a79bc2b682918ac7d03a3ebfa
peacefullyquietly <pjh73015@gmail.com>
2024-07-27 01:00:09 +0900

    refactor : 상품 주문 후 메시지 전송

--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -1,5 +1,6 @@
 package gift.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import gift.domain.OrderDTO;
 import gift.entity.OptionEntity;
 import gift.entity.OrderEntity;
@@ -14,19 +15,19 @@ public class OrderService {
     private final OptionService optionService;
     private final OrderRepository orderRepository;
     private final WishListService wishListService;
-//    private final KakaoApiService kakaoApiService;
+    private final KakaoUserService kakaoUserService;
 
     @Autowired
     public OrderService(OptionService optionService, OrderRepository orderRepository,
-        WishListService wishListService) {
+        WishListService wishListService, KakaoUserService kakaoUserService) {
         this.optionService = optionService;
         this.orderRepository = orderRepository;
         this.wishListService = wishListService;
-//        this.kakaoApiService = kakaoApiService;
+        this.kakaoUserService = kakaoUserService;
     }
 
     @Transactional
-    public void createOrder(OrderDTO order, Long userId, String email) {
+    public void createOrder(OrderDTO order, Long userId, String email, String accessToken) {
         Long product_id = optionService.getProductIdFromOption(order.getOptionId());
         OptionEntity option = optionService.readProductOption(product_id, order.getOptionId());
 
@@ -48,6 +49,11 @@ public class OrderService {
 
         orderRepository.save(orderEntity);
 
-//        kakaoApiService.sendOrderMessage(email,orderEntity);
+
+        try {
+            kakaoUserService.sendOrderMessage(accessToken, order);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("카카오 메시지 전송 실패: " + e.getMessage(), e);
+        }
     }
 }
