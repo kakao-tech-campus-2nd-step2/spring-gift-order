6f2c4b10b536bc97019f4a522c526817c5bf82c6
나제법 <skwpqjq@gmail.com>
2024-07-26 16:22:18 +0900

    feat: Member.java Password 필드 삭제

--- src/main/java/gift/domain/Member.java
+++ src/main/java/gift/domain/Member.java
@@ -2,8 +2,6 @@ package gift.domain;
 
 import gift.domain.base.BaseTimeEntity;
 import gift.domain.vo.Email;
-import gift.domain.vo.Password;
-import gift.web.validation.exception.client.IncorrectPasswordException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -14,9 +12,6 @@ public class Member extends BaseTimeEntity {
     @Embedded
     private Email email;
 
-    @Embedded
-    private Password password;
-
     @Column(nullable = false)
     private String name;
 
@@ -26,16 +21,10 @@ public class Member extends BaseTimeEntity {
     public static class Builder extends BaseTimeEntity.Builder<Builder> {
 
         private Email email;
-        private Password password;
         private String name;
 
         public Builder email(Email email) {
             this.email = email;
-            return this;
-        }
-
-        public Builder password(Password password) {
-            this.password = password;
             return this;
         }
 
@@ -58,7 +47,6 @@ public class Member extends BaseTimeEntity {
     private Member(Builder builder) {
         super(builder);
         email = builder.email;
-        password = builder.password;
         name = builder.name;
     }
 
@@ -66,17 +54,7 @@ public class Member extends BaseTimeEntity {
         return email;
     }
 
-    public Password getPassword() {
-        return password;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void matchPassword(String password) {
-        if (!this.password.matches(password)) {
-            throw new IncorrectPasswordException();
-        }
     }
 }
