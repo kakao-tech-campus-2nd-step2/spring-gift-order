b260eae92c46e0f060db75543f5911f2e55d2698
BOMIN LYU <rbm0524@naver.com>
2024-07-26 17:27:44 +0900

    refactor : subtractOption이 List를 받도록 수정

--- src/main/java/gift/Service/OptionService.java
+++ src/main/java/gift/Service/OptionService.java
@@ -4,6 +4,7 @@ import gift.Entity.Option;
 import gift.Mapper.Mapper;
 import gift.Model.OptionDto;
 import gift.Model.OrderRequestDto;
+import gift.Model.WishlistDto;
 import gift.Repository.OptionJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -54,5 +55,14 @@ public class OptionService {
         Option option = optionJpaRepository.findById(orderRequestDto.getOptionId()).orElseThrow(); // 기존 Option 찾기
         option.setQuantity(option.getQuantity() - orderRequestDto.getQuantity()); //기존 수량에서 주문 수량을 빼준다.
         optionJpaRepository.save(option); // 뺀 이후 update
+    }
+
+    public void subtractOption(List<OrderRequestDto> orderRequestDtoList) {
+        for (OrderRequestDto orderRequestDto : orderRequestDtoList) {
+            if(orderRequestDto.getQuantity() <= 0) continue; // 주문 수량이 0 이하인 경우 continue (다음 주문으로 넘어감
+            Option option = optionJpaRepository.findById(orderRequestDto.getOptionId()).orElseThrow(() -> new RuntimeException("Option not found"));
+            option.setQuantity(option.getQuantity() - orderRequestDto.getQuantity());
+            optionJpaRepository.save(option);
+        }
     }
 }
