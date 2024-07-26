ff63b57eaa9a5473344980f4aa0c37a8bf262d1e
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-26 18:53:30 +0900

    feat: 옵션 추가 메서드 생성

--- src/main/java/gift/controller/KakaoWishController.java
+++ src/main/java/gift/controller/KakaoWishController.java
@@ -1,16 +1,15 @@
 package gift.controller;
 
 import gift.annotation.KakaoUser;
-import gift.annotation.LoginUser;
-import gift.dto.KakaoUserDTO;
-import gift.dto.OrderRequestDTO;
-import gift.dto.WishPageResponseDTO;
-import gift.dto.WishRequestDTO;
-import gift.model.User;
+import gift.dto.*;
+
 import gift.service.KakaoApiService;
+import gift.service.OptionService;
 import gift.service.WishService;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,11 +19,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/kakao/wishes")
 public class KakaoWishController {
     private final WishService wishService;
+    private final OptionService optionService;
     private final KakaoApiService kakaoApiService;
 
-    public KakaoWishController(WishService wishService, KakaoApiService kakaoApiService) {
+    public KakaoWishController(WishService wishService, OptionService optionService, KakaoApiService kakaoApiService) {
         this.wishService = wishService;
         this.kakaoApiService = kakaoApiService;
+        this.optionService = optionService;
     }
 
     @GetMapping
@@ -35,6 +36,21 @@ public class KakaoWishController {
         return "kakaoWishlist";
     }
 
+    @GetMapping("/addWishOption")
+    public String addWishOptionPage(@KakaoUser KakaoUserDTO kakaoUserDTO, Model model, @PageableDefault(size = 3) Pageable pageable) {
+        OptionsPageResponseDTO options = optionService.getAllOptions(pageable);
+        model.addAttribute("options", options);
+
+        return "addKakaoWishOption"; // addWishProduct.html로 이동
+    }
+
+    @PostMapping("/addWishOption")
+    public ResponseEntity<String> addWishOption(@KakaoUser KakaoUserDTO kakaoUserDTO, @RequestBody WishRequestDTO wishRequestDTO, Model model) {
+        wishService.addWishOption(kakaoUserDTO.user().getId(), wishRequestDTO);
+
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+
     @PostMapping("/send/message")
     public ResponseEntity<Void> sendMessage(@KakaoUser KakaoUserDTO kakaoUserDTO, @RequestBody OrderRequestDTO orderRequestDTO) {
         kakaoApiService.sendMessage(kakaoUserDTO.accessToken(), orderRequestDTO);
@@ -42,10 +58,10 @@ public class KakaoWishController {
     }
 
     @DeleteMapping
-    public String deleteWishProduct(@LoginUser User user, @RequestBody WishRequestDTO wishRequestDTO, Model model, @PageableDefault(size = 3) Pageable pageable) {
-        wishService.deleteWishProduct(user.getId(), wishRequestDTO.optionId());
+    public String deleteWishOption(@KakaoUser KakaoUserDTO kakaoUserDTO, @RequestBody WishRequestDTO wishRequestDTO, Model model, @PageableDefault(size = 3) Pageable pageable) {
+        wishService.deleteWishOption(kakaoUserDTO.user().getId(), wishRequestDTO.optionId());
 
-        WishPageResponseDTO wishOptions = wishService.getWishlist(user.getId(), pageable);
+        WishPageResponseDTO wishOptions = wishService.getWishlist(kakaoUserDTO.user().getId(), pageable);
         model.addAttribute("wishOptions", wishOptions);
 
         return "kakaoWishlist";
