048c53480de9718d56c547b0f2d23e25acbc1a7a
sseooh <sseooh@gmail.com>
2024-07-26 09:30:31 +0900

    test: kakao login test

---
+++ src/test/java/gift/KakaoControllerTest.java
@@ -0,0 +1,75 @@
+package gift;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gift.dto.TokenResponse;
+import gift.service.KakaoLoginService;
+import gift.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class KakaoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MemberService memberService;
+
+    @MockBean
+    private KakaoLoginService kakaoLoginService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Map<String, Object> userInfo;
+
+    @BeforeEach
+    void setUp() {
+        userInfo = new HashMap<>();
+        userInfo.put("id", "12345");
+        userInfo.put("kakao_account", new HashMap<String, Object>());
+    }
+
+    @Test
+    @DisplayName("카카오 로그인 테스트")
+    void testKakaoLogin() throws Exception {
+        String code = "authorization_code";
+        String accessToken = "access_token";
+        String email = "kakao_12345@kakao.com";
+        String token = "jwt_token";
+
+        Mockito.when(kakaoLoginService.getAccessToken(code)).thenReturn(accessToken);
+        Mockito.when(kakaoLoginService.getUserInfo(accessToken)).thenReturn(userInfo);
+        Mockito.when(memberService.findByEmail(email)).thenReturn(null);
+        Mockito.when(memberService.generateTemporaryPassword()).thenReturn("temporary_password");
+        Mockito.when(memberService.generateToken(Mockito.any())).thenReturn(token);
+
+        mockMvc.perform(post("/kakao/login")
+                        .param("code", code)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(new TokenResponse(token))));
+    }
+
+}
