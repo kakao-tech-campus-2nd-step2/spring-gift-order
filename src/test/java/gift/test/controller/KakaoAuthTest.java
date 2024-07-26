d16a86ba83b61641345e231a04b87c1a302ed50b
GamJaDo <kim0615157@gmail.com>
2024-07-27 04:16:05 +0900

    test (다른 test와 일관성 있게 리팩토링)

--- src/test/java/gift/test/controller/KakaoAuthTest.java
+++ src/test/java/gift/test/controller/KakaoAuthTest.java
@@ -1,37 +1,29 @@
 package gift.test.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.ResponseEntity;
 
 import gift.controller.KakaoAuthController;
 import gift.service.KakaoAuthService;
 
-@WebMvcTest(KakaoAuthController.class)
 public class KakaoAuthTest {
 
-	private final MockMvc mockMvc;
+	@Mock
+	private KakaoAuthService kakaoAuthService;
 	
-	@Autowired
-	public KakaoAuthTest(MockMvc mockMvc) {
-		this.mockMvc = mockMvc;
-	}
-	
-	@MockBean
-    private KakaoAuthService kakaoAuthService;
+	@InjectMocks
+    private KakaoAuthController KakaoAuthController;
 	
 	@BeforeEach
 	public void setUp() {
@@ -39,16 +31,16 @@ public class KakaoAuthTest {
 	}
 	
 	@Test
-    public void testKakaoRedirect() throws Exception {
+    public void testKakaoRedirect() {
         String authorizationCode = "dummyAuthorizationCode";
-        Map<String, String> accessTokenResponse = new HashMap<>();
-        accessTokenResponse.put("access_token", "dummy_access_token");
+        Map<String, String> accssTokenResponse = new HashMap<>();
+        accssTokenResponse.put("access_token", "dummy_access_token");
         
-        when(kakaoAuthService.getAccessToken(anyString())).thenReturn(accessTokenResponse);
-
-        mockMvc.perform(get("/kakao/redirect")
-        		.param("code", authorizationCode))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.access_token").value("dummy_access_token"));
+        when(kakaoAuthService.getAccessToken(anyString())).thenReturn(accssTokenResponse);
+        
+        ResponseEntity<Map<String, String>> response = KakaoAuthController.kakaoRedirect(authorizationCode);
+        
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        assertThat(response.getBody().get("access_token")).isEqualTo("dummy_access_token");
     }
 }
