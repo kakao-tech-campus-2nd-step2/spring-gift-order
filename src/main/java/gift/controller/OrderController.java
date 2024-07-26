743225d846209cdfc22ebd9e0d749d6ba6cf24ec
안원모 <tommy45231@naver.com>
2024-07-26 16:23:01 +0900

    Feat : OrderController 생성

---
+++ src/main/java/gift/controller/OrderController.java
@@ -0,0 +1,29 @@
+package gift.controller;
+
+import gift.domain.Member;
+import gift.dto.OrderDTO;
+import gift.service.OrderService;
+import gift.util.LoginMember;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> createOrder(@LoginMember Member member, @RequestBody OrderRequest orderRequest) {
+        OrderDTO orderDTO = orderService.createOrder(member.getId(), orderRequest.getOptionId(),
+            orderRequest.getQuantity(), orderRequest.getMessage());
+        return ResponseEntity.status(201).body(orderDTO);
+    }
+}
