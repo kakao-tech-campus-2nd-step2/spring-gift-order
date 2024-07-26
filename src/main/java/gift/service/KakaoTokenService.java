487793144b7d5b1f783731ae3c2132bd3f3b760c
hjinshin <gudwls818@gmail.com>
2024-07-26 20:24:28 +0900

    refactor: CrudRepository로 Redis Repository 구현

--- src/main/java/gift/service/KakaoTokenService.java
+++ src/main/java/gift/service/KakaoTokenService.java
@@ -1,43 +1,48 @@
 package gift.service;
 
 import gift.common.exception.AuthenticationException;
-import gift.repository.KakaoTokenRepository;
+import gift.model.KakaoAccessToken;
+import gift.model.KakaoRefreshToken;
+import gift.repository.AccessTokenRepository;
+import gift.repository.RefreshTokenRepository;
 import gift.service.dto.KakaoTokenDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KakaoTokenService {
 
-    private final KakaoTokenRepository kakaoTokenRepository;
+    private final AccessTokenRepository accessTokenRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
     private final KakaoApiCaller kakaoApiCaller;
 
-    public KakaoTokenService(KakaoTokenRepository kakaoTokenRepository, KakaoApiCaller kakaoApiCaller) {
-        this.kakaoTokenRepository = kakaoTokenRepository;
+    public KakaoTokenService(AccessTokenRepository accessTokenRepository, RefreshTokenRepository refreshTokenRepository, KakaoApiCaller kakaoApiCaller) {
+        this.accessTokenRepository = accessTokenRepository;
+        this.refreshTokenRepository = refreshTokenRepository;
         this.kakaoApiCaller = kakaoApiCaller;
     }
 
     public void saveToken(Long memberId, KakaoTokenDto kakaoTokenDto) {
-        kakaoTokenRepository.saveAccessToken(memberId, kakaoTokenDto);
-        kakaoTokenRepository.saveRefreshToken(memberId, kakaoTokenDto);
+        accessTokenRepository.save(new KakaoAccessToken(memberId, kakaoTokenDto.access_token(), kakaoTokenDto.expires_in()));
+        refreshTokenRepository.save(new KakaoRefreshToken(memberId, kakaoTokenDto.refresh_token(), kakaoTokenDto.refresh_token_expires_in()));
     }
 
     public void deleteAccessToken(Long memberId) {
-        kakaoTokenRepository.deleteAccessToken(memberId);
+        accessTokenRepository.deleteById(memberId);
     }
 
     public void deleteRefreshToken(Long memberId) {
-        kakaoTokenRepository.deleteRefreshToken(memberId);
+        refreshTokenRepository.deleteById(memberId);
     }
 
     public String refreshIfAccessTokenExpired(Long memberId) {
-        if (kakaoTokenRepository.existsAccessToken(memberId)) {
-            return kakaoTokenRepository.getAccessToken(memberId);
+            return accessTokenRepository.findById(memberId)
+                    .orElseGet(() -> {
+                        String refreshToken = refreshTokenRepository.findById(memberId)
+                                .orElseThrow(() -> new AuthenticationException("Login has expired"))
+                                .getRefreshToken();
+                        KakaoTokenDto tokenDto = kakaoApiCaller.refreshAccessToken(refreshToken);
+                        return new KakaoAccessToken(memberId, tokenDto.access_token(), tokenDto.expires_in());
+                    }).getAccessToken();
         }
-        if (kakaoTokenRepository.existsRefreshToken(memberId)) {
-            String refreshToken = kakaoTokenRepository.getRefreshToken(memberId);
-            KakaoTokenDto tokenDto = kakaoApiCaller.refreshAccessToken(refreshToken);
-            return tokenDto.access_token();
-        }
-        throw new AuthenticationException("Login has expired");
-    }
+
 }
