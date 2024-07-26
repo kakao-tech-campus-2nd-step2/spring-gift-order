7d6a804e8418cb3f64b523160fdfbdc90d1f3685
GamJaDo <kim0615157@gmail.com>
2024-07-27 04:12:00 +0900

    feat (@Repository 어노테이션)

--- src/main/java/gift/repository/OptionRepository.java
+++ src/main/java/gift/repository/OptionRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import gift.entity.Option;
 
+@Repository
 public interface OptionRepository extends JpaRepository<Option, Long>{
 
 	List<Option> findByProductId(Long productId);
