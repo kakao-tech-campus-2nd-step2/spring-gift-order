1098e4332a7763f166b1a14594559927f36e095c
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-26 15:24:50 +0900

    feat : Jwt토큰 생성 메서드 추가

--- src/main/java/gift/security/JwtTokenProvider.java
+++ src/main/java/gift/security/JwtTokenProvider.java
@@ -2,27 +2,50 @@ package gift.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtTokenProvider {
 
-    @Value("${key}")
-    private String secretKey;
+    private Key secretKey;
 
     @Value("${token.expire-length}")
     private long validityInMilliseconds;
 
+    @PostConstruct
+    protected void init() {
+        this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    }
+
     public String getEmailFromToken(String token) {
         Claims claims = Jwts.parser()
-                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .setSigningKey(secretKey)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
         return claims.getSubject();
+    }
+
+    public String createToken(String email) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("sub", email);
+        long now = System.currentTimeMillis();
+        long validity = now + validityInMilliseconds;
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(validity))
+                .signWith(secretKey, SignatureAlgorithm.HS512)
+                .compact();
     }
 }
