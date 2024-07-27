7a68629306e78cc0c7ed56483ead53d264111e70
나제법 <skwpqjq@gmail.com>
2024-07-28 01:43:15 +0900

    feat: ReadMemberResponse.java password 필드 추가

--- src/main/java/gift/web/dto/response/member/ReadMemberResponse.java
+++ src/main/java/gift/web/dto/response/member/ReadMemberResponse.java
@@ -6,16 +6,18 @@ public class ReadMemberResponse {
 
     private Long id;
     private String email;
+    private String password;
     private String name;
 
-    private ReadMemberResponse(Long id, String email, String name) {
+    public ReadMemberResponse(Long id, String email, String password, String name) {
         this.id = id;
         this.email = email;
+        this.password = password;
         this.name = name;
     }
 
     public static ReadMemberResponse fromEntity(Member member) {
-        return new ReadMemberResponse(member.getId(), member.getEmail().getValue(),
+        return new ReadMemberResponse(member.getId(), member.getEmail().getValue(), member.getPassword().getValue(),
             member.getName());
     }
 
@@ -25,6 +27,10 @@ public class ReadMemberResponse {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getName() {
