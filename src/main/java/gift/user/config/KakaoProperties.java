d20cc8c0fbbcf0745a4e86bd63f5be8607012e00
Wooseok Jang <jwseok312@gmail.com>
2024-07-27 22:56:57 +0900

    fix(config): properties 수정으로 인한 prefix 설정

--- src/main/java/gift/user/config/KakaoProperties.java
+++ src/main/java/gift/user/config/KakaoProperties.java
@@ -3,7 +3,7 @@ package gift.user.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties
+@ConfigurationProperties("kakao")
 public record KakaoProperties(
     @Value("${client-id}") String clientId,
     @Value("${redirect-uri}") String redirectUri
