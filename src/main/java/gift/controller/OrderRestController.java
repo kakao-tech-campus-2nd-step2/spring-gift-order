a347cfaffdf010a81bc45bf7a31261994b024606
wvssm <neoramiregi@gmail.com>
2024-07-28 04:11:42 +0900

    feat(OrderRestController): 각 메서드에 대한 설명(Summary) 추가

--- src/main/java/gift/controller/OrderRestController.java
+++ src/main/java/gift/controller/OrderRestController.java
@@ -4,16 +4,13 @@ import gift.annotation.LoginMember;
 import gift.dto.request.MemberRequest;
 import gift.dto.request.OrderRequest;
 import gift.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/orders")
 public class OrderRestController {
     private final OrderService orderService;
@@ -22,6 +19,7 @@ public class OrderRestController {
         this.orderService = orderService;
     }
 
+    @Operation(summary = "새로운 주문을 생성합니다")
     @PostMapping
     public ResponseEntity<Void> order(@RequestHeader(HttpHeaders.AUTHORIZATION) String header, @LoginMember MemberRequest memberRequest, @RequestBody OrderRequest orderRequest){
         String token = header.substring(7);
