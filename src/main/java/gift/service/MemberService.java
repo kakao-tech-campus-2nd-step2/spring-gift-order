697f03d33019bdeaf608bef3d4f64f7c802e9085
나제법 <skwpqjq@gmail.com>
2024-07-28 01:42:06 +0900

    feat: MemberService.java login() 추가

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -1,13 +1,18 @@
 package gift.service;
 
+import gift.authentication.token.JwtProvider;
+import gift.authentication.token.Token;
 import gift.domain.Member;
 import gift.domain.vo.Email;
 import gift.repository.MemberRepository;
 import gift.repository.WishProductRepository;
 import gift.web.client.dto.KakaoAccount;
+import gift.web.dto.request.LoginRequest;
 import gift.web.dto.request.member.CreateMemberRequest;
+import gift.web.dto.response.LoginResponse;
 import gift.web.dto.response.member.CreateMemberResponse;
 import gift.web.dto.response.member.ReadMemberResponse;
+import gift.web.validation.exception.client.IncorrectEmailException;
 import java.util.NoSuchElementException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,10 +23,13 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final WishProductRepository wishProductRepository;
+    private final JwtProvider jwtProvider;
 
-    public MemberService(MemberRepository memberRepository, WishProductRepository wishProductRepository) {
+    public MemberService(MemberRepository memberRepository, WishProductRepository wishProductRepository,
+        JwtProvider jwtProvider) {
         this.memberRepository = memberRepository;
         this.wishProductRepository = wishProductRepository;
+        this.jwtProvider = jwtProvider;
     }
 
     @Transactional
@@ -47,5 +55,16 @@ public class MemberService {
         Member member = memberRepository.findById(id).orElseThrow(NoSuchElementException::new);
         wishProductRepository.deleteAllByMemberId(id);
         memberRepository.delete(member);
+    }
+
+    public LoginResponse login(LoginRequest request) {
+        Email email = Email.from(request.getEmail());
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new IncorrectEmailException(email.getValue()));
+
+        member.matchPassword(request.getPassword());
+
+        Token token = jwtProvider.generateToken(member);
+
+        return new LoginResponse(token.getValue());
     }
 }
