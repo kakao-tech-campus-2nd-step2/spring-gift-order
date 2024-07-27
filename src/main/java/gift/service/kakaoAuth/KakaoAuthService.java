ebbda526dad7421f61459836499c8f999541a16c
yoong-saks <wjdghtjd06@gmail.com>
2024-07-28 06:33:27 +0900

    feat: 인가코드로부터 토큰받아오기 구현

--- src/main/java/gift/service/kakaoAuth/KakaoAuthService.java
+++ src/main/java/gift/service/kakaoAuth/KakaoAuthService.java
@@ -1,6 +1,11 @@
 package gift.service.kakaoAuth;
 
+import gift.web.dto.Token;
+import java.net.URI;
+import java.util.Map;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class KakaoAuthService {
@@ -20,4 +25,20 @@ public class KakaoAuthService {
         return str.toString();
     }
 
+    public Token receiveToken(String code) {
+        String url = "https://kauth.kakao.com/oauth/token";
+        var body = kakaoProperties.createBody(code);
+
+        RestClient client = RestClient.create();
+        var response = client.post()
+            .uri(URI.create(url))
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(body)
+            .retrieve()
+            .toEntity(Map.class);
+
+        //System.out.println(response);
+        //System.out.println(response.getBody().get("access_token"));
+        return new Token(response.getBody().get("access_token").toString());
+    }
 }
--- src/main/java/gift/web/controller/LoginController.java
+++ src/main/java/gift/web/controller/LoginController.java
@@ -2,9 +2,12 @@ package gift.web.controller;
 
 import gift.service.kakaoAuth.KakaoAuthService;
 import gift.service.member.MemberService;
+import gift.web.dto.Token;
 import gift.web.jwt.JwtUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class LoginController {
 
@@ -25,4 +28,16 @@ public class LoginController {
     public String kakaoAuthConnect() {
         return "redirect:" + kakaoAuthService.getKakaoAuthUrl();
     }
+
+
+    @GetMapping("/login/kakao-callback")
+    public String kakaoAuthCallback(@RequestParam String code) {
+        System.out.println(code);
+        Token token = kakaoAuthService.receiveToken(code);
+
+        System.out.println(token);
+
+        return "redirect:/";
+    }
+
 }
