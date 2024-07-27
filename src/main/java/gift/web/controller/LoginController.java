ff379a09b260240b7532bdc9a221744598802274
yoong-saks <wjdghtjd06@gmail.com>
2024-07-28 06:27:09 +0900

    feat: 인가코드를 받는 url로 redirect 구현

--- src/main/java/gift/web/controller/LoginController.java
+++ src/main/java/gift/web/controller/LoginController.java
@@ -1,9 +1,28 @@
 package gift.web.controller;
 
+import gift.service.kakaoAuth.KakaoAuthService;
+import gift.service.member.MemberService;
+import gift.web.jwt.JwtUtils;
 import org.springframework.stereotype.Controller;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+    private final KakaoAuthService kakaoAuthService;
+
+    public LoginController(KakaoAuthService kakaoAuthService, MemberService memberService,
+        JwtUtils jwtUtils) {
+        this.kakaoAuthService = kakaoAuthService;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+
+    @GetMapping("/login/kakao-auth")
+    public String kakaoAuthConnect() {
+        return "redirect:" + kakaoAuthService.getKakaoAuthUrl();
+    }
 }
