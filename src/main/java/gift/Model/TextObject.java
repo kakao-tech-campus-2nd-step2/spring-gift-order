7a315e184a7365d4e1af948d4c378380fb1b43b7
BOMIN LYU <rbm0524@naver.com>
2024-07-28 03:11:32 +0900

    feat : getter, setter 추가

--- src/main/java/gift/Model/TextObject.java
+++ src/main/java/gift/Model/TextObject.java
@@ -5,13 +5,37 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TextObject {
 
-    private String objectType;
+    private String object_type;
     private String text;
     private Link link;
 
-    public TextObject(String objectType, String text, Link link) {
-        this.objectType = objectType;
+    public TextObject(String object_type, String text, Link link) {
+        this.object_type = object_type;
         this.text = text;
+        this.link = link;
+    }
+
+    public String getObject_type() {
+        return object_type;
+    }
+
+    public void setObject_type(String object_type) {
+        this.object_type = object_type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Link getLink() {
+        return link;
+    }
+
+    public void setLink(Link link) {
         this.link = link;
     }
 }
