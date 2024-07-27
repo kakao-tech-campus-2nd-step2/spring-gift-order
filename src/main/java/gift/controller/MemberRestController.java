6356170dadd2dd9833244220d62d6d7e5971ea1e
wvssm <neoramiregi@gmail.com>
2024-07-27 15:13:28 +0900

    refactor(MemberRestController): tokenResponse dto로 변경

--- src/main/java/gift/controller/MemberRestController.java
+++ src/main/java/gift/controller/MemberRestController.java
@@ -1,6 +1,6 @@
 package gift.controller;
 
-import gift.dto.response.JwtTokenResponse;
+import gift.dto.response.TokenResponse;
 import gift.dto.request.MemberRequest;
 import gift.service.JwtUtil;
 import gift.service.MemberService;
@@ -27,12 +27,12 @@ public class MemberRestController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JwtTokenResponse> giveAccessToken(@RequestBody MemberRequest request) {
+    public ResponseEntity<TokenResponse> giveAccessToken(@RequestBody MemberRequest request) {
         memberService.checkMemberExistsByEmailAndPassword(request.email(),request.password());
         String token = jwtUtil.generateToken(request.email());
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + token);
 
-        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(new JwtTokenResponse(token));
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(new TokenResponse(token));
     }
 }
