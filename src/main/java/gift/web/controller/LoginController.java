db300358a08e267225fbce070ae47c4718f59166
yoong-saks <wjdghtjd06@gmail.com>
2024-07-28 06:58:18 +0900

    feat: 멤버 회원가입 및 로그인 기능 구현 만약 회원db에 회원이 존재한다면, 바로 jwt 발급, 존재하지 않는다면, 회원가입을 하는 폼으로 이동시킨다. https://dc-choi.tistory.com/24

--- src/main/java/gift/web/controller/LoginController.java
+++ src/main/java/gift/web/controller/LoginController.java
@@ -3,20 +3,27 @@ package gift.web.controller;
 import gift.service.kakaoAuth.KakaoAuthService;
 import gift.service.kakaoAuth.KakaoInfo;
 import gift.service.member.MemberService;
+import gift.web.dto.MemberDto;
 import gift.web.dto.Token;
+import gift.web.exception.MemberNotFoundException;
 import gift.web.jwt.JwtUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 
     private final KakaoAuthService kakaoAuthService;
+    private final MemberService memberService;
+    private final JwtUtils jwtUtils;
 
     public LoginController(KakaoAuthService kakaoAuthService, MemberService memberService,
         JwtUtils jwtUtils) {
         this.kakaoAuthService = kakaoAuthService;
+        this.memberService = memberService;
+        this.jwtUtils = jwtUtils;
     }
 
     @GetMapping("/login")
@@ -32,13 +39,22 @@ public class LoginController {
 
 
     @GetMapping("/login/kakao-callback")
-    public String kakaoAuthCallback(@RequestParam String code) {
+    public String kakaoAuthCallback(@RequestParam String code, RedirectAttributes rttr) {
         System.out.println(code);
         Token token = kakaoAuthService.receiveToken(code);
 
         System.out.println(token);
         KakaoInfo kakaoInfo = kakaoAuthService.getMemberInfoFromKakaoServer(token);
 
+        MemberDto memberDto = null;
+
+        try {
+            memberDto = memberService.getMemberByEmail(kakaoInfo.email());
+        } catch (MemberNotFoundException e) {
+            rttr.addFlashAttribute("kakaoInfo", kakaoInfo);
+            return "redirect:/register-social";
+        }
+        rttr.addAttribute("token", new Token(jwtUtils.createJWT(memberDto)));
         return "redirect:/";
     }
 
