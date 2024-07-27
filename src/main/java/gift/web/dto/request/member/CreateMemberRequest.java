3b0b7853fd1eb24019c8bf61787fcf47b668a547
나제법 <skwpqjq@gmail.com>
2024-07-28 01:42:59 +0900

    feat: CreateMemberRequest.java password 필드 추가

--- src/main/java/gift/web/dto/request/member/CreateMemberRequest.java
+++ src/main/java/gift/web/dto/request/member/CreateMemberRequest.java
@@ -1,6 +1,7 @@
 package gift.web.dto.request.member;
 
 import gift.domain.Member;
+import gift.web.validation.constraints.Password;
 import jakarta.validation.constraints.Email;
 
 public class CreateMemberRequest {
@@ -8,15 +9,23 @@ public class CreateMemberRequest {
     @Email
     private String email;
 
+    @Password
+    private String password;
+
     private String name;
 
-    public CreateMemberRequest(String email, String name) {
+    public CreateMemberRequest(String email, String password, String name) {
         this.email = email;
+        this.password = password;
         this.name = name;
     }
 
     public String getEmail() {
         return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getName() {
@@ -26,7 +35,9 @@ public class CreateMemberRequest {
     public Member toEntity() {
         return new Member.Builder()
             .email(gift.domain.vo.Email.from(this.email))
+            .password(gift.domain.vo.Password.from(password))
             .name(this.name)
             .build();
     }
+
 }
