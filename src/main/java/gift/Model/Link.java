2db30a0e465270251be68d2310e4af75e7992f52
BOMIN LYU <rbm0524@naver.com>
2024-07-28 03:10:57 +0900

    feat: getter, setter 추가

--- src/main/java/gift/Model/Link.java
+++ src/main/java/gift/Model/Link.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Link {
+
     private String web_url;
     private String mobile_web_url;
 
@@ -12,4 +13,20 @@ public class Link {
         this.mobile_web_url = mobile_web_url;
     }
 
+
+    public String getWeb_url() {
+        return web_url;
+    }
+
+    public void setWeb_url(String web_url) {
+        this.web_url = web_url;
+    }
+
+    public String getMobile_web_url() {
+        return mobile_web_url;
+    }
+
+    public void setMobile_web_url(String mobile_web_url) {
+        this.mobile_web_url = mobile_web_url;
+    }
 }
