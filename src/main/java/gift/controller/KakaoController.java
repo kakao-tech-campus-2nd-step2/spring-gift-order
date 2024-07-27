5556fbb978ef1fadd2dcac96fee5d30bfc2d9a4c
Dalsungmin <150018566+Dalsungmin@users.noreply.github.com>
2024-07-27 12:56:26 +0900

    부산대 BE_문성민 5주차 과제 (1단계) (#239)
    * 0단계 환경 구현
    
    * README 1단계 작성
    
    * Feat: 인가코드 요청을 위한 쿼리 파라미터 모델 생성
    
    * Feat: 인가코드 요청 uri 리다이렉트
    
    클라이언트의 요청이 들어오면 키값과 미리 설정한 리다이텍트 uri를 넣어 카카오의서버의 인가코드 발급 uri로 리다이렉트 보낸다.
    
    * Feat: 토큰에 담을 사용자 정보 dto 생성
    
    * Feat: 응답 토큰 바디에 들어갈 dto 생성
    
    * Feat: 컨트롤러에서 토큰 요청해서 서비스 갔다가 받기
    
    전달 받은 인가 코드를 담아서 서비스로 전달, 서비스에서 엔티티로 담아 반환
    
    * Feat: 토큰 받아와서 사용자 정보 생성
    
    응답 토큰을 서비스로 전달해 사용자 정보 엔티티로 만들어 반환
    
    * Feat: 사용자 정보로 jwt 토큰 생성
    
    받은 사용자의 이메일과 비밀번호 member 생성 후 이 정보로 jwt 토큰 생성
    
    * Test: 토큰 생성 테스트
    
    라이브 클론코딩 시간에 했던 코드를 가져 왔습니다.
    
    ---------
    
    Co-authored-by: azjaehyun <60067259+azjaehyun@users.noreply.github.com>

---
+++ src/main/java/gift/controller/KakaoController.java
@@ -0,0 +1,53 @@
+package gift.controller;
+
+
+import gift.dto.KakaoMember;
+import gift.dto.KakaoTokenResponseDto;
+import gift.service.KakaoService;
+import gift.service.MemberService;
+import gift.util.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kakao/login")
+public class KakaoController {
+    private final JwtUtil jwtUtil;
+    private final KakaoService kakaoService;
+    private final MemberService memberService;
+
+    public KakaoController(KakaoService kakaoService, MemberService memberService) {
+        this.jwtUtil = new JwtUtil();
+        this.kakaoService = kakaoService;
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/authorize")
+    public void authorize(HttpServletResponse response) throws IOException {
+        response.sendRedirect("https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=8b0993ea8425d3f401667223d8d6b1a7&redirect_uri=http://localhost:8080/kakao/login/token");
+
+    }
+
+    @GetMapping("/token")
+    public ResponseEntity<?> token(@RequestParam("code") String code) {
+        Map<String, Object> responseBody = new HashMap<>();
+        KakaoTokenResponseDto kakaoTokenResponseDto = kakaoService.getAccessTokenFromKakao(code);
+        KakaoMember kakaoMember = kakaoService.getKakaoProfile(kakaoTokenResponseDto);
+        String token = memberService.loginKakaoMember(kakaoMember);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .header(HttpHeaders.AUTHORIZATION, token)
+            .body(responseBody);
+    }
+
+}
---
+++ src/main/java/gift/dto/KakaoMember.java
@@ -0,0 +1,16 @@
+package gift.dto;
+
+public record KakaoMember(KakaoProfile kakaoProfile, String password) {
+    public String name() {
+        return kakaoProfile.properties().nickname();
+    }
+
+    public String email() {
+        return kakaoProfile.id().toString() + kakaoProfile.properties().nickname();
+    }
+
+    public String password() {
+        return password;
+    }
+
+}
