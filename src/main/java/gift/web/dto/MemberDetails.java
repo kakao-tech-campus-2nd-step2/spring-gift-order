595ce3dce3140267d04ac9d2a6a29bb1af2f8679
나제법 <skwpqjq@gmail.com>
2024-07-28 01:41:12 +0900

    feat: MemberDetails.java platform 필드 추가

--- src/main/java/gift/web/dto/MemberDetails.java
+++ src/main/java/gift/web/dto/MemberDetails.java
@@ -1,20 +1,23 @@
 package gift.web.dto;
 
 import gift.domain.Member;
+import gift.domain.constants.Platform;
 import gift.domain.vo.Email;
 
 public class MemberDetails {
 
     private Long id;
     private Email email;
+    private Platform platform;
 
-    public MemberDetails(Long id, Email email) {
+    public MemberDetails(Long id, Email email, Platform platform) {
         this.id = id;
         this.email = email;
+        this.platform = platform;
     }
 
     public static MemberDetails from(Member member) {
-        return new MemberDetails(member.getId(), member.getEmail());
+        return new MemberDetails(member.getId(), member.getEmail(), member.getPlatform());
     }
 
     public Long getId() {
@@ -23,5 +26,9 @@ public class MemberDetails {
 
     public Email getEmail() {
         return email;
+    }
+
+    public Platform getPlatform() {
+        return platform;
     }
 }
