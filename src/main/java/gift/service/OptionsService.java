d88bbd0fff8540d6b6b0fa25be8a25e450dc87c4
anacnoda77 <sm1108shin@naver.com>
2024-07-27 22:19:28 +0900

    fix: ProductOptionResponse 잘못된 파라미터 수정

--- src/main/java/gift/service/OptionsService.java
+++ src/main/java/gift/service/OptionsService.java
@@ -11,6 +11,7 @@ import gift.repository.OptionsRepository;
 import gift.repository.ProductRepository;
 import gift.response.OptionResponse;
 import gift.response.ProductOptionsResponse;
+import gift.response.ProductResponse;
 import java.util.List;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.retry.ExhaustedRetryException;
@@ -38,7 +39,8 @@ public class OptionsService {
         List<OptionResponse> optionResponse = options.stream()
             .map(OptionResponse::createOptionResponse)
             .toList();
-        return new ProductOptionsResponse(product, optionResponse);
+        ProductResponse productResponse = new ProductResponse(product);
+        return new ProductOptionsResponse(productResponse, optionResponse);
     }
 
     public Options getOption(Long id) {
@@ -53,7 +55,8 @@ public class OptionsService {
         List<OptionResponse> optionResponse = options.stream()
             .map(OptionResponse::createOptionResponse)
             .toList();
-        return new ProductOptionsResponse(product, optionResponse);
+        ProductResponse productResponse = new ProductResponse(product);
+        return new ProductOptionsResponse(productResponse, optionResponse);
     }
 
     @Transactional
