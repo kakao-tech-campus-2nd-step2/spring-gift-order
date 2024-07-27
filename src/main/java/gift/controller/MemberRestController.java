a8e4eb26974d05c9899df9a460ca55769add7262
wvssm <neoramiregi@gmail.com>
2024-07-28 04:11:16 +0900

    feat(MemberRestController): 각 메서드에 대한 설명(Summary) 추가

--- src/main/java/gift/controller/MemberRestController.java
+++ src/main/java/gift/controller/MemberRestController.java
@@ -4,6 +4,7 @@ import gift.dto.response.TokenResponse;
 import gift.dto.request.MemberRequest;
 import gift.service.JwtUtil;
 import gift.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +21,14 @@ public class MemberRestController {
         this.jwtUtil = jwtUtil;
     }
 
+    @Operation(summary = "새로운 회원을 등록합니다")
     @PostMapping("/register")
     public ResponseEntity<Void> registerMember(@RequestBody MemberRequest request){
         memberService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "로그인하고 액세스 토큰(jwt 토큰)을 반환합니다")
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> giveAccessToken(@RequestBody MemberRequest request) {
         memberService.checkMemberExistsByEmailAndPassword(request.email(),request.password());
