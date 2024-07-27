ea843e1495a8a1814f9981ed2a291b7fa5f22edf
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-27 20:37:53 +0900

    test: SpringRestDocs를 사용하기 위한 `andDo` 작성

--- src/test/java/gift/controller/api/WishControllerTest.java
+++ src/test/java/gift/controller/api/WishControllerTest.java
@@ -9,6 +9,7 @@ import gift.service.WishService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
@@ -16,6 +17,9 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -23,10 +27,17 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = WishController.class)
+@AutoConfigureRestDocs
 @DisplayName("위시 컨트롤러 단위테스트")
 class WishControllerTest {
 
@@ -59,6 +70,20 @@ class WishControllerTest {
                 //Then
                 .andExpect(
                         status().isCreated()
+                )
+                .andDo(document("wish-add",
+                                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                                Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+
+                                requestHeaders(
+                                        headerWithName("Authorization").description("Authorization: Bearer ${ACCESS_TOKEN}")
+                                ),
+                                requestFields(
+                                        fieldWithPath("productId").type(JsonFieldType.NUMBER).description("위시에 추가할 상품 ID"),
+                                        fieldWithPath("quantity").type(JsonFieldType.NUMBER).description("위시에 추가할 상품의 수량")
+
+                                )
+                        )
                 );
     }
 
@@ -81,6 +106,69 @@ class WishControllerTest {
                         status().isOk(),
                         jsonPath("$.content[0].productName").value("product1"),
                         jsonPath("$.content[1].productName").value("product2")
+                )
+                .andDo(document("wish-get",
+                                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                                Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                                requestHeaders(
+                                        headerWithName("Authorization").description("Authorization: Bearer ${ACCESS_TOKEN}\n로그인으로 받게되는 토큰").optional()
+                                ),
+                                responseFields(
+                                        fieldWithPath("totalElements").type(JsonFieldType.NUMBER)
+                                                .description("전체 요소 수"),
+
+                                        fieldWithPath("totalPages").type(JsonFieldType.NUMBER)
+                                                .description("전체 페이지 수"),
+
+                                        fieldWithPath("first").type(JsonFieldType.BOOLEAN)
+                                                .description("첫 페이지인지 여부를 나타내는 boolean 값"),
+
+                                        fieldWithPath("last").type(JsonFieldType.BOOLEAN)
+                                                .description("마지막 페이지인지 여부를 나타내는 boolean 값"),
+
+                                        fieldWithPath("size").type(JsonFieldType.NUMBER)
+                                                .description("페이지당 요소 수"),
+
+                                        fieldWithPath("content").type(JsonFieldType.ARRAY)
+                                                .description("제품 목록"),
+
+                                        fieldWithPath("content[].productId").type(JsonFieldType.NUMBER)
+                                                .description("제품의 고유 식별자"),
+
+                                        fieldWithPath("content[].productName").type(JsonFieldType.STRING)
+                                                .description("제품의 이름"),
+
+                                        fieldWithPath("content[].productPrice").type(JsonFieldType.NUMBER)
+                                                .description("제품의 가격"),
+
+                                        fieldWithPath("content[].productImageUrl").type(JsonFieldType.STRING)
+                                                .description("제품 이미지의 URL"),
+
+                                        fieldWithPath("content[].productAmount").type(JsonFieldType.NUMBER)
+                                                .description("제품의 수량"),
+
+                                        fieldWithPath("number").type(JsonFieldType.NUMBER)
+                                                .description("현재 페이지 번호"),
+
+                                        fieldWithPath("sort.empty").type(JsonFieldType.BOOLEAN)
+                                                .description("정렬이 비어 있는지 여부를 나타내는 boolean 값"),
+
+                                        fieldWithPath("sort.sorted").type(JsonFieldType.BOOLEAN)
+                                                .description("결과가 정렬되어 있는지 여부를 나타내는 boolean 값"),
+
+                                        fieldWithPath("sort.unsorted").type(JsonFieldType.BOOLEAN)
+                                                .description("결과가 정렬되지 않은 상태인지 여부를 나타내는 boolean 값"),
+
+                                        fieldWithPath("pageable").type(JsonFieldType.STRING)
+                                                .description("페이지네이션 정보"),
+
+                                        fieldWithPath("numberOfElements").type(JsonFieldType.NUMBER)
+                                                .description("현재 페이지의 요소 수"),
+
+                                        fieldWithPath("empty").type(JsonFieldType.BOOLEAN)
+                                                .description("페이지가 비어 있는지 여부를 나타내는 boolean 값")
+                                )
+                        )
                 );
     }
 
@@ -99,6 +187,21 @@ class WishControllerTest {
                 //Then
                 .andExpect(
                         status().isOk()
+                ).andDo(document("wish-update",
+                                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                                Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                                requestHeaders(
+                                        headerWithName("Authorization").description("Authorization: Bearer ${ACCESS_TOKEN}\n로그인으로 받게되는 토큰").optional()
+                                ),
+
+                                requestFields(
+                                        fieldWithPath("productId").type(JsonFieldType.NUMBER)
+                                                .description("수정할 상품의 ID")
+                                        ,
+                                        fieldWithPath("quantity").type(JsonFieldType.NUMBER)
+                                                .description("수정할 수량")
+                                )
+                        )
                 );
     }
 
@@ -109,11 +212,21 @@ class WishControllerTest {
         when(authInterceptor.preHandle(any(), any(), any())).thenReturn(true);
 
         //When
-        mockMvc.perform(MockMvcRequestBuilders
-                        .delete(URL + "/1"))
+        mockMvc.perform(RestDocumentationRequestBuilders
+                        .delete(URL + "/{id}", 1))
                 //Then
                 .andExpect(
                         status().isOk()
+                ).andDo(document("wish-delete",
+                                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                                Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                                requestHeaders(
+                                        headerWithName("Authorization").description("Authorization: Bearer ${ACCESS_TOKEN}\n로그인으로 받게되는 토큰").optional()
+                                ),
+                                pathParameters(
+                                        parameterWithName("id").description("삭제하고 싶은 위시 ID")
+                                )
+                        )
                 );
     }
 }
