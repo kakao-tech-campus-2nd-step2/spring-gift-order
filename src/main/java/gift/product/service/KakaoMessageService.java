618aaf01e9b24581c5b28e30ec000391a3b1b99b
sanghee0820 <lsh277604@gmail.com>
2024-07-27 21:52:58 +0900

    fix: 메시지 전송 시 exception 처리 로직 수정

--- src/main/java/gift/product/service/KakaoMessageService.java
+++ src/main/java/gift/product/service/KakaoMessageService.java
@@ -3,6 +3,7 @@ package gift.product.service;
 import gift.config.KakaoMessageConfig;
 import gift.product.exception.kakao.KakaoMessageException;
 import gift.product.service.command.BuyProductMessageCommand;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,14 +18,13 @@ public class KakaoMessageService {
         var client = kakaoMessageConfig.createMessageSendClient(buyProductMessageCommand.accessToken());
         var body = buyProductMessageCommand.toKakaoMessageTemplate();
 
-        var response = client
+        var result = client
                 .post()
                 .body(body)
                 .retrieve()
+                .onStatus(HttpStatusCode::isError, ((request, response) -> {
+                    throw new KakaoMessageException();
+                }))
                 .toEntity(String.class);
-
-        if (response.getStatusCode().isError()) {
-            throw new KakaoMessageException();
-        }
     }
 }
