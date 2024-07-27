0b857199937af4c7ff661d74c05df95a7fe3b17f
Woojae Jeong <woo6388@naver.com>
2024-07-28 03:23:53 +0900

    refactor : @NotBalnk->@NotNull로 수정

--- src/main/java/gift/DTO/RequestOrderDTO.java
+++ src/main/java/gift/DTO/RequestOrderDTO.java
@@ -2,13 +2,14 @@ package gift.DTO;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record RequestOrderDTO (
-    @NotBlank(message = "옵션을 선택하지 않으셨습니다. 선택해주세요")
+    @NotNull(message = "옵션을 선택하지 않으셨습니다. 선택해주세요")
     @Min(value = 1, message = "옵션Id 값은 1이상입니다")
     Long optionId,
 
-    @NotBlank(message = "수량을 입력하지 않으셨습니다. 입력해주세요")
+    @NotNull(message = "수량을 입력하지 않으셨습니다. 입력해주세요")
     @Min(value = 1, message = "수량은 적어도 1이상입니다")
     Integer quantity,
 
