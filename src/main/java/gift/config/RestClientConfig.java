302531d08fa2bfdc2447ffc3a2679d36fcc9c2ca
Woojae Jeong <woo6388@naver.com>
2024-07-27 15:08:28 +0900

    feat : RestClient 타임아웃 구현

--- src/main/java/gift/config/RestClientConfig.java
+++ src/main/java/gift/config/RestClientConfig.java
@@ -2,13 +2,27 @@ package gift.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
     public RestClient restClient(){
-        return RestClient.builder().build();
+
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofMillis(5000))
+                .build();
+
+        JdkClientHttpRequestFactory factory = new JdkClientHttpRequestFactory(httpClient);
+        factory.setReadTimeout(Duration.ofMillis(5000));
+
+        return RestClient.builder()
+                .requestFactory(factory)
+                .build();
     }
 }
