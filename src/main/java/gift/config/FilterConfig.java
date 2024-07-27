1e1d5db814684dfab57ba4a6439797783a7002ef
Giheon Do <dgh0001@naver.com>
2024-07-27 15:29:00 +0900

    feat: 모든 route에 대해 filter 추가

--- src/main/java/gift/config/FilterConfig.java
+++ src/main/java/gift/config/FilterConfig.java
@@ -1,6 +1,8 @@
 package gift.config;
 
+import gift.filter.AdminFilter;
 import gift.filter.JwtFilter;
+import gift.repository.UserRepository;
 import gift.util.UserUtility;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -12,17 +14,32 @@ public class FilterConfig {
 
     @Value("${spring.var.token-prefix}")
     private String tokenPrefix;
-    private UserUtility userUtility;
 
-    public FilterConfig(UserUtility userUtility) {
+    private final UserUtility userUtility;
+    private final UserRepository userRepository;
+
+    public FilterConfig(UserUtility userUtility, UserRepository userRepository) {
         this.userUtility = userUtility;
+        this.userRepository = userRepository;
     }
 
     @Bean
     public FilterRegistrationBean<JwtFilter> jwtFilter() {
         FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new JwtFilter(tokenPrefix, userUtility));
+        registrationBean.addUrlPatterns("/api/products/*");
+        registrationBean.addUrlPatterns("/api/orders/*");
         registrationBean.addUrlPatterns("/api/wishlists/*");
+        registrationBean.addUrlPatterns("/api/users/me");
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<AdminFilter> adminFilter() {
+        FilterRegistrationBean<AdminFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new AdminFilter(tokenPrefix, userUtility));
+        registrationBean.addUrlPatterns("/admin/*");
+        registrationBean.addUrlPatterns("/api/categories/*");
         return registrationBean;
     }
 }
