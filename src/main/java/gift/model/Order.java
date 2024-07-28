22d560af547c667a5d1b33a61ed13f83952b8e65
donghyeon <thornappl2@gmail.com>
2024-07-29 00:05:04 +0900

    feat: Order DTO 구현, Entity 구현

---
+++ src/main/java/gift/model/Order.java
@@ -0,0 +1,53 @@
+package gift.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "option_id", nullable = false)
+    private Option option;
+
+    private int quantity;
+
+    private LocalDateTime orderDateTime;
+
+    private String message;
+
+    public Order() {
+    }
+
+    public Order(Option option, int quantity, LocalDateTime orderDateTime, String message) {
+        this.option = option;
+        this.quantity = quantity;
+        this.orderDateTime = orderDateTime;
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Option getOption() {
+        return option;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public LocalDateTime getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
