97fb2f36bcd80032d0dff863f14984c0a3e778e5
anacnoda77 <sm1108shin@naver.com>
2024-07-28 18:29:27 +0900

    feat: Mock ArgumentResolver 등록

--- src/test/java/gift/restdocs/MockAuthApiInterceptor.java
+++ src/test/java/gift/restdocs/MockAuthApiInterceptor.java
@@ -1,9 +1,11 @@
 package gift.restdocs;
 
 
+import gift.auth.CheckRole;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,6 +15,7 @@ public class MockAuthApiInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws Exception {
+        request.setAttribute("member_id", "1");
         return true;
     }
 
--- src/test/java/gift/restdocs/RestDocsConfig.java
+++ src/test/java/gift/restdocs/RestDocsConfig.java
@@ -1,11 +1,15 @@
 package gift.restdocs;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,5 +28,10 @@ public class RestDocsConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MockAuthApiInterceptor());
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new MockLoginMemberArgumentResolver());
     }
 }
