7ae595f3abe79e48367a9099c7759751a6806f01
daolove0323 <daolove0323@gmail.com>
2024-07-28 22:51:13 +0900

    feat: 카카오토큰 응답 Dto 구현

--- src/main/java/gift/controller/auth/KakaoTokenResponse.java
+++ src/main/java/gift/controller/auth/KakaoTokenResponse.java
@@ -1,5 +1,39 @@
 package gift.controller.auth;
 
-public record KakaoTokenResponse(String accessToken, String tokenType, String refreshToken,
-                                 String expiresIn, String refreshTokenExpiresIn) {
+public class KakaoTokenResponse {
+
+    String accessToken;
+    String tokenType;
+    String refreshToken;
+    String expiresIn;
+    String refreshTokenExpiresIn;
+
+    public KakaoTokenResponse(String accessToken, String tokenType, String refreshToken,
+        String expiresIn, String refreshTokenExpiresIn) {
+        this.accessToken = accessToken;
+        this.tokenType = tokenType;
+        this.refreshToken = refreshToken;
+        this.expiresIn = expiresIn;
+        this.refreshTokenExpiresIn = refreshTokenExpiresIn;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public String getExpiresIn() {
+        return expiresIn;
+    }
+
+    public String getRefreshTokenExpiresIn() {
+        return refreshTokenExpiresIn;
+    }
 }
