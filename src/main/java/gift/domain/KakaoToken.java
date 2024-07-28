a4ae68569fb74b75895b0e03d4d7a39719a3a28c
Lucerna00 <pjs3859811@naver.com>
2024-07-28 16:25:02 +0900

    refactor: add table name to entity

--- src/main/java/gift/domain/KakaoToken.java
+++ src/main/java/gift/domain/KakaoToken.java
@@ -3,8 +3,10 @@ package gift.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "kakao_token")
 public class KakaoToken {
 
     @Id
