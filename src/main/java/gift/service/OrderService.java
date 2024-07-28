69a66d6b7830659cc33058036c7ba182679a04a7
ajy4304 <ajy4304@naver.com>
2024-07-28 23:38:19 +0900

    feat(OrderService): 카카오 메세지 전송 메서드 사용

--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -20,12 +20,26 @@ public class OrderService {
     @Autowired
     private OptionService optionService;
 
+    @Autowired
+    private KakaoMessageService kakaoMessageService;
+
     public Order placeOrder(String email, OrderRequestDTO orderRequestDTO) {
         Member member = memberService.findMemberEntityByEmail(email);
         Option option = optionService.findOptionById(orderRequestDTO.getOptionId());
         optionService.subtractOptionQuantity(option.getId(), orderRequestDTO.getQuantity());
 
         Order order = new Order(member, option, orderRequestDTO.getQuantity(), orderRequestDTO.getMessage());
-        return orderRepository.save(order);
+        Order savedOrder = orderRepository.save(order);
+
+        kakaoMessageService.sendOrderMessage(member.getToken(), createOrderMessage(savedOrder));
+        return savedOrder;
+    }
+
+    private String createOrderMessage(Order order) {
+        return "주문이 완료되었습니다.\n" +
+            "상품명: " + order.getOption().getProduct().getName() + "\n" +
+            "옵션: " + order.getOption().getName() + "\n" +
+            "수량: " + order.getQuantity() + "\n" +
+            "메시지: " + order.getMessage();
     }
 }
