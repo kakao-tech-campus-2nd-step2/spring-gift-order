889ce778dddea0f19d7ef6a24637788c4c94e821
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-29 01:36:52 +0900

    feat: RestDocsApplicationTest 추가

---
+++ src/test/java/gift/TestingRestdocsApplicationTests.java
@@ -0,0 +1,14 @@
+package gift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+public class TestingRestdocsApplicationTests {
+
+    @Test
+    public void contextLoads() throws Exception {
+
+    }
+}
