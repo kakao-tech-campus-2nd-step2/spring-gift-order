9fda8d1e96ff3d0d684f3b2f4a89bdd0e49fd2f9
rdme0 <we6610@gmail.com>
2024-07-28 20:27:48 +0900

    :sparkles: feat: api 문서 기능 추가

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -1,6 +1,7 @@
 package gift.controller;
 
 import gift.annotation.LoginMember;
+import gift.dto.betweenClient.ResponseDTO;
 import gift.dto.betweenClient.member.MemberDTO;
 import gift.dto.betweenClient.order.OrderRequestDTO;
 import gift.dto.betweenClient.order.OrderResponseDTO;
@@ -9,6 +10,12 @@ import gift.service.MemberService;
 import gift.service.OptionService;
 import gift.service.OrderService;
 import gift.service.WishListService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,7 +45,16 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<?> order(@LoginMember MemberDTO memberDTO, @RequestBody OrderRequestDTO orderRequestDTO) {
+    @Operation(description = "클라이언트가 제출한 주문을 처리하고, 카카오톡으로 주문 메세지를 보냅니다. " +
+            " 클라이언트는 'Authorization' 헤더에 'Bearer {토큰}' 형식으로 토큰을 제출해야 합니다.", tags = "Order")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "주문을 성공하였습니다.",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = OrderResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다. 보통 주문에 필요한 회원, 옵션 정보가 잘못되었거나, 재고가 소진된 경우입니다.",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class))),
+            @ApiResponse(responseCode = "500", description = "서버에 의한 오류입니다.",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)))})
+    public ResponseEntity<?> order(@Parameter(hidden = true) @LoginMember MemberDTO memberDTO, @RequestBody OrderRequestDTO orderRequestDTO) {
         String accessToken = memberService.getMemberAccessToken(memberDTO.getEmail());
 
         optionService.subtractOptionQuantity(orderRequestDTO.optionId(),
