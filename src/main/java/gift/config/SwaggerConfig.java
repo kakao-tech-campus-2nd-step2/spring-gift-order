a2bee5488d729ca8d8b01b9745ebd87ff447f333
Giheon Do <dgh0001@naver.com>
2024-07-28 16:30:35 +0900

    refactor: springdoc으로 swagger 구성

--- src/main/java/gift/config/SwaggerConfig.java
+++ src/main/java/gift/config/SwaggerConfig.java
@@ -1,31 +1,21 @@
 package gift.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    public ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
+    public OpenAPI openAPI() {
+        Info info = new Info()
                 .title("spring-gift API Documentation")
                 .description("ktc spring-gift api")
-                .version("0.1")
-                .build();
+                .version("0.1");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
     }
 }
