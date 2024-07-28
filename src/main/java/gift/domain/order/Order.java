b91ebfbe00cbe592fea2554e760cda171761442b
yoong-saks <wjdghtjd06@gmail.com>
2024-07-29 04:23:22 +0900

    feat: Order 엔티티 생성

---
+++ src/main/java/gift/domain/order/Order.java
@@ -0,0 +1,34 @@
+package gift.domain.order;
+
+import gift.domain.option.Option;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Option option;
+
+    @Column(nullable = false)
+    private Long quantity;
+
+    @CreationTimestamp
+    private LocalDateTime orderDateTime;
+
+    @Column(nullable = false)
+    private String message;
+
+}
