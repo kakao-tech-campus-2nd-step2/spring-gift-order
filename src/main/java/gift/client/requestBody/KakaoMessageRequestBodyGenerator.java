5232b7fc4486040401cd6d7c68561c999cb0bfd8
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-29 00:48:36 +0900

    refactor: Logger 를 훨씬 가독성있게 수정

--- src/main/java/gift/client/requestBody/KakaoMessageRequestBodyGenerator.java
+++ src/main/java/gift/client/requestBody/KakaoMessageRequestBodyGenerator.java
@@ -3,15 +3,14 @@ package gift.client.requestBody;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.dto.request.KakaoMessageTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class KakaoMessageRequestBodyGenerator {
-
-    private String message;
+    private static final Logger log = LoggerFactory.getLogger(KakaoMessageRequestBodyGenerator.class);
+    private final String message;
     private final ObjectMapper objectMapper;
 
     public KakaoMessageRequestBodyGenerator(String message, ObjectMapper objectMapper) {
@@ -26,7 +25,7 @@ public class KakaoMessageRequestBodyGenerator {
         try {
             param.add("template_object", objectMapper.writeValueAsString(template));
         } catch (JsonProcessingException e) {
-            Logger.getLogger(KakaoMessageRequestBodyGenerator.class.getName()).log(Level.WARNING, "ObjectMapper가 JSON 파싱에 실패");
+            log.warn("ObjectMapper가 JSON 파싱에 실패");
         }
 
         return param;
