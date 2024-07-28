462c8ccd8d613e223d99cd53234b85350032ed2e
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-29 01:40:40 +0900

    feat : Product Controller 뷰 컨트롤러 분리

--- src/main/java/gift/product/ProductController.java
+++ src/main/java/gift/product/ProductController.java
@@ -1,72 +1,45 @@
 package gift.product;
 
-import gift.category.CategoryResponse;
-import gift.category.CategoryService;
 import gift.option.OptionRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/manager")
+@RestController
+@RequestMapping("/api")
 public class ProductController {
     private final ProductService productService;
-    private final CategoryService categoryService;
 
-    public ProductController(ProductService productService, CategoryService categoryService) {
-        this.categoryService = categoryService;
-        ;
+    public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
-    @ModelAttribute("categories")
-    public List<CategoryResponse> categories() {
-        return categoryService.findAllCategories();
+    @PostMapping("/products")
+    public ProductResponse addProduct(@Valid @RequestBody ProductOptionRequest productOptionRequest) {
+        return new ProductResponse(productService.insertNewProduct(productOptionRequest.getProductRequest(), productOptionRequest.getOptionRequest()));
     }
 
-
-    @PostMapping("/products/add")
-    public String addProduct(@Valid @ModelAttribute("newProduct") ProductRequest newProduct, BindingResult bindingResult1, @Valid @ModelAttribute("option") OptionRequest option, BindingResult bindingResult2, RedirectAttributes redirectAttributes) {
-        if (bindingResult1.hasErrors() || bindingResult2.hasErrors()) {
-            return "AddProduct";
-        }
-
-        Product product = productService.insertNewProduct(newProduct, option);
-        redirectAttributes.addAttribute("id", product.getId());
-
-        return "redirect:/manager/products/{id}";
-    }
-
-    @PutMapping("/products/update/{id}")
+    @PutMapping("/products/{id}")
     @Transactional
-    public String updateProduct(@PathVariable Long id, @Valid @ModelAttribute("product") ProductRequest product, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            return "UpdateProduct";
-        }
-        productService.updateProduct(id, product);
+    public ProductResponse updateProduct(@PathVariable Long id, @Valid @RequestBody ProductRequest product) {
 
-        redirectAttributes.addAttribute("id", id);
-        return "redirect:/manager/products/{id}";
+        return new ProductResponse(productService.updateProduct(id, product));
     }
 
-    @DeleteMapping("/products/delete/{id}")
-    public String deleteProduct(@PathVariable Long id) {
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
-        return "redirect:/manager/products";
+        return ResponseEntity.ok(null);
     }
 
     @GetMapping("/products/pages")
-    public ResponseEntity<Page<Product>> getProductsPage(@RequestParam(required = false, defaultValue = "0", value = "page") int page,
+    public ResponseEntity<Page<ProductResponse>> getProductsPage(@RequestParam(required = false, defaultValue = "0", value = "page") int page,
                                                          @RequestParam(required = false, defaultValue = "10", value = "size") @Min(1) @Max(20) int size,
                                                          @RequestParam(required = false, defaultValue = "id", value = "sortBy") String sortBy,
                                                          @RequestParam(required = false, defaultValue = "asc", value = "sortDirection") String sortDirection) {
@@ -76,27 +49,14 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public String getProductsView(Model model) {
-        model.addAttribute("products", productService.findAllProducts());
-        return "ManageProduct";
+    public List<ProductResponse> getProducts() {
+        return productService.findAllProducts();
     }
 
-    @GetMapping("/products/add")
-    public String addProductView(Model model) {
-        model.addAttribute("newProduct", new ProductResponse());
-        model.addAttribute("option", new OptionRequest());
-        return "AddProduct";
-    }
-
-    @GetMapping("/products/update/{id}")
-    public String updateProduct(@PathVariable Long id, Model model) {
-        model.addAttribute("product", new ProductResponse(productService.findByID(id)));
-        return "UpdateProduct";
-    }
 
     @GetMapping("/products/{id}")
-    public String getProduct(@PathVariable long id, Model model) {
-        model.addAttribute("product", new ProductResponse(productService.findByID(id)));
-        return "ProductInfo";
+    public ProductResponse getProduct(@PathVariable long id) {
+        return new ProductResponse(productService.findByID(id));
+
     }
 }
