933c59a50e808c8b51235418dbbc326f59714f5b
GamJaDo <kim0615157@gmail.com>
2024-07-29 00:12:53 +0900

    feat (TokenService)
    기존에 userService에서 하던 토큰 구분을 여기서 수행

---
+++ src/main/java/gift/service/TokenService.java
@@ -0,0 +1,36 @@
+package gift.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import gift.exception.InvalidTokenFormatException;
+
+@Service
+public class TokenService {
+
+	private final Map<String, TokenHandler> tokenParsers = new HashMap<>();
+	
+	public TokenService(AuthService authService, KakaoAuthService kakaoAuthService) {
+		tokenParsers.put(authService.getTokenSuffix(), authService);
+		tokenParsers.put(kakaoAuthService.getTokenSuffix(), kakaoAuthService);
+	}
+	
+	public String extractionEmail(String tokenWithSuffix) {
+		String tokenType = decisionTokenType(tokenWithSuffix);
+		String token = extractioAccessToekn(tokenWithSuffix, tokenType);
+		return tokenParsers.get(tokenType).parseToken(token);
+	}
+	
+	private String decisionTokenType(String tokenWithSuffix) {
+		return tokenParsers.keySet().stream()
+				.filter(suffix -> tokenWithSuffix.endsWith(suffix))
+				.findFirst()
+				.orElseThrow(() -> new InvalidTokenFormatException("Invalid token format."));
+	}
+	
+	private String extractioAccessToekn(String tokenWithSuffix, String tokenType) {
+		return tokenWithSuffix.replace(tokenType, "");
+	}
+}
