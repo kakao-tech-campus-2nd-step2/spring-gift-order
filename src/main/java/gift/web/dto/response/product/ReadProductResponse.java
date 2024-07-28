2fcfc261ba799f925944baa4b80754afbc1685e2
나제법 <skwpqjq@gmail.com>
2024-07-28 21:28:39 +0900

    feat: ReadProductResponse.java Options 필드 추가

--- src/main/java/gift/web/dto/response/product/ReadProductResponse.java
+++ src/main/java/gift/web/dto/response/product/ReadProductResponse.java
@@ -2,6 +2,9 @@ package gift.web.dto.response.product;
 
 import gift.domain.Product;
 import gift.web.dto.response.category.ReadCategoryResponse;
+import gift.web.dto.response.productoption.ReadProductOptionResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReadProductResponse {
 
@@ -9,20 +12,29 @@ public class ReadProductResponse {
     private final String name;
     private final Integer price;
     private final String imageUrl;
+    private final List<ReadProductOptionResponse> productOptions;
     private final ReadCategoryResponse category;
 
-    private ReadProductResponse(Long id, String name, Integer price, String imageUrl,
-        ReadCategoryResponse category) {
+    public ReadProductResponse(Long id, String name, Integer price, String imageUrl,
+        List<ReadProductOptionResponse> productOptions, ReadCategoryResponse category) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+        this.productOptions = productOptions;
         this.category = category;
     }
 
     public static ReadProductResponse fromEntity(Product product) {
-        return new ReadProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl().toString(), ReadCategoryResponse.fromEntity(product.getCategory()));
+        List<ReadProductOptionResponse> productOptions = product.getProductOptions().stream()
+            .map(productOption -> ReadProductOptionResponse.fromEntity(productOption))
+            .collect(Collectors.toList());
+
+        ReadCategoryResponse category = ReadCategoryResponse.fromEntity(product.getCategory());
+
+        return new ReadProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl().toString(), productOptions, category);
     }
+
 
     public Long getId() {
         return id;
@@ -38,6 +50,10 @@ public class ReadProductResponse {
 
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public List<ReadProductOptionResponse> getProductOptions() {
+        return productOptions;
     }
 
     public ReadCategoryResponse getCategory() {
