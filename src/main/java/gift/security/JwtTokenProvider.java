f41626b2632424d992d15d96407add825e117ba6
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-28 21:49:42 +0900

    feat : JwtTokenProvider 수정(추가 수정 필요)

--- src/main/java/gift/security/JwtTokenProvider.java
+++ src/main/java/gift/security/JwtTokenProvider.java
@@ -2,7 +2,6 @@ package gift.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,11 +15,11 @@ import java.util.Map;
 @Component
 public class JwtTokenProvider {
 
-    @Value("${jwt.secret-key}")
-    private String secretKeyString;
-
     @Value("${token.expire-length}")
     private long validityInMilliseconds;
+
+    @Value("${jwt.secret-key}")
+    private String secretKeyString;
 
     private Key secretKey;
 
@@ -30,11 +29,7 @@ public class JwtTokenProvider {
     }
 
     public String getEmailFromToken(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(secretKey)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = extractAllClaims(token);
         return claims.getSubject();
     }
 
@@ -48,7 +43,15 @@ public class JwtTokenProvider {
                 .setClaims(claims)
                 .setIssuedAt(new Date(now))
                 .setExpiration(new Date(validity))
-                .signWith(secretKey, SignatureAlgorithm.HS512)
+                .signWith(secretKey, io.jsonwebtoken.SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
