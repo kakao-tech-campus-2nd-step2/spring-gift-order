3b97dd54b3ecb04c2355f21504f403da26f7d595
xkrwjdals <developjmt@gmail.com>
2024-07-28 20:21:25 +0900

    refactor: Apply reviewer's feedback

--- src/main/java/gift/controller/KakaoApiController.java
+++ src/main/java/gift/controller/KakaoApiController.java
@@ -1,39 +1,40 @@
 package gift.controller;
 
-import gift.model.MemberDTO;
 import gift.service.KakaoApiService;
 import gift.service.MemberService;
-import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 
 @RestController
 @RequestMapping("/api/kakao")
 public class KakaoApiController {
 
     private final KakaoApiService kakaoApiService;
-    private final MemberService memberService;
 
-    public KakaoApiController(KakaoApiService kakaoApiService, MemberService memberService) {
+    public KakaoApiController(KakaoApiService kakaoApiService) {
         this.kakaoApiService = kakaoApiService;
-        this.memberService = memberService;
     }
 
     @GetMapping("/callback")
     public ResponseEntity<?> callback(@RequestParam("code") String code) {
-        String accessToken = kakaoApiService.getAccessToken(code);
-        String nickname = kakaoApiService.getKakaoProfileNickname(accessToken);
-        MemberDTO createdMember = memberService.createMember(
-            new MemberDTO(null, nickname, accessToken));
-        Map<String, String> response = new HashMap<>();
-        response.put("Aceess Token: ", accessToken);
-        response.put("Member ID: ", createdMember.id().toString());
+        Map<String, String> memberCredential = kakaoApiService.createKakaoMember(code);
+        return ResponseEntity.status(HttpStatus.OK).body(memberCredential);
+    }
 
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+    @ExceptionHandler(RestClientException.class)
+    public String handleModelValidationExceptions(RestClientException ex, Model model) {
+        String errorMessage = ex.getMessage();
+        model.addAttribute("Error", errorMessage);
+        return "error";
     }
 }
