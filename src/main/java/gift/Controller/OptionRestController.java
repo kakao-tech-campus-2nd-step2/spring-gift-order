bd178cfc32c97951f57cffe927c3b0ebe98bb82d
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-28 23:33:03 +0900

    경북대 BE_이도훈 5주차 과제 (3단계) (#335)
    * refactor : getMapping 경로 변경
    
    * docs : 2단계 구현사항 추가
    
    * 이전 단계 커밋 메시지 제거
    
    * feat : swaggerConfig 클래스 추가
    
    * docs : 3단계 구현사항 추가
    
    * feat : swagger 사용을 위한 springdoc dependency 추가
    
    * feat : swagger 정렬 등의 속성 추가
    
    * feat : api 명세서 설명 추가

--- src/main/java/gift/Controller/OptionRestController.java
+++ src/main/java/gift/Controller/OptionRestController.java
@@ -2,6 +2,9 @@ package gift.Controller;
 
 import gift.Model.DTO.OptionDTO;
 import gift.Service.OptionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "옵션 API", description = "옵션과 관련된 API")
 @RestController
 @RequestMapping("/api")
 public class OptionRestController {
@@ -18,6 +22,8 @@ public class OptionRestController {
         this.optionService = optionService;
     }
 
+    @Operation(summary = "옵션 조회", description = "상품의 id를 받으며, 해당 상품의 모든 옵션을 조회한다.")
+    @Parameter(name="id", description = "상품의 id로, 해당 상품의 옵션을 조회하는데 사용된다.")
     @GetMapping("/products/{id}/options")
     public List<OptionDTO> read(@PathVariable Long id){
         return optionService.read(id);
