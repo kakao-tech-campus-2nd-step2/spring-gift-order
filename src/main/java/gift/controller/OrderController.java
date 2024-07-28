5ef366b50c974df8b9b282048cdeaf825e067736
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-28 21:00:01 +0900

    feat: API명세서-Operation 추가

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -5,11 +5,11 @@ import gift.dto.OrderRequestDto;
 import gift.dto.OrderResponseDto;
 import gift.service.JwtUtil;
 import gift.service.KakaoApiService;
-import gift.service.OptionService;
 import gift.service.OrderService;
 import gift.vo.Option;
 import gift.vo.Order;
 import gift.vo.Product;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +29,10 @@ public class OrderController {
     }
 
     @PostMapping()
+    @Operation(
+            summary = "주문 처리",
+            description = "주문을 처리하고, 카카오로 로그인한 경우 주문 내역을 카카오 메시지로 전송하는 API입니다."
+    )
     public ResponseEntity<OrderResponseDto> processOrder(@RequestBody OrderRequestDto orderRequestDto, @RequestHeader("Authorization") String authorizationHeader) {
         Long memberId = jwtUtil.getMemberIdFromAuthorizationHeader(authorizationHeader);
 
