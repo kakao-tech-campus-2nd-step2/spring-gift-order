16dd27fdac8987f2378002e249f688d5339dd4b2
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-28 18:05:25 +0900

    feat : OrderService 생성 주문을 하면 해당 상품 옵션의 수량이 차감되고 위시 리스트에 있는 경우 위시 리스트에서 삭제됩니다.

---
+++ src/main/java/gift/service/OrderService.java
@@ -0,0 +1,44 @@
+package gift.service;
+
+
+import gift.dto.OrderRequestDTO;
+import gift.dto.OrderResponseDTO;
+import gift.model.Member;
+import gift.model.Option;
+import gift.model.Order;
+import gift.repository.OrderRepository;
+import java.time.LocalDateTime;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OptionService optionService;
+    private final MemberService memberService;
+    private final WishlistService wishlistService;
+
+    public OrderService(OrderRepository orderRepository, OptionService optionService, MemberService memberService,
+        WishlistService wishlistService) {
+        this.orderRepository = orderRepository;
+        this.optionService = optionService;
+        this.memberService = memberService;
+        this.wishlistService = wishlistService;
+    }
+
+    @Transactional
+    public OrderResponseDTO createOrder(OrderRequestDTO orderRequestDTO, String email) {
+        Option option = optionService.findOptionById(orderRequestDTO.optionId());
+        Member member = memberService.findMemberByEmail(email);
+        Long quantity = orderRequestDTO.quantity();
+        optionService.subtractQuantity(option.getId(), quantity);
+        Order order = new Order(null, option, quantity, LocalDateTime.now(), orderRequestDTO.message(), member);
+        orderRepository.save(order);
+        wishlistService.removeWishlist(member.getEmail(), option.getProduct().getId());
+        OrderResponseDTO orderResponseDTO = new OrderResponseDTO(order.getId(), order.getOption().getId(), order.getQuantity(), order.getOrderDateTime(), order.getMember().getEmail());
+        return orderResponseDTO;
+    }
+
+}
