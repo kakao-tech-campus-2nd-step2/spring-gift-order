78f09bd480903133b326933148d95c0c71d39608
donghyuun <bomul0524@gmail.com>
2024-07-28 14:59:37 +0900

    refactor: 불필요 에러 throw 제거

--- src/main/java/gift/domain/order/OrderRestController.java
+++ src/main/java/gift/domain/order/OrderRestController.java
@@ -1,14 +1,11 @@
 package gift.domain.order;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import gift.domain.user.dto.UserInfo;
 import gift.global.resolver.Login;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
 import jakarta.validation.Valid;
-import java.io.UnsupportedEncodingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,8 +29,7 @@ public class OrderRestController {
     public void order(
         @Valid @RequestBody OrderRequestDTO orderRequestDTO,
         @Parameter(description = "로그인 유저 정보") @Login UserInfo userInfo
-    )
-        throws JsonProcessingException, UnsupportedEncodingException {
+    ) {
         orderService.order(orderRequestDTO, userInfo);
     }
 }
