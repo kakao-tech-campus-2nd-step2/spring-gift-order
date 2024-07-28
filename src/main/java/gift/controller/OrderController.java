7b7e3ecd9abc039598d81c26ec1620afffa4e1d7
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-28 21:53:51 +0900

    refactor: token을 헤더로 가져오도록 수정

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -7,6 +7,8 @@ import gift.dto.RequestOrderDto;
 import gift.services.OrderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +23,8 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<OrderRequestStateDTO> addOrder(RequestOrderDto requestOrderDto,
-        String token) {
+    public ResponseEntity<OrderRequestStateDTO> addOrder(@RequestBody RequestOrderDto requestOrderDto,
+        @RequestHeader("Authorization") String token) {
         OrderDto orderDto = orderService.addOrder(requestOrderDto, token);
         return ResponseEntity.ok().body(new OrderRequestStateDTO(
             RequestStatus.success,
