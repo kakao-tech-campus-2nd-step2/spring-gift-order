c940baae70d2bbac33a2e42211f77e3286909538
ez23re <ez23re@gmail.com>
2024-07-28 12:30:59 +0900

    카카오서비스 로직 분리(단일 책임 원리)

--- src/main/java/gift/controller/KakaoController.java
+++ src/main/java/gift/controller/KakaoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping
 public class KakaoController {
 
     @Value("${kakao.api-key}")
