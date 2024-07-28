782cb0339643b3a05d3f9b87f8c0fcbfe9fe20e1
Lucerna00 <pjs3859811@naver.com>
2024-07-28 21:57:52 +0900

    refactor: change path

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -4,19 +4,17 @@ import gift.argumentresolver.LoginMember;
 import gift.dto.JwtResponse;
 import gift.dto.MemberDTO;
 import gift.dto.MemberPasswordDTO;
-import gift.exception.NoSuchMemberException;
 import gift.service.MemberService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/members")
+@RestController
+@RequestMapping("/api/members")
 public class MemberController {
 
     private final MemberService memberService;
@@ -27,19 +25,13 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public String register(@Valid @ModelAttribute MemberDTO memberDTO) {
-        memberService.register(memberDTO);
-        return "redirect:/kakao/login";
+    public ResponseEntity<JwtResponse> register(@Valid @RequestBody MemberDTO memberDTO) {
+        return ResponseEntity.ok().body(memberService.register(memberDTO));
     }
 
     @PostMapping("/login")
-    public Object login(@Valid @RequestBody MemberDTO memberDTO) {
-        try {
-            return ResponseEntity.ok().body(memberService.login(memberDTO));
-        } catch (
-            NoSuchMemberException e) {
-            return "register";
-        }
+    public ResponseEntity<JwtResponse> login(@Valid @RequestBody MemberDTO memberDTO) {
+        return ResponseEntity.ok().body(memberService.login(memberDTO));
     }
 
     @PostMapping("/password")
