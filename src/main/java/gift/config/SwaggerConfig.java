20b9017ad526f28381aae30cd64a016ba0c2df43
rdme0 <we6610@gmail.com>
2024-07-28 20:27:34 +0900

    :sparkles: feat: api 문서 기능 추가

--- src/main/java/gift/config/SwaggerConfig.java
+++ src/main/java/gift/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package gift.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,16 +13,16 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API 문서 제목")
+                        .title("Spring Gift API")
                         .version("1.0")
-                        .description("API 문서 설명"));
+                        .description("카카오 선물하기 클론 코딩 프로젝트"));
     }
 
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("public")
-                .pathsToMatch("/api/*l*")
+                .group("rdme0")
+                .pathsToMatch("/api/**", "/members/**", "/oauth/**")
                 .build();
     }
 }
