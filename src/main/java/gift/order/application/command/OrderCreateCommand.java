43e37450785fc996c9652a792933a49757df472a
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-28 15:26:25 +0900

    feat: Order Application계층 구현

---
+++ src/main/java/gift/order/application/command/OrderCreateCommand.java
@@ -0,0 +1,20 @@
+package gift.order.application.command;
+
+import gift.option.application.command.OptionCreateCommand;
+import gift.option.application.command.OptionSubtractQuantityCommand;
+import gift.option.domain.Option;
+import gift.order.domain.Order;
+
+public record OrderCreateCommand(
+        Long optionId,
+        Integer quantity,
+        String message
+) {
+    public OptionSubtractQuantityCommand toOptionSubtractQuantityCommand() {
+        return new OptionSubtractQuantityCommand(optionId, quantity);
+    }
+
+    public Order toOrder(Option option) {
+        return new Order(option, quantity, message);
+    }
+}
