64a0d88c908226f0b23a7993e00abf01be664104
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-29 00:22:11 +0900

    refactor: OrderRequest validation처리

--- src/main/java/gift/dto/request/OrderRequest.java
+++ src/main/java/gift/dto/request/OrderRequest.java
@@ -1,13 +1,23 @@
 package gift.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public class OrderRequest {
 
+    @NotNull(message = "옵션 ID를 입력하세요.")
     private Long optionId;
-    private Integer quantity;
-    private String message;
-    private Long receiveMemberId;
 
-    public OrderRequest() { }
+    @NotNull(message = "수량을 입력하세요")
+    @Positive(message = "수량은 양의 정수여야 합니다.")
+    private Integer quantity;
+
+    @NotBlank(message = "메시지를 입력하세요.")
+    private String message;
+
+    @NotNull(message = "수령 멤버를 선택하세요.")
+    private Long receiveMemberId;
 
     public OrderRequest(Long optionId, int quantity, String message, Long receiveMemberId) {
         this.optionId = optionId;
