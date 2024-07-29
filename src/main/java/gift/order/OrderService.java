5233b9f5132a4a6c71706bbbb19b3f7683de8a1a
SeoYeongU <westzeroright@gmail.com>
2024-07-29 16:52:19 +0900

    feat: 해당 상품이 위시 리스트에 있는 경우 위시 리스트에서 삭제

--- src/main/java/gift/order/OrderService.java
+++ src/main/java/gift/order/OrderService.java
@@ -7,10 +7,14 @@ import gift.exception.NotFoundOrder;
 import gift.member.Member;
 import gift.member.MemberRepository;
 import gift.member.MemberRequestDto;
+import gift.member.MemberService;
 import gift.option.Option;
 import gift.option.OptionRepository;
 import gift.option.OptionService;
+import gift.wishlist.Wishlist;
+import gift.wishlist.WishlistRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
@@ -22,22 +26,30 @@ public class OrderService {
     private final MemberRepository memberRepository;
     private final KakaoClient kakaoClient;
     private final OptionService optionService;
+    private final WishlistRepository wishlistRepository;
 
-    public OrderService(OrderRepository orderRepository, OptionRepository optionRepository, MemberRepository memberRepository, KakaoClient kakaoClient, OptionService optionService) {
+    public OrderService(OrderRepository orderRepository, OptionRepository optionRepository, MemberRepository memberRepository, KakaoClient kakaoClient, OptionService optionService, WishlistRepository wishlistRepository) {
         this.orderRepository = orderRepository;
         this.optionRepository = optionRepository;
         this.memberRepository = memberRepository;
         this.kakaoClient = kakaoClient;
         this.optionService = optionService;
+        this.wishlistRepository = wishlistRepository;
     }
 
-    public OrderResponseDto createOrder(MemberRequestDto memberRequestDto, OrderRequestDto orderRequestDto) {
+    public OrderResponseDto createOrder(MemberRequestDto memberRequestDto, OrderRequestDto orderRequestDto)
+        throws NotFoundMember {
         Option option = optionRepository.findById(orderRequestDto.optionId())
             .orElseThrow(() -> new NotFoundOption("해당 옵션을 찾을 수 없습니다"));
 
         optionService.substractQuantity(option.getId(), orderRequestDto.quantity());
 
         Order order = new Order(option, orderRequestDto.quantity(), orderRequestDto.message(), new Member(memberRequestDto.email(), memberRequestDto.password()));
+
+        Member member = memberRepository.findByEmail(memberRequestDto.email())
+                .orElseThrow(() -> new NotFoundMember("유효하지 않은 회원입니다."));
+
+        removeFromWishlistIfExists(member.getId(), order.getOption().getId());
 
         orderRepository.save(order);
 
@@ -77,6 +89,16 @@ public class OrderService {
         }
 
         orderRepository.delete(order);
+    }
+
+    public void removeFromWishlistIfExists(Long memberId, Long optionId) {
+        Option option = optionRepository.findById(optionId)
+            .orElseThrow(() -> new NotFoundOption("유효하지 않은 옵션입니다."));
+        Long productId = option.getProduct().getId();
+
+        if (wishlistRepository.existsByProductIdAndMemberId(productId, memberId)) {
+            wishlistRepository.deleteByMemberIdAndProductId(memberId, productId);
+        }
     }
 
 }
