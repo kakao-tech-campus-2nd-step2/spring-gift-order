4b1cf1ef033ef5c1183d7256cc1332c318c6bfce
hyunseo <wndlthsk@naver.com>
2024-07-29 15:29:18 +0900

    feat : swagger 어노테이션 추가

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -7,6 +7,10 @@ import gift.dto.OrderResponseDto;
 import gift.service.OrderService;
 import gift.service.ProductService;
 import gift.service.WishService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/orders")
+@Tag(name = "Order", description = "주문 API")
 public class OrderController {
     private final OrderService orderService;
 
@@ -25,7 +30,9 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<OrderResponseDto> createOrder(@Valid @RequestBody OrderRequestDto orderRequestDto, @LoginMember Member member) {
+    @Operation(summary = "주문 처리", description = "주문을 처리합니다.")
+    @SecurityRequirement(name = "Authorization")
+    public ResponseEntity<OrderResponseDto> createOrder(@Valid @RequestBody OrderRequestDto orderRequestDto, @Parameter(hidden = true)@LoginMember Member member) {
         OrderResponseDto orderResponseDto = orderService.processOrder(orderRequestDto, member);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(orderResponseDto);
