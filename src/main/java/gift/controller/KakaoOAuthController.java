0473408b315c8030f3476c5760b450d118bd7e7a
안원모 <tommy45231@naver.com>
2024-07-29 19:48:25 +0900

    Feat : Swagger 의존성 추가 + 컨트롤러 반영

--- src/main/java/gift/controller/KakaoOAuthController.java
+++ src/main/java/gift/controller/KakaoOAuthController.java
@@ -2,10 +2,13 @@ package gift.controller;
 
 import gift.dto.KakaoUserProfile;
 import gift.service.KakaoOAuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Tag(name = "카카오 OAuth API", description = "카카오 OAuth 관련 API")
 @Controller
 public class KakaoOAuthController {
 
@@ -15,6 +18,7 @@ public class KakaoOAuthController {
         this.kakaoService = kakaoService;
     }
 
+    @Operation(summary = "카카오 OAuth 콜백", description = "카카오 OAuth 인증 후 콜백을 처리합니다.")
     @GetMapping("/oauth/kakao/callback")
     public String kakaoCallback(@RequestParam String code) {
         String accessToken = kakaoService.getAccessToken(code);
--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -1,16 +1,16 @@
 package gift.controller;
 
-import gift.domain.Member;
 import gift.dto.OrderDTO;
 import gift.dto.OrderRequest;
 import gift.service.OrderService;
 import gift.util.LoginMember;
+import gift.domain.Member;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "주문 API", description = "주문 관련된 API")
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
@@ -21,6 +21,7 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @Operation(summary = "주문 생성", description = "새로운 주문을 생성합니다.")
     @PostMapping
     public ResponseEntity<OrderDTO> createOrder(@LoginMember Member member, @RequestBody OrderRequest orderRequest) {
         OrderDTO orderDTO = orderService.createOrder(member.getId(), orderRequest.getOptionId(), orderRequest.getQuantity(), orderRequest.getMessage());
