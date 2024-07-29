710c5f39d73ff790bb808dfa00b4aae47f57403e
eunsoni <esk12704@gmail.com>
2024-07-29 21:54:33 +0900

    fix: set prefix path

--- src/main/java/gift/controller/KakaoLoginController.java
+++ src/main/java/gift/controller/KakaoLoginController.java
@@ -9,9 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/kakao")
 public class KakaoLoginController {
 
     private final KakaoProperties kakaoProperties;
@@ -23,14 +25,14 @@ public class KakaoLoginController {
         this.kakaoService = kakaoService;
     }
 
-    @GetMapping("/kakao/login")
+    @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("kakaoClientId", kakaoProperties.clientId());
         model.addAttribute("kakaoRedirectUrl", kakaoProperties.redirectUrl());
         return "kakaoLogin";
     }
 
-    @GetMapping("/kakao/oauth2/callback")
+    @GetMapping("/oauth2/callback")
     public String callbackKakao(@RequestParam String code, Model model) {
 
         try {
@@ -43,7 +45,7 @@ public class KakaoLoginController {
 
     }
 
-    @GetMapping("/kakao/loginSuccess")
+    @GetMapping("/loginSuccess")
     public String loginSuccess() {
         return "kakaoLoginSuccess";
     }
