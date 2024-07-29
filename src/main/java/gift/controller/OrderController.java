b21e9da94432229965e7d3422f704d85c8902f9f
박민규 <pmq0101@naver.com>
2024-07-29 13:06:43 +0900

    feat(OrderController): step3 커밋내역 삭제

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -9,8 +9,6 @@ import gift.dto.requestdto.OrderRequestDTO;
 import gift.dto.responsedto.OrderResponseDTO;
 import gift.service.OptionService;
 import gift.service.OrderService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/orders")
-@Tag(name = "주문 api", description = "주문 api입니다")
 public class OrderController {
     private final OptionService optionService;
     private final OrderService orderService;
@@ -32,7 +29,6 @@ public class OrderController {
     }
 
     @PostMapping()
-    @Operation(summary = "주문 api", description = "주문 api입니다")
     public ResponseEntity<SuccessBody<OrderResponseDTO>> createOrder(
         @LoginUser User user,
         @Valid @RequestBody OrderRequestDTO orderRequestDTO
