8f718db17993deec69e415766dd77e1ddb76be3b
minju26 <mjalswn26@gmail.com>
2024-07-29 15:08:50 +0900

    chore(AuthService): import문 수정

--- src/main/java/gift/service/AuthService.java
+++ src/main/java/gift/service/AuthService.java
@@ -1,6 +1,5 @@
 package gift.service;
 
-import gift.LoginType;
 import gift.domain.Member;
 import gift.dto.request.AuthRequest;
 import gift.dto.response.AuthResponse;
@@ -10,6 +9,8 @@ import gift.repository.MemberRepository;
 import gift.util.JwtUtil;
 import org.springframework.stereotype.Service;
 
+import static gift.LoginType.EMAIL;
+import static gift.LoginType.KAKAO;
 import static gift.exception.ErrorCode.KAKAO_LOGIN_USER;
 
 
@@ -24,14 +25,14 @@ public class AuthService {
     }
 
     public AuthResponse addMember(AuthRequest authRequest) {
-        Member requestMember = new Member(authRequest.email(), authRequest.password(), LoginType.EMAIL);
+        Member requestMember = new Member(authRequest.email(), authRequest.password(), EMAIL);
         Member savedMember = memberRepository.save(requestMember);
         return new AuthResponse(jwtUtil.createJWT(savedMember.getId(), savedMember.getLoginType()));
     }
 
     public AuthResponse login(AuthRequest authRequest) {
         Member storedMember = memberRepository.findMemberByEmail(authRequest.email()).orElseThrow(() -> new CustomException(ErrorCode.DATA_NOT_FOUND));
-        if (storedMember.getIsKakao()) {
+        if (storedMember.getLoginType().equals(KAKAO)) {
             throw new CustomException(KAKAO_LOGIN_USER);
         }
         return new AuthResponse(jwtUtil.createJWT(storedMember.getId(), storedMember.getLoginType()));
