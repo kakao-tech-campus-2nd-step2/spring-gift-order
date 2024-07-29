1fe57ac29da6f82615747d1696f1331a0a3cff11
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-07-29 18:46:50 +0900

    충남대 BE 박상우 5주차 과제 (3단계) (#349)
    * Init: step3 생성
    
    * Refactor: 피드백 반영
    
    jwt토큰을 활용하기 위해서 accessToken을 member에 추가하여 활용
    
    * Remove: MemberController 제거
    
    * Feat: swagger 설정 및 config 추가
    
    * Docs: swagger를 이용해 Product api 작성
    
    * Feat: 위시리스트 api작성 및 기본값설정
    
    * Feat: 카테고리 api 작성
    
    * Feat: option api 작성
    
    * Feat: login api 작성
    
    * Feat: order api 작성 및 order관련 DTO 설정
    
    * Remove: loginController 주석 제거
    
    * Refactor: 인터셉터 경로 변경
    
    wish에서 wishlist로 변경하여 인터셉터 등록 경로도 같이 변경

--- src/main/java/gift/Controller/OrderController.java
+++ src/main/java/gift/Controller/OrderController.java
@@ -1,62 +1,48 @@
 package gift.Controller;
 
-import gift.Model.Member;
-import gift.Model.Option;
 import gift.Model.OrderRequestDTO;
 import gift.Model.OrderResponseDTO;
-import gift.Model.Product;
 
-import gift.Service.LoginService;
-import gift.Service.MemberService;
-import gift.Service.OptionService;
 import gift.Service.OrderService;
-import gift.Service.ProductService;
-import gift.Service.WishlistService;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Order", description = "Order 관련 API")
 @RestController
 public class OrderController {
-    private final OptionService optionService;
-    private final MemberService memberService;
-    private final LoginService loginService;
-    private final WishlistService wishlistService;
     private final OrderService orderService;
     
-    public OrderController(ProductService productService, OptionService optionService, MemberService memberService, LoginService loginService, WishlistService wishlistService, OrderService orderService){
-        this.optionService = optionService;
-        this.memberService = memberService;
-        this.loginService = loginService;
-        this.wishlistService = wishlistService;
+    public OrderController(OrderService orderService){
         this.orderService = orderService;
     }
-
+    @Operation(
+        summary = "상품 주문하기",
+        description = "정보를 전달받아 상품을 주문"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "상품 주문하기 성공"
+    )
+    @Parameters({
+        @Parameter(name = "jwtToken", description = "회원 및 다양한 정보를 얻기 위해 클라이언트와 주고 받는 jwtToken"),
+        @Parameter(name = "orderRequestDTO", description = "주문하기 정보를 담은 객체")
+    })
     @PostMapping("/api/orders")
-    public ResponseEntity<OrderResponseDTO> doOrder(@RequestHeader(value = "Authorization") String bearerToken, @RequestBody OrderRequestDTO orderRequestDTO) {
-        // Bearer 제거해 토큰 얻음
-        String token = bearerToken.substring(6).trim();
-        // optionId를 통해 해당하는 option 가져오기
-        Option option = optionService.getOption(orderRequestDTO.getOptionId());
-        // option에 해당하는 product 가져오기
-        Product orderProduct = option.getProduct();
-        // 토큰으로 얻은 정보로 해당하는 member 가져오기
-        Member member = memberService.getMemberByEmail(loginService.getId(token)+"@kakao.com");
-        // wishlistId가 존재 한다면 wishlist 삭제
-        Long wishlistId = wishlistService.getWishlistId(member.getEmail(),orderProduct.getId());
-        if (wishlistId != null){
-            wishlistService.deleteWishlist(member.getEmail(),orderProduct.getId(),wishlistId);
-        }
-        // 해당하는 quantity만큼 option에서 빼기
-        int result = optionService.subtractQuantity(orderRequestDTO);
-
-        // response 만들기
-        OrderResponseDTO response = orderService.makeResponse(orderRequestDTO, orderProduct.getId());
+    public ResponseEntity<OrderResponseDTO> doOrder(@RequestHeader(value = "Authorization") String jwtToken, @RequestBody OrderRequestDTO orderRequestDTO) {
+        // 주문하기 후 response
+        OrderResponseDTO response = orderService.doOrder(jwtToken, orderRequestDTO);
         // 메세지 보내기
-        orderService.sendMessage(orderRequestDTO.getMessage(), token);
+        orderService.sendMessage(orderRequestDTO.getMessage(), jwtToken);
         return ResponseEntity.status(201).body(response); // 201코드와 response 리턴
     }
 }
