6c2c07bb61a7b98644055b530b69278ce53d591c
박민규 <pmq0101@naver.com>
2024-07-29 18:56:51 +0900

    refactor(SwaggerConfig): SecurityScheme 어노테이션 적용

--- src/main/java/gift/config/SwaggerConfig.java
+++ src/main/java/gift/config/SwaggerConfig.java
@@ -1,14 +1,19 @@
 package gift.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+    name = "JWT",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
 @Configuration
 public class SwaggerConfig {
     @Bean
@@ -21,16 +26,9 @@ public class SwaggerConfig {
         String jwtSchemeName = "JWT";
 
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
-        Components components = new Components()
-            .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
-                .name(jwtSchemeName)
-                .type(Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT"));
 
         return new OpenAPI()
             .info(info)
-            .addSecurityItem(securityRequirement)
-            .components(components);
+            .addSecurityItem(securityRequirement);
     }
 }
