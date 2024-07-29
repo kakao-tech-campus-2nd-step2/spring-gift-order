c95fdef58bff92d049c47750a202f1b0c652ce21
daolove0323 <daolove0323@gmail.com>
2024-07-29 15:38:39 +0900

    refac: 인가, 토큰 발급관련 코드 리팩토링

--- src/main/java/gift/controller/auth/AuthController.java
+++ src/main/java/gift/controller/auth/AuthController.java
@@ -1,28 +1,38 @@
 package gift.controller.auth;
 
-import gift.config.LoginUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gift.controller.member.MemberResponse;
+import gift.exception.AuthorizationCodeException;
 import gift.exception.UnauthorizedException;
 import gift.service.AuthService;
+import gift.service.KakaoTokenService;
+import gift.service.MemberService;
+import gift.util.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.UUID;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 public class AuthController {
 
     private final AuthService authService;
+    private final MemberService memberService;
+    private final KakaoTokenService kakaoTokenService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, MemberService memberService,
+        KakaoTokenService kakaoTokenService) {
         this.authService = authService;
+        this.memberService = memberService;
+        this.kakaoTokenService = kakaoTokenService;
     }
 
     public static void validateUserOrAdmin(LoginResponse member, UUID id) {
@@ -40,20 +50,23 @@ public class AuthController {
     }
 
     @GetMapping("/authorize")
-    public void getAuthorizationCode(@LoginUser LoginResponse member, HttpServletResponse response)
-        throws IOException {
-        response.setHeader("memberId", "" + member.id());
-        response.sendRedirect(authService.getAuthorizationUrl());
+    public void getAuthorizationCode(HttpServletResponse response) {
+        try {
+            response.sendRedirect(authService.getAuthorizationUrl());
+        } catch (Exception e) {
+            throw new AuthorizationCodeException();
+        }
     }
 
     @GetMapping("/kakaoLogin")
-    public ResponseEntity<KakaoToken> loginWithKakao(@RequestHeader("memberId") String memberId,
-        @RequestParam String code) {
-        KakaoToken token = authService.getKakaoToken(code);
-        authService.saveToken(memberId, token);
+    public ResponseEntity<Token> loginWithKakao(@RequestParam String code) {
+        KakaoTokenResponse kakaoToken = authService.getKakaoToken(code);
+        KakaoMemberInfoResponse memberInfo = authService.getMemberInfo(kakaoToken);
+        MemberResponse member = memberService.findByEmail(memberInfo.email());
+        kakaoTokenService.save(member.id(), kakaoToken);
+        Token token = new Token(JwtUtil.generateToken(member.id(), member.email()));
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + token.accessToken());
-        return ResponseEntity.status(HttpStatus.OK).headers(headers)
-            .body(token);
+        headers.add("Authorization", "Bearer " + token.token());
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(token);
     }
 }
