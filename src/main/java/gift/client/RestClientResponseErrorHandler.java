7f56f49aec1383f2899289f8b74e3b0e6f522700
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-29 13:11:36 +0900

    feat: ErrorHandler에서 예외 발생 추가

--- src/main/java/gift/client/RestClientResponseErrorHandler.java
+++ src/main/java/gift/client/RestClientResponseErrorHandler.java
@@ -3,6 +3,8 @@ package gift.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResponseErrorHandler;
 
 import java.io.BufferedReader;
@@ -24,5 +26,12 @@ public class RestClientResponseErrorHandler implements ResponseErrorHandler {
         String bodyString = reader.lines().collect(Collectors.joining(System.lineSeparator()));
 
         log.error(String.format("[카카오API ERROR 발생] 상태코드: %s %s, 에러응답필드: %s", response.getStatusCode(), response.getStatusText(), bodyString));
+
+        if (response.getStatusCode().is4xxClientError()) {
+            throw new HttpClientErrorException(response.getStatusCode(), response.getStatusText());
+        }
+        if (response.getStatusCode().is5xxServerError()) {
+            throw new HttpServerErrorException(response.getStatusCode(), response.getStatusText());
+        }
     }
 }
