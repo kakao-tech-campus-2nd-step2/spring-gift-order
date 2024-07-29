ba3013d1da8568e51e4e80f3bc6ced654321abe9
bokyeong <wntjdals0412@gmail.com>
2024-07-29 15:41:35 +0900

    Refactor: @Valid 어노테이션 추가

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -5,6 +5,7 @@ import gift.authentication.UserDetails;
 import gift.dto.ApiResponse;
 import gift.dto.OrderRequestDto;
 import gift.service.OrderService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,7 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<ApiResponse> makeOrder(@LoginMember UserDetails userDetails, @RequestBody OrderRequestDto request) {
+    public ResponseEntity<ApiResponse> makeOrder(@LoginMember UserDetails userDetails, @Valid @RequestBody OrderRequestDto request) {
         orderService.handleOrder(userDetails.id(), request);
         return ResponseEntity.ok(new ApiResponse(HttpStatus.CREATED, "주문에 성공하였습니다"));
     }
