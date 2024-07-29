dd79e8b526117fed96a9320f4f8c47845cad6b73
minju26 <mjalswn26@gmail.com>
2024-07-29 23:11:13 +0900

    feat(SwaggerConfig): swagger config 설정

---
+++ src/main/java/gift/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package gift.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "accessToken";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .components(components)
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("KakaoTechCampus Spring-Gift API Docs")
+                .description("Step2 클론코딩(카카오톡 선물하기) minju26의 API 명세입니다.")
+                .version("1.0.0");
+    }
+}
