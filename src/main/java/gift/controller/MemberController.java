bebd5df842e073940686fba9a4f98443b5e12f71
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-29 18:51:23 +0900

    refactor: 카카오로그인서비스 리팩토링

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -1,40 +1,29 @@
 package gift.controller;
 
-import gift.domain.LoginType;
 import gift.domain.Member;
 import gift.dto.request.MemberRequest;
-import gift.dto.response.KakaoProfileResponse;
-import gift.dto.response.KakaoTokenResponse;
-import gift.exception.DuplicateMemberEmailException;
-import gift.service.KakaoAuthService;
 import gift.service.MemberService;
 import gift.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static gift.domain.LoginType.KAKAO;
 import static gift.domain.LoginType.NORMAL;
 
 @RestController
 @RequestMapping("/members")
 public class MemberController {
 
-    private static final String KAKAO_EMAIL_SUFFIX = "@kakao.com";
-
     private final MemberService memberService;
     private final TokenService tokenService;
-    private final KakaoAuthService kakaoAuthService;
 
     @Autowired
-    public MemberController(MemberService memberService, TokenService tokenService, KakaoAuthService kakaoAuthService) {
+    public MemberController(MemberService memberService, TokenService tokenService) {
         this.memberService = memberService;
         this.tokenService = tokenService;
-        this.kakaoAuthService = kakaoAuthService;
     }
 
     @PostMapping("/register")
@@ -61,32 +50,8 @@ public class MemberController {
 
     @GetMapping("/kakao/login")
     public ResponseEntity<Map<String, String>> kakaoCallback(@RequestParam String code) {
-        KakaoTokenResponse tokenResponse = kakaoAuthService.getKakaoToken(code);
-
-        KakaoProfileResponse profileResponse = kakaoAuthService.getUserProfile(tokenResponse.accessToken());
-
-        String email = profileResponse.kakaoAccount().profile().nickname() + KAKAO_EMAIL_SUFFIX;
-
-        if (KAKAO_EMAIL_SUFFIX.equals(email)) {
-            return new ResponseEntity<>(Map.of("error", "이메일을 가져올 수 없습니다."), HttpStatus.BAD_REQUEST);
-        }
-
-        LoginType loginType = KAKAO;
-        Member member;
-        try {
-            member = memberService.register(new MemberRequest(email, "kakao"), loginType);
-        } catch (DuplicateMemberEmailException e) {
-            member = memberService.findByEmailAndLoginType(email, loginType);
-        }
-
-        String accessToken = tokenService.saveToken(member, tokenResponse.accessToken());
-
-        Map<String, String> response = new HashMap<>();
-        response.put("authorizationCode", code);
-        response.put("accessToken", accessToken);
-        response.put("email", email);
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        Map<String, String> response = memberService.handleKakaoLogin(code);
+        return ResponseEntity.ok(response);
 
     }
 }
