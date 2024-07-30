eed7e4e4666f47c1cd59e39edf683cc14ecd9306
나제법 <skwpqjq@gmail.com>
2024-07-31 00:20:58 +0900

    feat: OrderService.java 코드 리팩토링 및 주문 정보 저장 로직 추가

--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -3,12 +3,13 @@ package gift.service;
 import gift.authentication.token.JwtResolver;
 import gift.authentication.token.Token;
 import gift.config.KakaoProperties;
+import gift.domain.Order;
+import gift.repository.OrderRepository;
 import gift.web.client.KakaoClient;
 import gift.web.client.dto.KakaoCommerce;
 import gift.web.dto.request.order.CreateOrderRequest;
 import gift.web.dto.response.order.OrderResponse;
 import gift.web.dto.response.product.ReadProductResponse;
-import gift.web.dto.response.productoption.SubtractProductOptionQuantityResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,52 +18,65 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
 
     private final KakaoClient kakaoClient;
-    private final JwtResolver jwtResolver;
-    private final ProductOptionService productOptionService;
-    private final ProductService productService;
     private final KakaoProperties kakaoProperties;
 
+    private final JwtResolver jwtResolver;
+
+    private final ProductOptionService productOptionService;
+    private final ProductService productService;
+    private final OrderRepository orderRepository;
+
     public OrderService(KakaoClient kakaoClient, JwtResolver jwtResolver, ProductOptionService productOptionService,
-        ProductService productService, KakaoProperties kakaoProperties) {
+        ProductService productService, KakaoProperties kakaoProperties,
+        OrderRepository orderRepository) {
         this.kakaoClient = kakaoClient;
         this.jwtResolver = jwtResolver;
         this.productOptionService = productOptionService;
         this.productService = productService;
         this.kakaoProperties = kakaoProperties;
+        this.orderRepository = orderRepository;
     }
 
+    /**
+     * 주문을 생성합니다<br>
+     * 카카오 로그인을 통해 서비스를 이용 중인 회원은 나에게 보내기를 통해 알림을 전송합니다.
+     * @param accessToken 우리 서비스의 토큰
+     * @param productId 구매할 상품 ID
+     * @param memberId 구매자 ID
+     * @param request 주문 요청
+     * @return
+     */
     @Transactional
-    public OrderResponse createOrder(String accessToken, Long productId, CreateOrderRequest request) {
+    public OrderResponse createOrder(String accessToken, Long productId, Long memberId, CreateOrderRequest request) {
         //상품 옵션 수량 차감
-        SubtractProductOptionQuantityResponse subtractOptionStockResponse = productOptionService.subtractOptionStock(request);
+        productOptionService.subtractOptionStock(request);
 
-        ReadProductResponse product = productService.readProductById(productId);
-        KakaoCommerce kakaoCommerce = KakaoCommerce.of(product, request.getMessage());
+        //주문 정보 저장
+        Order order = orderRepository.save(request.toEntity(memberId, productId));
 
-        sendOrderMessageIfSocialMember(accessToken, kakaoCommerce);
-        return new OrderResponse(
-            productId,
-            request.getOptionId(),
-            subtractOptionStockResponse.getStock(),
-            request.getQuantity(),
-            product.getName(),
-            request.getMessage());
+        sendOrderMessageIfSocialMember(accessToken, productId, request);
+        return OrderResponse.from(order);
     }
 
     /**
      * 소셜 로그인을 통해 주문한 경우 카카오톡 메시지를 전송합니다
-     * @param accessToken Bearer Token
-     * @param kakaoCommerce 카카오 상거래 메시지
+     * @param accessToken 우리 서비스의 토큰
+     * @param productId 상품 ID
+     * @param request 주문 요청
      */
-    private void sendOrderMessageIfSocialMember(String accessToken, KakaoCommerce kakaoCommerce) {
+    private void sendOrderMessageIfSocialMember(String accessToken, Long productId, CreateOrderRequest request) {
         jwtResolver.resolveSocialToken(Token.fromBearer(accessToken))
-            .ifPresent(socialToken -> {
-                String json = kakaoCommerce.toJson();
+            .ifPresent(socialToken ->
                 kakaoClient.sendMessage(
                     kakaoProperties.getMessageUrlAsUri(),
                     getBearerToken(socialToken),
-                    json);
-            });
+                    generateKakaoCommerce(productId, request).toJson()
+                ));
+    }
+
+    private KakaoCommerce generateKakaoCommerce(Long productId, CreateOrderRequest request) {
+        ReadProductResponse productResponse = productService.readProductById(productId);
+        return KakaoCommerce.of(productResponse, request.getMessage());
     }
 
     private String getBearerToken(String token) {
