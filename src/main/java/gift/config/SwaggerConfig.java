21a9d66bac19371026dd05e07586561d082a0295
donghyeon <thornappl2@gmail.com>
2024-07-30 23:35:26 +0900

    feat: SwaggerConfig 구현

---
+++ src/main/java/gift/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package gift.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Gift Order API")
+                        .description("API documentation for the Gift Order application")
+                        .version("v1.0"));
+    }
+}
