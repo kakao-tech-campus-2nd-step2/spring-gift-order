0527adc7bc24b9d3084e143c1767cbb0cabfa911
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-30 18:19:09 +0900

    feat : 주문하기 트랜잭션 분리

--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -8,12 +8,17 @@ import gift.model.Option;
 import gift.model.Order;
 import gift.repository.OrderRepository;
 import java.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
 public class OrderService {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
 
     private final OrderRepository orderRepository;
     private final OptionService optionService;
@@ -38,11 +43,20 @@ public class OrderService {
         Order order = new Order(null, option, quantity, LocalDateTime.now(),
             orderRequestDTO.message(), member);
         orderRepository.save(order);
-        wishlistService.removeWishlist(member.getEmail(), option.getProduct().getId());
+        removeFromWishlist(member.getEmail(), option.getProduct().getId());
         OrderResponseDTO orderResponseDTO = new OrderResponseDTO(order.getId(),
             order.getOption().getId(), order.getQuantity(), order.getOrderDateTime(),
             order.getMessage());
         return orderResponseDTO;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void removeFromWishlist(String email, Long productId) {
+        try {
+            wishlistService.removeWishlist(email, productId);
+        } catch (Exception e) {
+            logger.error("주문은 성공했지만 위시리스트에서의 삭제는 실패했습니다.");
+        }
     }
 
 }
