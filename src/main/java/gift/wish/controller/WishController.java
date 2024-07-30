73e884c4491573ddfbf5327cf5f0d3d98b4c6f44
us4c0d3 <jwseok312@gmail.com>
2024-07-31 01:54:03 +0900

    feat(Option): Option api 명세 추가

--- src/main/java/gift/wish/controller/WishController.java
+++ src/main/java/gift/wish/controller/WishController.java
@@ -7,6 +7,14 @@ import gift.wish.dto.request.CreateWishRequest;
 import gift.wish.dto.request.UpdateWishRequest;
 import gift.wish.dto.response.WishResponse;
 import gift.wish.service.WishService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.List;
@@ -14,6 +22,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +36,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/wishes")
+@Tag(name = "위시리스트 관리", description = "위시리스트를 관리하는 API")
 public class WishController {
 
     private final WishService wishService;
@@ -36,6 +46,14 @@ public class WishController {
     }
 
     @GetMapping
+    @Operation(summary = "위시리스트 조회", description = "유저의 위시리스트를 조회합니다")
+    @Parameter(name = "Authorization", description = "Bearer jwt 토큰")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공",
+            content = @Content(schema = @Schema(implementation = Page.class))),
+        @ApiResponse(responseCode = "404", description = "위시리스트가 존재하지 않음",
+            content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+    })
     public ResponseEntity<Page<WishResponse>> getWishes(
         @LoginUser User user,
         @PageableDefault(
@@ -48,6 +66,14 @@ public class WishController {
     }
 
     @PostMapping
+    @Operation(summary = "위시 추가", description = "유저에게 새로운 위시를 추가합니다")
+    @Parameter(name = "Authorization", description = "Bearer jwt 토큰")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "성공",
+            headers = {@Header(name = "location", description = "위시가 생성된 위치 엔드포인트")}),
+        @ApiResponse(responseCode = "400", description = "Request Body가 잘못됨",
+            content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+    })
     public ResponseEntity<WishResponse> addWish(
         @LoginUser User user, @RequestBody @Valid CreateWishRequest request
     ) {
@@ -59,6 +85,13 @@ public class WishController {
     }
 
     @PatchMapping
+    @Operation(summary = "위시 수정", description = "유저의 {id} 위시를 수정합니다")
+    @Parameter(name = "Authorization", description = "Bearer jwt 토큰")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "404", description = "위시리스트가 존재하지 않음",
+            content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+    })
     public ResponseEntity<Void> updateWishes(
         @LoginUser User user, @RequestBody List<UpdateWishRequest> requests
     ) {
@@ -67,6 +100,13 @@ public class WishController {
     }
 
     @DeleteMapping({"id"})
+    @Operation(summary = "위시 삭제", description = "유저의 {id} 위시를 삭제합니다")
+    @Parameter(name = "Authorization", description = "Bearer jwt 토큰")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "404", description = "위시리스트가 존재하지 않음",
+            content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+    })
     public ResponseEntity<Void> deleteWishes(
         @LoginUser User user, @RequestParam Long id
     ) {
