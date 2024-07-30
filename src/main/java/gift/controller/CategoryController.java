003032979793fb23376a75091b2f3d9bc6458dd2
Shsin9797 <aaa8141@naver.com>
2024-07-30 23:16:36 +0900

    docs : Swagger @Parameter를 Controller 메서드중 PathVariable의 파라미터에 적용

--- src/main/java/gift/controller/CategoryController.java
+++ src/main/java/gift/controller/CategoryController.java
@@ -3,11 +3,13 @@ package gift.controller;
 import gift.dto.CategoryResponseDto;
 import gift.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
@@ -17,7 +19,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@Tag(name="category",description = "카테고리 API")
+
+@Tag(name = "category", description = "카테고리 API")
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
@@ -31,7 +34,7 @@ public class CategoryController {
     @Operation(summary = "카테고리 목록 조회", description = "모든 카테고리 목록을 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "카테고리 목록 조회 성공", content = @Content(schema = @Schema(implementation = CategoryResponseDto.class))),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class))) })
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
     public ResponseEntity<List<CategoryResponseDto>> getAllCategories() {
         List<CategoryResponseDto> categoryResponseDto = categoryService.getAllCategoryResponseDto();
         return new ResponseEntity<>(categoryResponseDto, HttpStatus.OK);
@@ -41,8 +44,10 @@ public class CategoryController {
     @Operation(summary = "특정 상품 카테고리 조회", description = "해당 상품 id를 가지고 카테고리를 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "상품 id로 카테고리 조회 성공", content = @Content(schema = @Schema(implementation = CategoryResponseDto.class))),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class))) })
-    public ResponseEntity<CategoryResponseDto> getCategoryDtoByProductId(@RequestParam("product_id") Long productId) {
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
+    public ResponseEntity<CategoryResponseDto> getCategoryDtoByProductId(@Parameter(name = "product_id", description = "product 의 id", in = ParameterIn.PATH)
+                                                                         @RequestParam("product_id") Long productId
+    ) {
         CategoryResponseDto categoryResponseDto = categoryService.getCategoryDtoByProductId(productId);
         return new ResponseEntity<>(categoryResponseDto, HttpStatus.OK);
     }
--- src/main/java/gift/controller/TokenController.java
+++ src/main/java/gift/controller/TokenController.java
@@ -2,16 +2,15 @@ package gift.controller;
 
 import gift.service.BasicTokenService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-@Tag(name="basic-token",description = "Basic 토큰 API")
+
+@Tag(name = "basic-token", description = "Basic 토큰 API")
 @RequestMapping("/token")
 @RestController
 public class TokenController {
@@ -21,9 +20,11 @@ public class TokenController {
         this.basicTokenService = basicTokenService;
     }
 
-    @GetMapping("/{memberId}")
+    @GetMapping("/{member_id}")
     @Operation(summary = "멤버 id로 토큰 생성", description = "멤버 id로 토큰을 생성합니다.")
-    public String makeTokenFrom(@RequestParam("memberId") Long memberId) {
+    public String makeTokenFrom(@Parameter(name = "memberid", description = "member 의 id", in = ParameterIn.PATH)
+                                @RequestParam("member_id") Long memberId
+    ) {
         return basicTokenService.makeTokenFrom(memberId.toString());
     }
 
