2b1f4bd4d452220cd16527ca4b1ee25f1991735b
GamJaDo <103554906+GamJaDo@users.noreply.github.com>
2024-07-30 23:48:46 +0900

    강원대 BE_김병수 5주차 (step3) (#429)
    * builc (swagger)
    
    * docs (step3 기능 요구 사항)
    
    * feat (swaggerConfig)
    
    * feat (api 설명 추가)

--- src/main/java/gift/controller/WishlistController.java
+++ src/main/java/gift/controller/WishlistController.java
@@ -18,7 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 import gift.dto.WishlistRequest;
 import gift.dto.WishlistResponse;
 import gift.service.WishlistService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "위시리스트 관리", description = "위시리스트 관련 API")
 @RestController
 @RequestMapping("/wishlist")
 public class WishlistController {
@@ -29,6 +33,8 @@ public class WishlistController {
 		this.wishlistService = wishlistService;
 	}
 
+	@Operation(summary = "위시리스트 조회", description = "사용자의 위시리스트를 페이지네이션으로 조회합니다.")
+	@ApiResponse(responseCode = "200", description = "위시리스트 조회 성공")
 	@GetMapping
 	public ResponseEntity<Page<WishlistResponse>> getWishlist(@RequestHeader("Authorization") String token,
 			@PageableDefault(sort = "product.name") Pageable pageable) {
@@ -39,6 +45,8 @@ public class WishlistController {
 		return ResponseEntity.status(HttpStatus.OK).body(wishlist);
 	}
 
+	@Operation(summary = "위시리스트 추가", description = "위시리스트에 상품을 추가합니다.")
+	@ApiResponse(responseCode = "201", description = "위시리스트 추가 성공")
 	@PostMapping
 	public ResponseEntity<Void> addWishlist(@RequestHeader("Authorization") String token,
 			@RequestBody WishlistRequest request, BindingResult bindingResult) {
@@ -46,6 +54,8 @@ public class WishlistController {
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
+	@Operation(summary = "위시리스트 삭제", description = "위시리스트에서 상품을 제거합니다.")
+	@ApiResponse(responseCode = "200", description = "위시리스트 삭제 성공")
 	@DeleteMapping
 	public ResponseEntity<Void> removeWishlist(@RequestHeader("Authorization") String token,
 			@RequestBody WishlistRequest request, BindingResult bindingResult) {
@@ -53,6 +63,8 @@ public class WishlistController {
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 
+	@Operation(summary = "위시리스트 업데이트", description = "위시리스트의 상품 수량을 업데이트합니다.")
+	@ApiResponse(responseCode = "200", description = "위시리스트 업데이트 성공")
 	@PutMapping
 	public ResponseEntity<Void> updateWishlist(@RequestHeader("Authorization") String token,
 			@RequestBody WishlistRequest request, BindingResult bindingResult) {
