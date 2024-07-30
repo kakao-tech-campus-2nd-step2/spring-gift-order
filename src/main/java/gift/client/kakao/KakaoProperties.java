b11eee7bd1119bd1738da696d4dd86e24434ed4b
us4c0d3 <jwseok312@gmail.com>
2024-07-30 15:47:25 +0900

    refactor(KakaoProperties): `@Value` 삭제

--- src/main/java/gift/client/kakao/KakaoProperties.java
+++ src/main/java/gift/client/kakao/KakaoProperties.java
@@ -1,16 +1,15 @@
 package gift.client.kakao;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("kakao")
 public record KakaoProperties(
-    @Value("${client-id}") String clientId,
-    @Value("${redirect-uri}") String redirectUri,
-    @Value("${token-url}") String tokenUrl,
-    @Value("${user-info-url}") String userInfoUrl,
-    @Value("${message-url}") String messageUrl,
-    @Value("${template-id}") String templateId
+    String clientId,
+    String redirectUri,
+    String tokenUrl,
+    String userInfoUrl,
+    String messageUrl,
+    String templateId
 ) {
 
 }
