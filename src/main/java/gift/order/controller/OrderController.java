0ec49c34229eb89b3968a03c81cc26804872aebf
us4c0d3 <jwseok312@gmail.com>
2024-07-31 01:58:47 +0900

    feat(Order): Order API 명세 추가

--- src/main/java/gift/order/controller/OrderController.java
+++ src/main/java/gift/order/controller/OrderController.java
@@ -5,6 +5,10 @@ import gift.order.dto.request.OrderRequest;
 import gift.order.dto.response.OrderResponse;
 import gift.order.service.OrderService;
 import gift.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/orders")
+@Tag(name = "주문", description = "주문을 위한 API")
 public class OrderController {
 
     private final OrderService orderService;
@@ -24,6 +29,12 @@ public class OrderController {
     }
 
     @PostMapping
+    @Operation(summary = "주문", description = "주문을 요청하고, 성공하면 카카오톡 메시지를 보냅니다")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "주문 요청 성공"),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청으로 인한 주문 실패"),
+        @ApiResponse(responseCode = "500", description = "요청 처리 중 서버 오류로 인한 문제 발생")
+    })
     public ResponseEntity<OrderResponse> createOrder(@LoginUser User user,
         @RequestBody OrderRequest orderRequest) {
         var response = orderService.createOrder(user, orderRequest);
