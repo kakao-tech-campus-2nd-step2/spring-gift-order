dafd7703fb25f7cf3ffadc60bf95255a2cba99f7
yoong-saks <wjdghtjd06@gmail.com>
2024-07-31 23:17:11 +0900

    feat: 이메일로 토큰 찾도록 구현

--- src/main/java/gift/domain/oAuthToken/OAuthTokenRepository.java
+++ src/main/java/gift/domain/oAuthToken/OAuthTokenRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OAuthTokenRepository extends JpaRepository<OAuthToken, Long> {
 
+    OAuthToken findByMemberEmail(String memberEmail);
 }
