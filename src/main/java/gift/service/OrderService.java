95ba4a6be643a17de937721427771969e2d28c9d
bokyeong <wntjdals0412@gmail.com>
2024-07-31 19:59:26 +0900

    Refactor: JSON 생성 로직 분리

--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -1,7 +1,5 @@
 package gift.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.domain.member.Member;
 import gift.domain.member.MemberRepository;
 import gift.domain.option.Option;
@@ -12,6 +10,11 @@ import gift.dto.OrderRequestDto;
 import gift.exception.CustomException;
 import gift.exception.ErrorCode;
 import gift.infra.TemplateObject;
+import gift.util.JsonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
@@ -25,56 +28,51 @@ public class OrderService {
     private final OptionRepository optionRepository;
     private final WishRepository wishRepository;
     private final MemberRepository memberRepository;
-    private final ObjectMapper objectMapper;
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
-    public OrderService(OptionRepository optionRepository, WishRepository wishRepository, MemberRepository memberRepository, ObjectMapper objectMapper) {
+    private final static String ORDER_URI = "https://kapi.kakao.com/v2/api/talk/memo/default/send";
+
+    public OrderService(OptionRepository optionRepository, WishRepository wishRepository, MemberRepository memberRepository) {
         this.optionRepository = optionRepository;
         this.wishRepository = wishRepository;
         this.memberRepository = memberRepository;
-        this.objectMapper = objectMapper;
     }
 
     @Transactional
     public void handleOrder(Long memberId, OrderRequestDto request) {
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_MEMBER));
-        //1. 옵션 수량 차감
+                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_MEMBER,memberId));
         Option option = subtractOption(request.optionId(), request.quantity());
-        //2. 위시 리스트 삭제
         Optional<Wish> wish = wishRepository.findByMemberIdAndProductId(memberId, option.getProduct().getId());
         wish.ifPresent(wishRepository::delete);
 
-        //3. 주문 내역 전송
         sendMessage(member.getAccessToken());
     }
 
     private Option subtractOption(Long optionId, Integer quantity) {
         Option option = optionRepository.findById(optionId)
-                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_OPTION));
+                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_OPTION,optionId));
         option.subtract(quantity);
         return option;
     }
 
     private void sendMessage(String accessToken) {
-        String uri = "https://kapi.kakao.com/v2/api/talk/memo/default/send";
         TemplateObject templateObject = new TemplateObject("text", "상품 주문",
                 new TemplateObject.Link("https://gift.kakao.com/home", "https://gift.kakao.com/home"));
-        String templateObjectJson;
-        try {
-            templateObjectJson = objectMapper.writeValueAsString(templateObject);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        String templateObjectJson = JsonUtil.toJson(templateObject);
         RestClient restClient = RestClient.builder().build();
         var body = new LinkedMultiValueMap<String, String>();
         body.add("template_object", templateObjectJson);
-        System.out.println(templateObjectJson);
         var response = restClient.post()
-                .uri(URI.create(uri))
+                .uri(URI.create(ORDER_URI))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .header("Authorization", "Bearer " + accessToken)
                 .body(body)
                 .retrieve()
+                .onStatus(HttpStatusCode::isError, (req, res) -> {
+                    log.error("{}: msg : {}", res.getStatusCode(),res.getHeaders());
+                    throw new RuntimeException("주문 처리 중 에러가 발생하였습니다.");
+                })
                 .toEntity(String.class);
-        System.out.println(response.getBody());
     }
 }
