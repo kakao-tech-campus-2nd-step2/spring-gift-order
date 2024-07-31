063f015605da7b69bc7f726c43b5c876d70eab49
yoong-saks <wjdghtjd06@gmail.com>
2024-07-31 23:18:24 +0900

    fix: kakao access token 서비스단에서 받아오도록 위임

--- src/main/java/gift/web/controller/OrderController.java
+++ src/main/java/gift/web/controller/OrderController.java
@@ -3,14 +3,11 @@ package gift.web.controller;
 import gift.service.order.OrderService;
 import gift.web.dto.MemberDto;
 import gift.web.dto.OrderDto;
-import gift.web.dto.Token;
 import gift.web.jwt.AuthUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +21,7 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<OrderDto> createOrder(@AuthUser MemberDto memberDto, @PathVariable Long productId, @RequestBody OrderDto orderDto, @RequestHeader
-        Token token) {
-        return new ResponseEntity<>(orderService.createOrder(token, memberDto, productId, orderDto), HttpStatus.CREATED);
+    public ResponseEntity<OrderDto> createOrder(@AuthUser MemberDto memberDto, @RequestBody OrderDto orderDto) {
+        return new ResponseEntity<>(orderService.createOrder(memberDto, orderDto), HttpStatus.CREATED);
     }
 }
