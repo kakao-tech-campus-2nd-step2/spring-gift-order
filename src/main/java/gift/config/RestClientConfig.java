c241e2fa05376d906a9576d94339ab0ec865fe51
신예준 <dpwns1826@gmail.com>
2024-08-01 15:21:06 +0900

    feat: RestClient Tiemout 추가

--- src/main/java/gift/config/RestClientConfig.java
+++ src/main/java/gift/config/RestClientConfig.java
@@ -1,7 +1,11 @@
 package gift.config;
 
+import java.time.Duration;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -9,6 +13,12 @@ public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.create();
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+            .withConnectTimeout(Duration.ofSeconds(30))
+            .withReadTimeout(Duration.ofSeconds(30));
+        ClientHttpRequestFactory requestFactory = ClientHttpRequestFactories.get(settings);
+        return RestClient.builder()
+            .requestFactory(requestFactory)
+            .build();
     }
 }
